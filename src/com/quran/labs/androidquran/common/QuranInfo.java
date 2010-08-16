a8cc711c34014e10f1c8d31c48a575a437e53845
ahmedre <ahmedre@gmail.com>
2010-08-16 06:17:00 +0300

    added more pieces of data for later use to constants.

--- src/com/quran/labs/androidquran/common/QuranInfo.java
+++ src/com/quran/labs/androidquran/common/QuranInfo.java
@@ -160,6 +160,32 @@ public class QuranInfo {
 		402, 422, 442, 462, 482, 502, 522, 542, 562, 582
 	};
 	
+	public static int[] SURA_NUM_AYAHS = {
+		7, 286, 200, 176, 120, 165, 206, 75, 129, 109, 123, 111,
+		43, 52, 99, 128, 111, 110, 98, 135, 112, 78, 118, 64, 77,
+		227, 93, 88, 69, 60, 34, 30, 73, 54, 45, 83, 182, 88, 75,
+		85, 54, 53, 89, 59, 37, 35, 38, 29, 18, 45, 60, 49, 62, 55,
+		78, 96, 29, 22, 24, 13, 14, 11, 11, 18, 12, 12, 30, 52, 52,
+		44, 28, 28, 20, 56, 40, 31, 50, 40, 46, 42, 29, 19, 36, 25,
+		22, 17, 19, 26, 30, 20, 15, 21, 11, 8, 8, 19, 5, 8, 8, 11,
+		11, 8, 3, 9, 5, 4, 7, 3, 6, 3, 5, 4, 5, 6
+	};
+	
+	public static boolean[] SURA_IS_MAKKI = {
+		true, false, false, false, false, true, true, false, false, true,
+		true, true, false, true, true, true, true, true, true, true,
+		true, false, true, false, true, true, true, true, true, true,
+		true, true, false, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, false, false, false, true,
+		true, true, true, true, false, true, false, false, false, false,
+		false, false, false, false, false, false, true, true, true, true,
+		true, true, true, true, true, false, true, true, true, true,
+		true, true, true, true, true, true, true, true, true, true,
+		true, true, true, true, true, true, true, true, false, false, true,
+		true, true, true, true, true, true, true, true, true, false,
+		true, true, true, true
+	};
+	
 	public static String getSuraNameFromPage(int page){
 		for (int i = 0; i < 114; i++){
 			if (SURA_PAGE_START[i] == page)
