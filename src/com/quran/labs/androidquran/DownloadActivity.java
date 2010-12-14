b9929bbb8c6e9fb59f5d691696070c1b46003187
Hussein Maher <hussein.maher@dynamix-systems.com>
2010-12-14 12:50:59 +0200

    Download activity initial files

---
+++ src/com/quran/labs/androidquran/DownloadActivity.java
@@ -0,0 +1,14 @@
+package com.quran.labs.androidquran;
+
+import android.os.Bundle;
+
+import com.quran.labs.androidquran.common.BaseQuranActivity;
+
+public class DownloadActivity extends BaseQuranActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	}
+
+}
