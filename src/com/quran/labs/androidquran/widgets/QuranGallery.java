dde3bc387056aac27c7dba40a838d237169fc9b3
ahmedre <ahmedre@gmail.com>
2011-03-26 00:39:10 +0200

    fixed scrollview in gallery bug al7amdulillah.

--- src/com/quran/labs/androidquran/widgets/QuranGallery.java
+++ src/com/quran/labs/androidquran/widgets/QuranGallery.java
@@ -5,18 +5,52 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.Gallery;
 
+// http://stackoverflow.com/questions/5286115
 public class QuranGallery extends Gallery {
 
+	private float mInitialX;
+    private float mInitialY;
+    private boolean mNeedToRebase;
+    private boolean mIgnore;
+
+    
 	public QuranGallery(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 
-	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY){
-		int vX;
-		if (e2.getX() > e1.getX())
-			vX = 10;
-		else vX = -10;
-		return super.onFling(e1, e2, vX, velocityY);
-	}
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+            float distanceY) {
+        if (mNeedToRebase) {
+            mNeedToRebase = false;
+            distanceX = 0;
+        }
+        return super.onScroll(e1, e2, distanceX, distanceY);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent e) {
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                mIgnore = false;
+                mNeedToRebase = true;
+                mInitialX = e.getX();
+                mInitialY = e.getY();
+                return false;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                if (!mIgnore) {
+                    float deltaX = Math.abs(e.getX() - mInitialX);
+                    float deltaY = Math.abs(e.getY() - mInitialY);
+                    mIgnore = deltaX < deltaY;
+                    return !mIgnore;
+                }
+                return false;
+            }
+            default: {
+                return super.onInterceptTouchEvent(e);
+            }
+        }
+    }
 }
