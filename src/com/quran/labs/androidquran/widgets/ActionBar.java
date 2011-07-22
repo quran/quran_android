5b913c958181c87c2f7306e11b91b5da08e0df8f
Hussein Maher <hussein.maher@dynamix-systems.com>
2011-07-23 00:47:27 +0200

    Remove warning

--- src/com/quran/labs/androidquran/widgets/ActionBar.java
+++ src/com/quran/labs/androidquran/widgets/ActionBar.java
@@ -250,7 +250,8 @@ public class ActionBar extends RelativeLayout implements OnClickListener {
     /**
      * A {@link LinkedList} that holds a list of {@link Action}s.
      */
-    public static class ActionList extends LinkedList<Action> {
+    @SuppressWarnings("serial")
+	public static class ActionList extends LinkedList<Action> {
     }
 
     /**
