ced6afaa736a1e0a07757257c47d1e15daf4a1fc
Hussein Maher <hussein.maher@dynamix-systems.com>
2011-07-30 14:45:21 +0200

    Fixes

--- src/com/quran/labs/androidquran/QuranViewActivity.java
+++ src/com/quran/labs/androidquran/QuranViewActivity.java
@@ -193,6 +193,7 @@ public class QuranViewActivity extends PageViewQuranActivity implements
 		actionBar.removeAllActions();
 		actionBar.addAction(actionBarActions.get(ACTION_PLAY), 0);
 		actionBar.addAction(actionBarActions.get(ACTION_CHANGE_READER), 1);
+		actionBar.addAction(actionBarActions.get(ACTION_JUMP_TO_AYAH), 2);
 	}
 
 	private void showDownloadDialog(final AyahItem i) {
@@ -466,8 +467,10 @@ public class QuranViewActivity extends PageViewQuranActivity implements
 		int page = QuranInfo.getPageFromSuraAyah(nextAyah.getSoura(), nextAyah
 				.getAyah());
 
-		if (quranPageFeeder.getCurrentPagePosition() != page)
+		if (quranPageFeeder.getCurrentPagePosition() != page) {
 			quranPageFeeder.jumpToPage(page);
+			updatePageInfo(page);
+		}
 		return true;
 	}
 
