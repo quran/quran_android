7535552b0ba1828eecf2bf4a94e345b917badd4d
fouad <Ahmed.fouad@Dynamix-systems.com>
2011-08-01 15:25:22 +0200

    -- show download options

--- src/com/quran/labs/androidquran/QuranViewActivity.java
+++ src/com/quran/labs/androidquran/QuranViewActivity.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
