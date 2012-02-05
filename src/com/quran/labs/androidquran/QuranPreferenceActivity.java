79ee0b031040b75a1d2911ae0197d190d6eaab82
ahmedre <ahmedre@gmail.com>
2012-02-05 00:05:13 -0800

    update preference activity to not crash on 1.5+

--- src/com/quran/labs/androidquran/QuranPreferenceActivity.java
+++ src/com/quran/labs/androidquran/QuranPreferenceActivity.java
@@ -5,41 +5,41 @@ import java.util.Locale;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
-
-import com.quran.labs.androidquran.util.QuranSettings;
 
 public class QuranPreferenceActivity extends PreferenceActivity {
 	
-	private boolean isArabic;
-	private CheckBoxPreference arabicPreference;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		//Inflate preference screen
 		addPreferencesFromResource(R.xml.quran_preferences);
-		isArabic = QuranSettings.getInstance().isArabicNames();
-		arabicPreference = (CheckBoxPreference) findPreference(getResources().getString(R.string.prefs_use_arabic_names));
+		CheckBoxPreference arabicPreference = (CheckBoxPreference) findPreference(getResources().getString(R.string.prefs_use_arabic_names));
+		arabicPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener(){
+
+			@Override
+			public boolean onPreferenceChange(Preference preference,
+					Object newValue) {
+				boolean isArabic = (Boolean)newValue;
+
+				Locale lang = (isArabic? new Locale("ar") : Locale.getDefault());
+				Locale.setDefault(lang);
+				Configuration config = new Configuration();
+				config.locale = lang;
+				getBaseContext().getResources().updateConfiguration(config,
+				      getBaseContext().getResources().getDisplayMetrics());
+				
+				return true;
+			}
+			
+		});
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-	}
-	
-	@Override
-	public void onBackPressed() {
-		// Now reload configurations if language has changed..
-		if (isArabic != arabicPreference.isChecked()) {
-			Locale locale = arabicPreference.isChecked() ? new Locale("ar") : new Locale("");
-			Locale.setDefault(locale);
-			Configuration config = new Configuration();
-			config.locale = locale;
-			getBaseContext().getResources().updateConfiguration(config,
-			      getBaseContext().getResources().getDisplayMetrics());
-		}
-		super.onBackPressed();
 	}
 }
