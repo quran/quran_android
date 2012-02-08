907b9bdc50f4b403c02a88dbeb4dc4ce06c9aca8
ahmedre <ahmedre@gmail.com>
2012-02-08 01:58:51 -0800

    quran data activity runs first and then calls quran activity (rather than previous way).

--- src/com/quran/labs/androidquran/QuranActivity.java
+++ src/com/quran/labs/androidquran/QuranActivity.java
@@ -32,6 +32,7 @@ public class QuranActivity extends BaseQuranActivity {
 	private ActionBar actionBar;
 	private static final String ACTION_BOOKMARK = "BOOKMARK";
 	private static final String ACTION_RESUME = "RESUME";
+	private static boolean alreadyJumped = false;
 		
     /** Called when the activity is first created. */
     @Override
@@ -53,8 +54,6 @@ public class QuranActivity extends BaseQuranActivity {
     
     public void setArabicFont(){
     	ArabicStyle.setAssetManager(getAssets());
-        Intent i = new Intent(this, QuranDataActivity.class);
-		startActivityForResult(i, DATA_CHECK_CODE);
     }
     
     /**
@@ -110,12 +109,7 @@ public class QuranActivity extends BaseQuranActivity {
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == DATA_CHECK_CODE){
-			showSuras();
-			Integer lastPage = QuranSettings.getInstance().getLastPage();
-			if (lastPage != null && lastPage != NO_PAGE_SAVED)
-				jumpTo(lastPage);
-		} else if (requestCode == SETTINGS_CODE) {
+		if (requestCode == SETTINGS_CODE) {
 			QuranSettings.load(prefs);
 			showSuras();
 		}
@@ -127,8 +121,12 @@ public class QuranActivity extends BaseQuranActivity {
 		QuranSettings.load(prefs);
 		showSuras();
 		
-		int lastPage = QuranSettings.getInstance().getLastPage();
-		if ((lastPage > 0) && (lastPage < 605)){
+		Integer lastPage = QuranSettings.getInstance().getLastPage();
+		if (lastPage != null && lastPage != NO_PAGE_SAVED && !alreadyJumped){
+			alreadyJumped = true;
+			jumpTo(lastPage);
+		}
+		else if (lastPage != null && lastPage > 0 && lastPage < 605){
 			int currentSura = QuranInfo.PAGE_SURA_START[lastPage-1];
 			int juz = QuranInfo.getJuzFromPage(lastPage);
 			int position = currentSura + juz - 1;
