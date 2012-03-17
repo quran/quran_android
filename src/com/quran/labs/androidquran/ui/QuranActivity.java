8968981d5501421129d69b8fb074ac450e035055
ahmedre <ahmedre@gmail.com>
2012-03-17 02:28:27 -0700

    ui fixes, initial bookmarks view, singleTask for quran page view

--- src/com/quran/labs/androidquran/ui/QuranActivity.java
+++ src/com/quran/labs/androidquran/ui/QuranActivity.java
@@ -3,9 +3,14 @@ package com.quran.labs.androidquran.ui;
 import static com.quran.labs.androidquran.data.ApplicationConstants.JUZ2_COUNT;
 import static com.quran.labs.androidquran.data.ApplicationConstants.PAGES_LAST;
 import static com.quran.labs.androidquran.data.ApplicationConstants.SURAS_COUNT;
+
+import java.util.List;
+
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,8 +25,10 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.quran.labs.androidquran.R;
+import com.quran.labs.androidquran.data.ApplicationConstants;
 import com.quran.labs.androidquran.data.QuranInfo;
 import com.quran.labs.androidquran.util.ArabicStyle;
+import com.quran.labs.androidquran.util.QuranUtils;
 
 public class QuranActivity extends SherlockActivity implements ActionBar.TabListener {
    public final String TAG = "QuranActivity";
@@ -66,6 +73,9 @@ public class QuranActivity extends SherlockActivity implements ActionBar.TabList
       switch (tabTag){
       case JUZ2_LIST:
       case BOOKMARKS_LIST:
+    	  layout = R.layout.index_sura_row;
+    	  elements = getBookmarks();
+    	  break;
       case SURA_LIST:
       default:
          layout = R.layout.index_sura_row;
@@ -101,16 +111,19 @@ public class QuranActivity extends SherlockActivity implements ActionBar.TabList
    }
 
    private class QuranRow {
-      public boolean isJuz;
       public int number;
       public int page;
       public String text;
+      public String metadata;
+      public boolean isHeader;
 
-      public QuranRow(String text, boolean isJuz, int number, int page){
+      public QuranRow(String text, String metadata, 
+    		  boolean isHeader, int number, int page){
          this.text = text;
-         this.isJuz = isJuz;
+         this.isHeader = isHeader;
          this.number = number;
          this.page = page;
+         this.metadata = metadata;
       }
    }
    
@@ -122,7 +135,7 @@ public class QuranActivity extends SherlockActivity implements ActionBar.TabList
 
       for (int juz=1; juz <= JUZ2_COUNT; juz++){
          elements[pos++] = new QuranRow(QuranInfo.getJuzTitle() + " " +
-               juz, true, juz, QuranInfo.JUZ_PAGE_START[juz-1]);
+               juz, null, true, juz, QuranInfo.JUZ_PAGE_START[juz-1]);
          next = (juz == JUZ2_COUNT) ? PAGES_LAST+1 :
             QuranInfo.JUZ_PAGE_START[juz];
          
@@ -130,13 +143,43 @@ public class QuranActivity extends SherlockActivity implements ActionBar.TabList
                 (QuranInfo.SURA_PAGE_START[sura-1] < next)) {
             String title = QuranInfo.getSuraTitle() 
                   + " " + QuranInfo.getSuraName(sura-1);
-            elements[pos++] = new QuranRow(title, false, sura,
-                  QuranInfo.SURA_PAGE_START[sura-1]);
+            elements[pos++] = new QuranRow(title, 
+            		QuranInfo.getSuraListMetaString(sura),
+            		false, sura, QuranInfo.SURA_PAGE_START[sura-1]);
             sura++;
          }
       }
 
       return elements;
+   }
+   
+   private QuranRow[] getBookmarks(){
+	   SharedPreferences prefs = PreferenceManager.
+			   getDefaultSharedPreferences(getApplicationContext());
+	   List<Integer> bookmarks = QuranUtils.getBookmarks(prefs);
+	   int lastPage = prefs.getInt(ApplicationConstants.PREF_LAST_PAGE,
+			   ApplicationConstants.NO_PAGE_SAVED);
+	   boolean showLastPage = lastPage != ApplicationConstants.NO_PAGE_SAVED;
+	   int size = bookmarks.size() + (showLastPage? 1 : 0);
+	   
+	   int index = 0;
+	   QuranRow[] res = new QuranRow[size];
+	   if (showLastPage){
+		   QuranRow currentPosition = new QuranRow(
+				   getString(R.string.bookmarks_current_page),
+				   QuranInfo.getSuraNameString(lastPage) + " - " +
+				   QuranInfo.getSuraDetailsForBookmark(lastPage),
+				   false, QuranInfo.PAGE_SURA_START[lastPage], lastPage);
+		   res[index++] = currentPosition;
+	   }
+	   
+	   for (int page : bookmarks){
+		   res[index++] = new QuranRow(
+				   QuranInfo.getSuraNameString(page),
+				   QuranInfo.getSuraDetailsForBookmark(page),
+				   false, QuranInfo.PAGE_SURA_START[page], page);
+	   }
+	   return res;
    }
 
    private class EfficientAdapter extends BaseAdapter {
@@ -179,13 +222,14 @@ public class QuranActivity extends SherlockActivity implements ActionBar.TabList
           }
           else { holder = (ViewHolder) convertView.getTag(); }
 
-          holder.page.setText(ArabicStyle.reshape(String.valueOf(mElements[position].page)));
-          holder.text.setText(ArabicStyle.reshape(mElements[position].text));
-          holder.header.setText(ArabicStyle.reshape(mElements[position].text));
-          holder.number.setText(ArabicStyle.reshape(String.valueOf(mElements[position].number)));
+    	  QuranRow item = mElements[position];
+          holder.page.setText(ArabicStyle.reshape(String.valueOf(item.page)));
+          holder.text.setText(ArabicStyle.reshape(item.text));
+          holder.header.setText(ArabicStyle.reshape(item.text));
+          holder.number.setText(ArabicStyle.reshape(String.valueOf(item.number)));
           
           int color = R.color.suraDetailsColor;
-          if (mElements[position].isJuz){
+          if (mElements[position].isHeader){
               holder.text.setVisibility(View.GONE);
               holder.header.setVisibility(View.VISIBLE);
               holder.metadata.setVisibility(View.GONE);
@@ -193,7 +237,7 @@ public class QuranActivity extends SherlockActivity implements ActionBar.TabList
               color = R.color.headerTextColor;
           }
           else {
-              String info = QuranInfo.getSuraListMetaString(mElements[position].number);
+              String info = item.metadata;
               holder.metadata.setVisibility(View.VISIBLE);
               holder.number.setVisibility(View.VISIBLE);
               holder.text.setVisibility(View.VISIBLE);
