f3cf2d8ab8834959094d69a1fdea44ce418dee7a
Hussein Maher <hussein.maher@gmail.com>
2012-07-19 11:33:40 +0200

    unused imports

--- src/com/quran/labs/androidquran/HelpActivity.java
+++ src/com/quran/labs/androidquran/HelpActivity.java
@@ -1,14 +1,13 @@
 package com.quran.labs.androidquran;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 
