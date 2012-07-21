0c269af3c401339d7234530f22e30901873f66bd
Ahmed Farra <ahmedmfarra@gmail.com>
2012-07-21 13:27:37 -0400

    Add ability to remove bookmarks from index screen

--- src/com/quran/labs/androidquran/ui/helpers/QuranRow.java
+++ src/com/quran/labs/androidquran/ui/helpers/QuranRow.java
@@ -1,19 +1,25 @@
 package com.quran.labs.androidquran.ui.helpers;
 
 public class QuranRow {
+   // Row Types
+   public static final int NONE = 0;
+   public static final int HEADER = 1;
+   public static final int PAGE_BOOKMARK = 2;
+   public static final int AYAH_BOOKMARK = 3;
+   
    public int sura;
    public int ayah;
    public int page;
    public String text;
    public String metadata;
-   public boolean isHeader;
+   public int rowType;
    public Integer imageResource;
    public String imageText;
 
-   public QuranRow(String text, String metadata, boolean isHeader, 
+   public QuranRow(String text, String metadata, int rowType, 
         int sura, int ayah, int page, Integer imageResource){
       this.text = text;
-      this.isHeader = isHeader;
+      this.rowType = rowType;
       this.sura = sura;
       this.ayah = ayah;
       this.page = page;
@@ -22,8 +28,29 @@ public class QuranRow {
       this.imageText = "";
    }
 
-   public QuranRow(String text, String metadata, boolean isHeader, 
+   public QuranRow(String text, String metadata, int rowType, 
          int sura, int page, Integer imageResource){
-      this(text, metadata, isHeader, sura, 0, page, imageResource);
+      this(text, metadata, rowType, sura, 0, page, imageResource);
+   }
+   
+   public QuranRow(String text, String metadata, 
+         int sura, int page, Integer imageResource){
+      this(text, metadata, NONE, sura, 0, page, imageResource);
+   }
+   
+   public boolean isHeader() {
+      return rowType == HEADER;
+   }
+   
+   public boolean isBookmark() {
+      return rowType == PAGE_BOOKMARK || rowType == AYAH_BOOKMARK;
+   }
+   
+   public boolean isPageBookmark() {
+      return rowType == PAGE_BOOKMARK;
+   }
+   
+   public boolean isAyahBookmark() {
+      return rowType == AYAH_BOOKMARK;
    }
 }
