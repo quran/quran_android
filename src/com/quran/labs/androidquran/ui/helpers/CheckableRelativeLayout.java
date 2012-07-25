4e6d190c821e09f5092abb8bd8cecc06ae37e684
ahmedre <ahmedre@gmail.com>
2012-07-25 01:55:34 -0700

    improve performance of checkable relative layout

--- src/com/quran/labs/androidquran/ui/helpers/CheckableRelativeLayout.java
+++ src/com/quran/labs/androidquran/ui/helpers/CheckableRelativeLayout.java
@@ -12,69 +12,32 @@ import android.widget.RelativeLayout;
 
 public class CheckableRelativeLayout extends RelativeLayout implements Checkable {
 
-   private boolean isChecked;
-   private List<Checkable> checkableViews;
-   
-   public CheckableRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
-      super(context, attrs, defStyle);
-      this.isChecked = false;
-      this.checkableViews = new ArrayList<Checkable>();
+   private boolean mIsChecked = false;
+
+   public CheckableRelativeLayout(Context context) {
+      super(context);
    }
 
    public CheckableRelativeLayout(Context context, AttributeSet attrs) {
       super(context, attrs);
-      this.isChecked = false;
-      this.checkableViews = new ArrayList<Checkable>();
    }
 
-   public CheckableRelativeLayout(Context context, int checkableId) {
-      super(context);
-      this.isChecked = false;
-      this.checkableViews = new ArrayList<Checkable>();
+   public CheckableRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+      super(context, attrs, defStyle);
    }
 
    @Override
    public void setChecked(boolean checked) {
-      this.isChecked = checked;
-      for (Checkable c : checkableViews) {
-         c.setChecked(checked);
-      }
+      mIsChecked = checked;
    }
 
    @Override
    public boolean isChecked() {
-      return isChecked;
+      return mIsChecked;
    }
 
    @Override
    public void toggle() {
-      this.isChecked = !this.isChecked;
-      for (Checkable c : checkableViews) {
-         c.toggle();
-      }
-   }
-   
-   @Override
-   protected void onFinishInflate() {
-      super.onFinishInflate();
-
-      final int childCount = this.getChildCount();
-      for (int i = 0; i < childCount; ++i) {
-         findCheckableChildren(this.getChildAt(i));
-      }
-   }
-
-   private void findCheckableChildren(View v) {
-      if (v instanceof Checkable) {
-         this.checkableViews.add((Checkable) v);
-      }
-
-      if (v instanceof ViewGroup) {
-         final ViewGroup vg = (ViewGroup) v;
-         final int childCount = vg.getChildCount();
-         for (int i = 0; i < childCount; ++i) {
-            findCheckableChildren(vg.getChildAt(i));
-         }
-      }
+      mIsChecked = !mIsChecked;
    }
 }
