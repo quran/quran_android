c08dc1e06c5611bf8538ab935e24d479f886680d
Ahmed Farra <ahmedmfarra@gmail.com>
2012-07-25 03:08:37 -0400

    Change bookmark removing to use Contextual Action Bar instead of dialog.

--- src/com/quran/labs/androidquran/ui/fragment/BookmarksFragment.java
+++ src/com/quran/labs/androidquran/ui/fragment/BookmarksFragment.java
@@ -1,14 +1,14 @@
 package com.quran.labs.androidquran.ui.fragment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +19,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.ActionMode;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.quran.labs.androidquran.R;
 import com.quran.labs.androidquran.data.Constants;
 import com.quran.labs.androidquran.data.QuranInfo;
@@ -31,6 +35,7 @@ import com.quran.labs.androidquran.ui.helpers.QuranRow;
 public class BookmarksFragment extends SherlockFragment {
 
    private ListView mListView;
+   private ActionMode mMode;
    private QuranListAdapter mAdapter;
    private TextView mEmptyTextView;
    private AsyncTask<Void, Void, QuranRow[]> loadingTask = null;
@@ -51,11 +56,26 @@ public class BookmarksFragment extends SherlockFragment {
             R.layout.index_sura_row, new QuranRow[]{});
       mListView.setAdapter(mAdapter);
       
+      mMode = null;
+      mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+      mListView.setItemsCanFocus(false);
+      
       mListView.setOnItemClickListener(new OnItemClickListener(){
          public void onItemClick(AdapterView<?> parent, View v,
                int position, long id){
             QuranRow elem = (QuranRow)mAdapter.getItem((int)id);
-            if (!elem.isHeader()){
+            
+            // If we're not in CAB mode or the element shouldn't
+            // be checked then undo the checking
+            if (mMode == null || !elem.isBookmark())
+               mListView.setItemChecked(position, false);
+            
+            // If we're in CAB mode don't handle the click
+            if (mMode != null)
+               return;
+            
+            // We're not in CAB mode so handle the click normally
+            if (!elem.isHeader()) {
                if (elem.isAyahBookmark())
                   ((QuranActivity)getActivity()).jumpToAndHighlight(
                         elem.page, elem.sura, elem.ayah);
@@ -69,28 +89,31 @@ public class BookmarksFragment extends SherlockFragment {
          public boolean onItemLongClick(AdapterView<?> parent, View view,
                int position, long id) {
             final QuranRow elem = (QuranRow)mAdapter.getItem((int)id);
-            if (elem.isBookmark()){
-               final Activity activity = getActivity();
-               if (activity == null){ return false; }
-               
-               int[] optionIds = {R.string.remove_bookmark};
-               CharSequence[] options = new CharSequence[optionIds.length];
-               for (int i=0; i<optionIds.length; i++){
-                  options[i] = activity.getString(optionIds[i]);
-               }
-               AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-               builder.setItems(options, new DialogInterface.OnClickListener() {
-                  public void onClick(DialogInterface dialog, int selection) {
-                     if (selection == 0) {
-                        new RemoveBookmarkTask().execute(elem);
-                     }
-                  }
-               });
-               AlertDialog dlg = builder.create();
-               dlg.show();
-               return true;
+            
+            // Activate CAB only for bookmarks
+            if (!elem.isBookmark())
+               return false;
+            
+            // Long click toggles checked state
+            boolean itemIsChecked = mListView.isItemChecked(position);
+            mListView.setItemChecked(position, !itemIsChecked);
+            
+            // If no more rows are selected, finish the CAB
+            boolean hasCheckedElement = false;
+            SparseBooleanArray checked = mListView.getCheckedItemPositions();
+            for (int i = 0; i < checked.size() && !hasCheckedElement; i++) {
+               hasCheckedElement = checked.valueAt(i);
             }
-            return false;
+            if (hasCheckedElement) {
+               if (mMode == null) {
+                  mMode = getSherlockActivity().startActionMode(new ModeCallback());
+               }
+            } else {
+               if (mMode != null) {
+                  mMode.finish();
+               }
+            }
+            return true;
          }
       });
       
@@ -113,18 +136,79 @@ public class BookmarksFragment extends SherlockFragment {
          loadingTask.cancel(true);
       }
       loadingTask = null;
+      // If currently in CAB mode, end it
+      if (mMode != null) {
+         mMode.finish();
+      }
    }
 
+   private class ModeCallback implements ActionMode.Callback {
+
+      @Override
+      public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+         MenuInflater inflater = getSherlockActivity().getSupportMenuInflater();
+         inflater.inflate(R.menu.bookmark_menu, menu);
+         return true;
+      }
+
+      @Override
+      public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+         return false;
+      }
+
+      @Override
+      public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+         switch (item.getItemId()) {
+         case R.id.cab_remove_bookmark:
+            List<QuranRow> rows = new ArrayList<QuranRow>();
+            SparseBooleanArray sel = mListView.getCheckedItemPositions();
+            for (int i = 0; i < sel.size(); i++) {
+               if (sel.valueAt(i)) {
+                  QuranRow elem = (QuranRow)mAdapter.getItem(sel.keyAt(i));
+                  if (elem.isBookmark())
+                     rows.add(elem);
+               }
+            }
+            QuranRow[] rowsArr = rows.toArray(new QuranRow[rows.size()]);
+            new RemoveBookmarkTask().execute(rowsArr);
+            return true;
+         case R.id.cab_select_all_bookmarks:
+            for (int i = 0; i < mAdapter.getCount(); i++) {
+               QuranRow elem = (QuranRow)mAdapter.getItem(i);
+               if (elem.isBookmark()) {
+                  mListView.setItemChecked(i, true);
+               }
+            }
+            return true;
+         default:
+            return false;
+         }
+      }
+
+      @Override
+      public void onDestroyActionMode(ActionMode mode) {
+         for (int i = 0; i < mAdapter.getCount(); i++)
+            mListView.setItemChecked(i, false);
+         if (mode == mMode)
+            mMode = null;
+      }
+      
+   }
+   
    private class RemoveBookmarkTask extends AsyncTask<QuranRow, Void, Void> {
       @Override
       protected Void doInBackground(QuranRow... params) {
-         QuranRow elem = params[0];
          BookmarksDBAdapter db = new BookmarksDBAdapter(getActivity());
          db.open();
-         if (elem.isPageBookmark())
-            db.togglePageBookmark(elem.page);
-         else if (elem.isAyahBookmark())
-            db.toggleAyahBookmark(elem.page, elem.sura, elem.ayah);
+         for (int i = 0; i < params.length; i++) {
+            QuranRow elem = params[i];
+            // Ideally should have a method for setting bookmark rather than toggling,
+            // but don't want to break anything until after Ramadan :)
+            if (elem.isPageBookmark())
+               db.togglePageBookmark(elem.page);
+            else if (elem.isAyahBookmark())
+               db.toggleAyahBookmark(elem.page, elem.sura, elem.ayah);
+         }
          db.close();
          return null;
       }
@@ -146,6 +230,13 @@ public class BookmarksFragment extends SherlockFragment {
 
       @Override
       protected void onPostExecute(QuranRow[] result) {
+         // If in CAB mode, wait until we reach here before finishing it to make
+         // the UI seem smoother. Otherwise, on slow devices, it closes the CAB,
+         // then a bit later the list refreshes and the removed bookmarks disappear.
+         // Maybe have an indeterminate progress bar instead.
+         if (mMode != null)
+            mMode.finish();
+         
          if (result.length == 0){
             mEmptyTextView.setText(R.string.no_bookmarks);
          }
