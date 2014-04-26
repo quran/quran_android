92c7b62d65052e061e57d45ff99d79ead32071b7
Ahmed Farra <ahmed@planleaf.org>
2014-04-25 21:15:40 -0400

    Change HighlightType from enum to class (so we can create new highlight types from tags)

--- app/src/main/java/com/quran/labs/androidquran/ui/helpers/HighlightType.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/helpers/HighlightType.java
@@ -3,18 +3,24 @@ package com.quran.labs.androidquran.ui.helpers;
 import android.graphics.Color;
 import android.graphics.Paint;
 
-public enum HighlightType {
+public class HighlightType implements Comparable<HighlightType> {
 
-  // Declaration order determines highlighting priority (first is highest)
-  SELECTION (true,  Color.argb(64, 70,  148, 166)), //#404694A6 (blue)
-  AUDIO     (true,  Color.argb(64, 70,  166, 70)),  //#4046A646 (green)
-  NOTE      (true,  Color.argb(64, 235, 235, 33)),  //#40EBEB21 (yellow)
-  BOOKMARK  (false, Color.argb(64, 164, 164, 164)); //#40A4A4A4 (gray)
+  private static final int BLUE =   Color.argb(64, 70,  148, 166);  //#404694A6
+  private static final int GREEN =  Color.argb(64, 70,  166, 70);   //#4046A646
+  private static final int YELLOW = Color.argb(64, 235, 235, 33);   //#40EBEB21
+  private static final int GRAY =   Color.argb(64, 164, 164, 164);  //#40A4A4A4
 
+  public static final HighlightType SELECTION = new HighlightType(1, true,  BLUE);
+  public static final HighlightType AUDIO =     new HighlightType(2, true,  GREEN);
+  public static final HighlightType NOTE =      new HighlightType(3, true,  YELLOW);
+  public static final HighlightType BOOKMARK =  new HighlightType(4, false, GRAY);
+
+  private Long mId;
   private boolean mUnique;
   private Paint mPaint;
 
-  private HighlightType(boolean unique, int color) {
+  private HighlightType(long id, boolean unique, int color) {
+    mId = id;
     mUnique = unique;
     mPaint = new Paint();
     mPaint.setColor(color);
@@ -28,7 +34,20 @@ public enum HighlightType {
     return mPaint;
   }
 
-  public long getId() {
-    return this.ordinal();
+  @Override
+  public int compareTo(HighlightType another) {
+    return mId.compareTo(another.mId);
   }
+
+  @Override
+  public boolean equals(Object o) {
+    return this == o || o != null && o.getClass() == HighlightType.class &&
+        mId.equals(((HighlightType) o).mId);
+  }
+
+  @Override
+  public int hashCode() {
+    return mId.hashCode();
+  }
+
 }
