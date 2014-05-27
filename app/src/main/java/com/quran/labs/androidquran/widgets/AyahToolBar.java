4b7a6c6ca46fdd70d43da86cb78e1fd8c4c33c09
ahmedre <ahmedre@gmail.com>
2014-05-26 23:14:39 -0700

    Favor using submenus over using two separate menus.

--- app/src/main/java/com/quran/labs/androidquran/widgets/AyahToolBar.java
+++ app/src/main/java/com/quran/labs/androidquran/widgets/AyahToolBar.java
@@ -20,12 +20,11 @@ import android.widget.Toast;
 
 public class AyahToolBar extends LinearLayout implements
     View.OnClickListener, View.OnLongClickListener {
-  private Menu mMenu;
   private Context mContext;
+  private Menu mMenu;
+  private Menu mCurrentMenu;
   private int mItemWidth;
-  private int mCurrentMenu;
   private OnItemSelectedListener mItemSelectedListener;
-  private boolean mBookmarked;
 
   public interface OnItemSelectedListener {
     void onItemSelected(int itemId);
@@ -52,24 +51,30 @@ public class AyahToolBar extends LinearLayout implements
     final Resources resources = context.getResources();
     mItemWidth = resources.getDimensionPixelSize(R.dimen.toolbar_item_width);
     setOrientation(LinearLayout.HORIZONTAL);
-    showMenu(R.menu.ayah_menu);
-  }
 
-  private void showMenu(int menuId) {
     mMenu = new MenuBuilder(mContext);
     final MenuInflater inflater = new MenuInflater(mContext);
-    inflater.inflate(menuId, mMenu);
-    mCurrentMenu = menuId;
+    inflater.inflate(R.menu.ayah_menu, mMenu);
+    showMenu(mMenu);
+  }
+
+  private void showMenu(Menu menu) {
+    if (mCurrentMenu == menu) {
+      // no need to re-draw
+      return;
+    }
 
     removeAllViews();
-    final int count = mMenu.size();
+    final int count = menu.size();
     for (int i=0; i<count; i++) {
-      final MenuItem item = mMenu.getItem(i);
+      final MenuItem item = menu.getItem(i);
       if (item.isVisible()) {
         final View view = getMenuItemView(item);
         addView(view);
       }
     }
+
+    mCurrentMenu = menu;
   }
 
   private View getMenuItemView(MenuItem item) {
@@ -85,19 +90,19 @@ public class AyahToolBar extends LinearLayout implements
   }
 
   public int getToolBarWidth() {
-    final int width = getWidth();
-    return width > 0 ? width : mMenu.size() * mItemWidth;
+    // relying on getWidth() may give us the width of a shorter
+    // submenu instead of the actual menu
+    return mMenu.size() * mItemWidth;
   }
 
   public void setBookmarked(boolean bookmarked) {
-    mBookmarked = bookmarked;
-    if (mCurrentMenu == R.menu.ayah_menu) {
-      MenuItem bookmarkItem = mMenu.findItem(R.id.cab_bookmark_ayah);
-      bookmarkItem.setIcon(bookmarked ? R.drawable.favorite : R.drawable.not_favorite);
-      ImageButton bookmarkButton = (ImageButton) findViewById(R.id.cab_bookmark_ayah);
-      if (bookmarkButton != null) {
-        bookmarkButton.setImageDrawable(bookmarkItem.getIcon());
-      }
+    MenuItem bookmarkItem = mMenu.findItem(R.id.cab_bookmark_ayah);
+    bookmarkItem.setIcon(bookmarked ?
+        R.drawable.favorite : R.drawable.not_favorite);
+    ImageButton bookmarkButton =
+        (ImageButton) findViewById(R.id.cab_bookmark_ayah);
+    if (bookmarkButton != null) {
+      bookmarkButton.setImageDrawable(bookmarkItem.getIcon());
     }
   }
 
@@ -111,9 +116,7 @@ public class AyahToolBar extends LinearLayout implements
   }
 
   public void showMenu() {
-    if (mCurrentMenu != R.menu.ayah_menu) {
-      showMenu(R.menu.ayah_menu);
-    }
+    showMenu(mMenu);
     this.setVisibility(VISIBLE);
   }
 
@@ -128,8 +131,12 @@ public class AyahToolBar extends LinearLayout implements
   @Override
   public void onClick(View v) {
     if (v.getId() == R.id.cab_share_ayah) {
-      showMenu(R.menu.ayah_menu_share);
+      final MenuItem item = mMenu.findItem(R.id.cab_share_ayah);
+      if (item != null && item.hasSubMenu()) {
+        showMenu(item.getSubMenu());
+      }
     }
+
     if (mItemSelectedListener != null) {
       mItemSelectedListener.onItemSelected(v.getId());
     }
@@ -139,10 +146,9 @@ public class AyahToolBar extends LinearLayout implements
   public boolean onLongClick(View v) {
     MenuItem item = mMenu.findItem(v.getId());
     if (item != null && item.getTitle() != null) {
-      Toast.makeText(this.getContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+      Toast.makeText(mContext, item.getTitle(), Toast.LENGTH_SHORT).show();
       return true;
     }
     return false;
   }
-
 }
