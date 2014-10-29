e9689f59b600cb44d147f293a0b62181746a1189
ahmedre <ahmedre@gmail.com>
2014-10-28 22:48:48 -0700

    Update build, target sdk 21, enable java 1.7.
    - update to gradle 2.1
    - update to the latest android plugin
    - upgrade crashlytics to fabric
    - enable java 1.7
    - set the target sdk to L
    - update support v4 to v21.
    - use explicit service intents.

--- app/src/main/java/com/quran/labs/androidquran/data/Constants.java
+++ app/src/main/java/com/quran/labs/androidquran/data/Constants.java
@@ -1,11 +1,6 @@
 package com.quran.labs.androidquran.data;
 
-import com.quran.labs.androidquran.BuildConfig;
-
 public class Constants {
-   public static final boolean CRASH_REPORTING_ENABLED =
-       BuildConfig.LOG_CRASHES;
-
    // Numerics
    public static final int DEFAULT_NIGHT_MODE_TEXT_BRIGHTNESS = 255;
    public static final int DEFAULT_TEXT_SIZE = 15;
