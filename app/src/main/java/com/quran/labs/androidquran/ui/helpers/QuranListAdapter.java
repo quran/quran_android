2c6e4a1a87b78ce0d7e11b27e6cae0d5fc509aae
ahmedre <ahmedre@gmail.com>
2014-12-02 23:44:40 -0800

    Initial material updates for the index.

--- app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranListAdapter.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranListAdapter.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -54,23 +55,24 @@ public class QuranListAdapter extends BaseAdapter {
    }
 
    public View getView(final int position, View convertView, ViewGroup parent) {
-      ViewHolder holder;
+      if (getItemViewType(position) == 0) {
+         return getHeaderView(position, convertView, parent);
+      }
 
+      ViewHolder holder;
       if (convertView == null) {
          convertView = mInflater.inflate(mLayout, parent, false);
          holder = new ViewHolder();
-         holder.text = (TextView)convertView.findViewById(R.id.suraName);
-         holder.metadata = (TextView)convertView.findViewById(R.id.suraDetails);
-         holder.page = (TextView)convertView.findViewById(R.id.pageNumber);
-         holder.number = (TextView)convertView.findViewById(R.id.suraNumber);
-         holder.header = (TextView)convertView.findViewById(R.id.headerName);
-         holder.image = (TextView)convertView.findViewById(R.id.rowIcon);
+         holder.text = (TextView) convertView.findViewById(R.id.suraName);
+         holder.metadata = (TextView) convertView.findViewById(R.id.suraDetails);
+         holder.page = (TextView) convertView.findViewById(R.id.pageNumber);
+         holder.number = (TextView) convertView.findViewById(R.id.suraNumber);
+         holder.image = (ImageView) convertView.findViewById(R.id.rowIcon);
 
          if (mUseArabicFont){
             Typeface typeface = ArabicStyle.getTypeface(mContext);
             holder.text.setTypeface(typeface);
             holder.metadata.setTypeface(typeface);
-            holder.header.setTypeface(typeface);
          }
          convertView.setTag(holder);
       }
@@ -84,41 +86,25 @@ public class QuranListAdapter extends BaseAdapter {
 
       holder.page.setText(QuranUtils.getLocalizedNumber(mContext, item.page));
       holder.text.setText(text);
-      holder.header.setText(text);
       holder.number.setText(
               QuranUtils.getLocalizedNumber(mContext, item.sura));
 
-      int color = R.color.sura_details_color;
-      if (mElements[position].isHeader()){
-         holder.text.setVisibility(View.GONE);
-         holder.header.setVisibility(View.VISIBLE);
-         holder.metadata.setVisibility(View.GONE);
-         holder.number.setVisibility(View.GONE);
+      String info = item.metadata;
+      holder.metadata.setVisibility(View.VISIBLE);
+      holder.text.setVisibility(View.VISIBLE);
+      holder.metadata.setText(ArabicStyle.reshape(mContext, info));
+
+      if (item.imageResource == null){
+         holder.number.setVisibility(View.VISIBLE);
          holder.image.setVisibility(View.GONE);
-         color = R.color.header_text_color;
       }
       else {
-         String info = item.metadata;
-         holder.metadata.setVisibility(View.VISIBLE);
-         holder.text.setVisibility(View.VISIBLE);
-         holder.header.setVisibility(View.GONE);
-         holder.metadata.setText(ArabicStyle.reshape(mContext, info));
-
-         if (item.imageResource == null){
-            holder.number.setVisibility(View.VISIBLE);
-            holder.image.setVisibility(View.GONE);
-         }
-         else {
-            holder.image.setBackgroundResource(item.imageResource);
-            holder.image.setText(item.imageText);
-            holder.image.setVisibility(View.VISIBLE);
-            holder.number.setVisibility(View.GONE);
-         }
+         holder.image.setImageResource(item.imageResource);
+         //holder.image.setText(item.imageText);
+         holder.image.setVisibility(View.VISIBLE);
+         holder.number.setVisibility(View.GONE);
       }
-      holder.page.setTextColor(mContext.getResources().getColor(color));
-      int pageVisibility = item.page == 0? View.GONE : View.VISIBLE;
-      holder.page.setVisibility(pageVisibility);
-      
+
       // If the row is checked (for CAB mode), theme its bg appropriately
       if (parent != null && parent instanceof ListView){
          int background = ((ListView) parent).isItemChecked(position)?
@@ -129,7 +115,51 @@ public class QuranListAdapter extends BaseAdapter {
       return convertView;
    }
 
-  @Override
+   private View getHeaderView(int pos, View convertView, ViewGroup parent) {
+      HeaderHolder holder;
+      if (convertView == null) {
+         convertView = mInflater.inflate(
+             R.layout.index_header_row, parent, false);
+         holder = new HeaderHolder();
+         holder.header = (TextView)convertView.findViewById(R.id.title);
+         holder.pageNumber = (TextView)convertView.findViewById(R.id.count);
+
+         if (mUseArabicFont){
+            Typeface typeface = ArabicStyle.getTypeface(mContext);
+            holder.header.setTypeface(typeface);
+         }
+         convertView.setTag(holder);
+      }
+      else { holder = (HeaderHolder) convertView.getTag(); }
+
+      final QuranRow item = mElements[pos];
+      String text = item.text;
+      if (mReshapeArabic){
+         text = ArabicStyle.reshape(mContext, text);
+      }
+
+      holder.header.setText(text);
+      if (item.page == 0) {
+         holder.pageNumber.setVisibility(View.GONE);
+      } else {
+         holder.pageNumber.setVisibility(View.VISIBLE);
+         holder.pageNumber.setText(
+             QuranUtils.getLocalizedNumber(mContext, item.page));
+      }
+      return convertView;
+   }
+
+   @Override
+   public int getViewTypeCount() {
+      return 2;
+   }
+
+   @Override
+   public int getItemViewType(int position) {
+      return mElements[position].isHeader() ? 0 : 1;
+   }
+
+   @Override
   public boolean isEnabled(int position) {
     final QuranRow selected = mElements[position];
     return mSelectableHeaders || selected.isBookmark() ||
@@ -137,12 +167,16 @@ public class QuranListAdapter extends BaseAdapter {
         (selected.isBookmarkHeader() && selected.tagId >= 0);
   }
 
+   class HeaderHolder {
+      TextView header;
+      TextView pageNumber;
+   }
+
   class ViewHolder {
       TextView text;
       TextView page;
       TextView number;
       TextView metadata;
-      TextView header;
-      TextView image;
+      ImageView image;
    }
 }
