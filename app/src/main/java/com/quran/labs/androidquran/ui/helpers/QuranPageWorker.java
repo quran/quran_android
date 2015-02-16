3b23e7b64f238cf3b366f654cb5eafbf6a017955
ahmedre <ahmedre@gmail.com>
2015-02-15 17:09:54 -0800

    Remove the LruCache for images.
    We used to maintain a cache of the recently used images. In reality,
    this was only useful if you moved backwards to the past few pages.
    Since this is a rare use case, and since the memory usage is high,
    and since the number of cached pages would typically be 2 or 3,
    removed this cache for now.
    
    In the future, we should introduce a pool of bitmap objects and use
    inBitmap to load images.

--- app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranPageWorker.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranPageWorker.java
@@ -1,12 +1,9 @@
 package com.quran.labs.androidquran.ui.helpers;
 
-import com.crashlytics.android.Crashlytics;
 import com.quran.labs.androidquran.common.Response;
 import com.quran.labs.androidquran.util.QuranExecutorService;
 import com.quran.labs.androidquran.util.QuranPageTask;
 
-import android.annotation.TargetApi;
-import android.app.ActivityManager;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -14,32 +11,31 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v4.util.LruCache;
-import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
 public class QuranPageWorker {
-  private static final String TAG = "QuranPageWorker";
   private static final int MSG_IMAGE_LOADED = 1;
 
   private volatile static QuranPageWorker sInstance;
 
   private Context mContext;
   private Resources mResources;
-  private LruCache<String, BitmapDrawable> mMemoryCache;
 
   private static final ExecutorService sExecutorService =
       new QuranExecutorService();
-  private static final Handler sHandler = new Handler() {
+
+  private static class WorkerHandler extends Handler {
     @Override
     public void handleMessage(Message msg) {
       if (msg.what == MSG_IMAGE_LOADED && sInstance != null) {
         sInstance.onImageLoaded((QuranPageTask.QuranTaskData) msg.obj);
       }
     }
-  };
+  }
+
+  private static final Handler sHandler = new WorkerHandler();
 
   public static synchronized QuranPageWorker getInstance(Context context) {
     if (sInstance == null) {
@@ -57,83 +53,12 @@ public class QuranPageWorker {
   private QuranPageWorker(Context context) {
     mContext = context.getApplicationContext();
     mResources = mContext.getResources();
-
-    final int memClass = ((ActivityManager) context.getSystemService(
-        Context.ACTIVITY_SERVICE)).getMemoryClass();
-    final int cacheSize = 1024 * 1024 * memClass / 8;
-    final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-    Crashlytics.log(Log.DEBUG, TAG, "memory class: " + memClass +
-        ", cache size: " + cacheSize + ", max memory: " + maxMemory);
-    mMemoryCache = new LruCache<String, BitmapDrawable>(cacheSize) {
-      @Override
-      protected void entryRemoved(boolean evicted, String key,
-          BitmapDrawable oldValue,
-          BitmapDrawable newValue) {
-        if (RecyclingBitmapDrawable.class.isInstance(oldValue)) {
-          ((RecyclingBitmapDrawable) oldValue).setIsCached(false);
-        }
-      }
-
-      @Override
-      protected int sizeOf(String key, BitmapDrawable bitmapDrawable) {
-        Bitmap bitmap = bitmapDrawable.getBitmap();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-          return getBitmapSizeKitkat(bitmap);
-        } else if (Build.VERSION.SDK_INT >=
-            Build.VERSION_CODES.HONEYCOMB_MR1) {
-          return getBitmapSizeHoneycombMr1(bitmap);
-        }
-
-        Crashlytics.log(Log.DEBUG, TAG, "row bytes: " +
-            bitmap.getRowBytes() + ", height: " +
-            bitmap.getHeight() + ", " + (bitmap.getRowBytes() *
-            bitmap.getHeight()));
-        return bitmap.getRowBytes() * bitmap.getHeight();
-      }
-    };
-
-    Crashlytics.log(Log.DEBUG, TAG,
-        "initial LruCache size: " + (memClass / 8));
-  }
-
-  @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-  private int getBitmapSizeHoneycombMr1(Bitmap bitmap) {
-    return bitmap.getByteCount();
-  }
-
-  @TargetApi(Build.VERSION_CODES.KITKAT)
-  private int getBitmapSizeKitkat(Bitmap bitmap) {
-    return bitmap.getAllocationByteCount();
-  }
-
-  private void addBitmapToCache(String key, BitmapDrawable drawable) {
-    if (drawable != null && getBitmapFromCache(key) == null) {
-      if (RecyclingBitmapDrawable.class.isInstance(drawable)) {
-        ((RecyclingBitmapDrawable) drawable).setIsCached(true);
-      }
-      mMemoryCache.put(key, drawable);
-      Crashlytics.log(Log.DEBUG, TAG, "cache size: " + mMemoryCache.size());
-    }
-
-    Crashlytics.log(Log.DEBUG, TAG, "cache: number of puts: " +
-        mMemoryCache.putCount() +
-        ", number of evicts: " + mMemoryCache.evictionCount());
-  }
-
-  private BitmapDrawable getBitmapFromCache(String key) {
-    return mMemoryCache.get(key);
   }
 
   public Future<?> loadPage(String widthParam, int page, AyahTracker tracker) {
-    final BitmapDrawable drawable = getBitmapFromCache(page + widthParam);
-    if (drawable != null) {
-      tracker.onLoadImageResponse(drawable, Response.fromPage(page));
-      return null;
-    } else {
-      QuranPageTask task = new QuranPageTask(mContext,
-          widthParam, tracker, page);
-      return sExecutorService.submit(task);
-    }
+    QuranPageTask task = new QuranPageTask(mContext,
+        widthParam, tracker, page);
+    return sExecutorService.submit(task);
   }
 
   // once complete, see if ImageView is still around and set bitmap.
@@ -148,9 +73,6 @@ public class QuranPageWorker {
         } else {
           drawable = new RecyclingBitmapDrawable(mResources, bitmap);
         }
-
-        addBitmapToCache(response.getPageNumber() +
-            response.getWidthParam(), drawable);
       }
     }
 
