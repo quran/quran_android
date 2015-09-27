3fef95f50afcebfe3d5d7388f6e77c4053967c1d
ahmedre <ahmedre@gmail.com>
2015-09-27 13:53:34 -0700

    Fix indentation on QuranSettings.

--- app/src/main/java/com/quran/labs/androidquran/util/QuranSettings.java
+++ app/src/main/java/com/quran/labs/androidquran/util/QuranSettings.java
@@ -10,106 +10,109 @@ import android.support.annotation.NonNull;
 
 
 public class QuranSettings {
-   private static QuranSettings sInstance;
-   private SharedPreferences mPrefs;
 
-   public static synchronized QuranSettings getInstance(@NonNull Context context) {
-     if (sInstance == null) {
-       sInstance = new QuranSettings(context.getApplicationContext());
-     }
-     return sInstance;
-   }
+  private static QuranSettings sInstance;
+  private SharedPreferences mPrefs;
 
-   private QuranSettings(@NonNull Context appContext) {
-     mPrefs = PreferenceManager.getDefaultSharedPreferences(appContext);
-   }
+  public static synchronized QuranSettings getInstance(@NonNull Context context) {
+    if (sInstance == null) {
+      sInstance = new QuranSettings(context.getApplicationContext());
+    }
+    return sInstance;
+  }
 
-   public boolean isArabicNames(){
-      return mPrefs.getBoolean(Constants.PREF_USE_ARABIC_NAMES, false);
-   }
+  private QuranSettings(@NonNull Context appContext) {
+    mPrefs = PreferenceManager.getDefaultSharedPreferences(appContext);
+  }
 
-   public boolean isLockOrientation(){
-      return mPrefs.getBoolean(Constants.PREF_LOCK_ORIENTATION, false);
-   }
+  public boolean isArabicNames() {
+    return mPrefs.getBoolean(Constants.PREF_USE_ARABIC_NAMES, false);
+  }
 
-   public boolean isLandscapeOrientation(){
-      return mPrefs.getBoolean(Constants.PREF_LANDSCAPE_ORIENTATION, false);
-   }
+  public boolean isLockOrientation() {
+    return mPrefs.getBoolean(Constants.PREF_LOCK_ORIENTATION, false);
+  }
 
-   public boolean shouldStream(){
-      return mPrefs.getBoolean(Constants.PREF_PREFER_STREAMING, false);
-   }
+  public boolean isLandscapeOrientation() {
+    return mPrefs.getBoolean(Constants.PREF_LANDSCAPE_ORIENTATION, false);
+  }
 
-   public boolean isNightMode(){
-      return mPrefs.getBoolean(Constants.PREF_NIGHT_MODE, false);
-   }
+  public boolean shouldStream() {
+    return mPrefs.getBoolean(Constants.PREF_PREFER_STREAMING, false);
+  }
 
-   public boolean useNewBackground() {
-     return mPrefs.getBoolean(Constants.PREF_USE_NEW_BACKGROUND, true);
-   }
+  public boolean isNightMode() {
+    return mPrefs.getBoolean(Constants.PREF_NIGHT_MODE, false);
+  }
 
-   public boolean highlightBookmarks() {
-     return mPrefs.getBoolean(Constants.PREF_HIGHLIGHT_BOOKMARKS, true);
-   }
+  public boolean useNewBackground() {
+    return mPrefs.getBoolean(Constants.PREF_USE_NEW_BACKGROUND, true);
+  }
 
-   public int getNightModeTextBrightness(){
-      return mPrefs.getInt(Constants.PREF_NIGHT_MODE_TEXT_BRIGHTNESS,
-              Constants.DEFAULT_NIGHT_MODE_TEXT_BRIGHTNESS);
-   }
+  public boolean highlightBookmarks() {
+    return mPrefs.getBoolean(Constants.PREF_HIGHLIGHT_BOOKMARKS, true);
+  }
 
-   public boolean shouldOverlayPageInfo(){
-      return mPrefs.getBoolean(Constants.PREF_OVERLAY_PAGE_INFO, true);
-   }
+  public int getNightModeTextBrightness() {
+    return mPrefs.getInt(Constants.PREF_NIGHT_MODE_TEXT_BRIGHTNESS,
+        Constants.DEFAULT_NIGHT_MODE_TEXT_BRIGHTNESS);
+  }
 
-   public boolean shouldDisplayMarkerPopup(){
-      return mPrefs.getBoolean(Constants.PREF_DISPLAY_MARKER_POPUP, true);
-   }
+  public boolean shouldOverlayPageInfo() {
+    return mPrefs.getBoolean(Constants.PREF_OVERLAY_PAGE_INFO, true);
+  }
 
-   public boolean shouldHighlightBookmarks(){
-      return mPrefs.getBoolean(Constants.PREF_HIGHLIGHT_BOOKMARKS, true);
-   }
+  public boolean shouldDisplayMarkerPopup() {
+    return mPrefs.getBoolean(Constants.PREF_DISPLAY_MARKER_POPUP, true);
+  }
 
-   public boolean wantArabicInTranslationView(){
-      return mPrefs.getBoolean(Constants.PREF_AYAH_BEFORE_TRANSLATION, true);
-   }
+  public boolean shouldHighlightBookmarks() {
+    return mPrefs.getBoolean(Constants.PREF_HIGHLIGHT_BOOKMARKS, true);
+  }
 
-   public int getPreferredDownloadAmount(){
-      String str = mPrefs.getString(Constants.PREF_DOWNLOAD_AMOUNT,
-                  "" + AudioUtils.LookAheadAmount.PAGE);
-      int val = AudioUtils.LookAheadAmount.PAGE;
-      try { val = Integer.parseInt(str); }
-      catch (Exception e){}
+  public boolean wantArabicInTranslationView() {
+    return mPrefs.getBoolean(Constants.PREF_AYAH_BEFORE_TRANSLATION, true);
+  }
 
-      if (val > AudioUtils.LookAheadAmount.MAX ||
-              val < AudioUtils.LookAheadAmount.MIN){
-         return AudioUtils.LookAheadAmount.PAGE;
-      }
-      return val;
-   }
+  public int getPreferredDownloadAmount() {
+    String str = mPrefs.getString(Constants.PREF_DOWNLOAD_AMOUNT,
+        "" + AudioUtils.LookAheadAmount.PAGE);
+    int val = AudioUtils.LookAheadAmount.PAGE;
+    try {
+      val = Integer.parseInt(str);
+    } catch (Exception e) {
+    }
 
-   public int getTranslationTextSize(){
-      return mPrefs.getInt(Constants.PREF_TRANSLATION_TEXT_SIZE,
-              Constants.DEFAULT_TEXT_SIZE);
-   }
+    if (val > AudioUtils.LookAheadAmount.MAX ||
+        val < AudioUtils.LookAheadAmount.MIN) {
+      return AudioUtils.LookAheadAmount.PAGE;
+    }
+    return val;
+  }
 
-  public int getLastPage(){
+  public int getTranslationTextSize() {
+    return mPrefs.getInt(Constants.PREF_TRANSLATION_TEXT_SIZE,
+        Constants.DEFAULT_TEXT_SIZE);
+  }
+
+  public int getLastPage() {
     return mPrefs.getInt(Constants.PREF_LAST_PAGE, Constants.NO_PAGE_SAVED);
   }
 
-   public void setLastPage(int page){
-      mPrefs.edit().putInt(Constants.PREF_LAST_PAGE, page).apply();
-   }
+  public void setLastPage(int page) {
+    mPrefs.edit().putInt(Constants.PREF_LAST_PAGE, page).apply();
+  }
 
-   public String getAppCustomLocation() {
-      return mPrefs.getString(Constants.PREF_APP_LOCATION,
-            Environment.getExternalStorageDirectory().getAbsolutePath());
-   }
+  public String getAppCustomLocation() {
+    return mPrefs.getString(Constants.PREF_APP_LOCATION,
+        Environment.getExternalStorageDirectory().getAbsolutePath());
+  }
 
-   public void setAppCustomLocation(String newLocation) {
-       mPrefs.edit().putString(Constants.PREF_APP_LOCATION, newLocation).apply();
-   }
+  public void setAppCustomLocation(String newLocation) {
+    mPrefs.edit().putString(Constants.PREF_APP_LOCATION, newLocation).apply();
+  }
 
-   public void setActiveTranslation(String translation) {
-     mPrefs.edit().putString(Constants.PREF_ACTIVE_TRANSLATION, translation).apply();
-   }
+  public void setActiveTranslation(String translation) {
+    mPrefs.edit().putString(Constants.PREF_ACTIVE_TRANSLATION, translation).apply();
+  }
 }
