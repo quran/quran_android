84168fd5c5eee7fe6ffb76571ecafde6c09347a4
ahmedre <ahmedre@gmail.com>
2015-10-01 00:19:49 -0700

    Update handling of Quran directory settings.
    This change does a few things for the "Quran Data Directory" setting in
    Quran's settings screen:
    1. adds documentation on the handling of storage locations on Kitkat+.
    2. tweaks settings on Kitkat+ to also return the external storage
    directory (as opposed to just the various getExternalFilesDirs results).
    3. tweaks settings on M to always show the Quran data directory option.
    4. add a permission flag and check/request permissions on M when needed.
    5. introduce a shared PermissionUtil class for shared code between the
    launch activity and the settings activity.
    6. change results to show the path along with the free space instead of
    just the amount of free space.

---
+++ app/src/main/java/com/quran/labs/androidquran/service/util/PermissionUtil.java
@@ -0,0 +1,26 @@
+package com.quran.labs.androidquran.service.util;
+
+import com.quran.labs.androidquran.util.QuranSettings;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+public class PermissionUtil {
+
+  public static boolean haveWriteExternalStoragePermission(Context context) {
+    return Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+        ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+            PackageManager.PERMISSION_GRANTED;
+  }
+
+  public static boolean canRequestWriteExternalStoragePermission(Activity activity) {
+    return !QuranSettings.getInstance(activity).didPresentSdcardPermissionsRationale() ||
+        ActivityCompat.shouldShowRequestPermissionRationale(activity,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE);
+  }
+}
