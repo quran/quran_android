e9f62c1ceeb326a47ddb013e2a2773730d53d0aa
ahmedre <ahmedre@gmail.com>
2015-11-07 23:36:17 -0800

    Inline SeekBarPreference.
    This has two benefits - first, we get a "material-ish" theme everywhere,
    and second, it adds some color to preferences. The ui was inspired by the
    MaterialSeekBarPreference library, but none of their code was used.

--- app/src/main/java/com/quran/labs/androidquran/ui/preference/SeekBarPreference.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/preference/SeekBarPreference.java
@@ -6,83 +6,80 @@
 
 package com.quran.labs.androidquran.ui.preference;
 
+import com.quran.labs.androidquran.R;
 import com.quran.labs.androidquran.data.Constants;
+import com.quran.labs.androidquran.util.QuranUtils;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.preference.DialogPreference;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
+import android.preference.Preference;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class SeekBarPreference extends DialogPreference implements
-    SeekBar.OnSeekBarChangeListener {
+public class SeekBarPreference extends Preference implements SeekBar.OnSeekBarChangeListener {
 
-  private static final String androidns = "http://schemas.android.com/apk/res/android";
+  private static final String ANDROID_NS = "http://schemas.android.com/apk/res/android";
 
+  private Context mContext;
   private SeekBar mSeekBar;
   private TextView mValueText;
-  private Context mContext;
 
-  private String mDialogMessage, mSuffix;
+  private String mSuffix;
+  private int mTintColor;
+  private int mCurrentValue;
   private int mDefault, mMax, mValue = 0;
 
   public SeekBarPreference(Context context, AttributeSet attrs) {
     super(context, attrs);
     mContext = context;
-
-    mDialogMessage = attrs.getAttributeValue(androidns, "dialogMessage");
-    mSuffix = attrs.getAttributeValue(androidns, "text");
-    mDefault = attrs.getAttributeIntValue(androidns, "defaultValue",
+    mSuffix = attrs.getAttributeValue(ANDROID_NS, "text");
+    mDefault = attrs.getAttributeIntValue(ANDROID_NS, "defaultValue",
         Constants.DEFAULT_TEXT_SIZE);
-    mMax = attrs.getAttributeIntValue(androidns, "max", 100);
+    mMax = attrs.getAttributeIntValue(ANDROID_NS, "max", 100);
+    setLayoutResource(R.layout.seekbar_pref);
+    mTintColor = context.getResources().getColor(R.color.accent_color);
   }
 
   @Override
-  protected View onCreateDialogView() {
-    LinearLayout.LayoutParams params;
-    LinearLayout layout = new LinearLayout(mContext);
-    layout.setOrientation(LinearLayout.VERTICAL);
-    layout.setPadding(6, 6, 6, 6);
-
-    final TextView splashText = new TextView(mContext);
-    if (mDialogMessage != null) {
-      splashText.setText(mDialogMessage);
-    }
-    layout.addView(splashText);
-
-    mValueText = new TextView(mContext);
-    mValueText.setGravity(Gravity.CENTER_HORIZONTAL);
-    mValueText.setTextSize(32);
-    params = new LinearLayout.LayoutParams(
-        LinearLayout.LayoutParams.FILL_PARENT,
-        LinearLayout.LayoutParams.WRAP_CONTENT);
-    layout.addView(mValueText, params);
-
-    mSeekBar = new SeekBar(mContext);
+  protected View onCreateView(ViewGroup parent) {
+    View view = super.onCreateView(parent);
+    mSeekBar = (SeekBar) view.findViewById(R.id.seekbar);
+    mValueText = (TextView) view.findViewById(R.id.value);
     mSeekBar.setOnSeekBarChangeListener(this);
-    layout.addView(mSeekBar, new LinearLayout.LayoutParams(
-        LinearLayout.LayoutParams.FILL_PARENT,
-        LinearLayout.LayoutParams.WRAP_CONTENT));
-
-    if (shouldPersist()) {
-      mValue = getPersistedInt(mDefault);
-    }
-
-    mSeekBar.setMax(mMax);
-    mSeekBar.setProgress(mValue);
-    return layout;
+    styleSeekBar();
+    return view;
   }
 
-  @Override
-  protected void onBindDialogView(@NonNull View v) {
-    super.onBindDialogView(v);
-    mSeekBar.setMax(mMax);
-    mSeekBar.setProgress(mValue);
+  private void styleSeekBar() {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+      final Drawable progressDrawable = mSeekBar.getProgressDrawable();
+      if (progressDrawable != null) {
+        if (progressDrawable instanceof LayerDrawable) {
+          LayerDrawable ld = (LayerDrawable) progressDrawable;
+          int layers = ld.getNumberOfLayers();
+          for (int i = 0; i < layers; i++) {
+            ld.getDrawable(i).mutate().setColorFilter(mTintColor, PorterDuff.Mode.SRC_ATOP);
+          }
+        } else {
+          progressDrawable.mutate().setColorFilter(mTintColor, PorterDuff.Mode.SRC_ATOP);
+        }
+      }
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        final Drawable thumb = mSeekBar.getThumb();
+        if (thumb != null) {
+          thumb.mutate().setColorFilter(mTintColor, PorterDuff.Mode.SRC_ATOP);
+        }
+      }
+    }
   }
 
   @Override
@@ -94,6 +91,9 @@ public class SeekBarPreference extends DialogPreference implements
         tv.setTextColor(Color.WHITE);
       }
     }
+    mValue = shouldPersist() ? getPersistedInt(mDefault) : 0;
+    mSeekBar.setMax(mMax);
+    mSeekBar.setProgress(mValue);
   }
 
   @Override
@@ -106,37 +106,20 @@ public class SeekBarPreference extends DialogPreference implements
     }
   }
 
+  @Override
   public void onProgressChanged(SeekBar seek, int value, boolean fromTouch) {
-    String t = String.valueOf(value);
+    String t = QuranUtils.getLocalizedNumber(mContext, value);
     mValueText.setText(mSuffix == null ? t : t.concat(mSuffix));
-    if (shouldPersist()) {
-      persistInt(value);
-    }
-    callChangeListener(value);
+    mCurrentValue = value;
   }
 
   public void onStartTrackingTouch(SeekBar seek) {
   }
 
   public void onStopTrackingTouch(SeekBar seek) {
-  }
-
-  public void setMax(int max) {
-    mMax = max;
-  }
-
-  public int getMax() {
-    return mMax;
-  }
-
-  public void setProgress(int progress) {
-    mValue = progress;
-    if (mSeekBar != null) {
-      mSeekBar.setProgress(progress);
+    if (shouldPersist()) {
+      persistInt(mCurrentValue);
+      callChangeListener(mCurrentValue);
     }
-  }
-
-  public int getProgress() {
-    return mValue;
   }
 }
