f6ceb7ee152dd0aa0274e40cd172a11214abadd8
ahmedre <ahmedre@gmail.com>
2015-11-28 23:38:44 -0800

    Initial work on exporting of tags and bookmarks.

---
+++ app/src/main/java/com/quran/labs/androidquran/model/BookmarkJsonModel.java
@@ -0,0 +1,27 @@
+package com.quran.labs.androidquran.model;
+
+import com.quran.labs.androidquran.dao.BookmarkData;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+
+import okio.BufferedSink;
+import okio.BufferedSource;
+
+public class BookmarkJsonModel {
+  private final JsonAdapter<BookmarkData> jsonAdapter;
+
+  public BookmarkJsonModel() {
+    Moshi moshi = new Moshi.Builder().build();
+    jsonAdapter = moshi.adapter(BookmarkData.class);
+  }
+
+  public void toJson(BufferedSink sink, BookmarkData bookmarks) throws IOException {
+    jsonAdapter.toJson(sink, bookmarks);
+  }
+
+  public BookmarkData fromJson(BufferedSource jsonSource) throws IOException {
+    return jsonAdapter.fromJson(jsonSource);
+  }
+}
