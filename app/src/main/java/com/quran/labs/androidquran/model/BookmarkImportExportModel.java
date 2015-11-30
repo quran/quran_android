cf673d3e4c824729bcfdaf26cf5c284579485a0d
ahmedre <ahmedre@gmail.com>
2015-11-29 17:14:49 -0800

    Share backup file after export.
    After an export, share the backup file. This uses the proper method of
    sharing (i.e. a FileProvider instead of external storage uris), so it's
    secure (and also supported on Marshmallow).

--- app/src/main/java/com/quran/labs/androidquran/model/BookmarkImportExportModel.java
+++ app/src/main/java/com/quran/labs/androidquran/model/BookmarkImportExportModel.java
@@ -1,12 +1,14 @@
 package com.quran.labs.androidquran.model;
 
+import com.quran.labs.androidquran.R;
 import com.quran.labs.androidquran.dao.BookmarkData;
 import com.quran.labs.androidquran.database.BookmarksDBAdapter;
 
 import android.content.Context;
+import android.net.Uri;
+import android.support.v4.content.FileProvider;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
@@ -19,37 +21,42 @@ public class BookmarkImportExportModel {
   private final BookmarkJsonModel jsonModel;
   private final BookmarksDBAdapter databaseAdapter;
   private final File externalFilesDir;
+  private final Context appContext;
 
   public BookmarkImportExportModel(Context context) {
     jsonModel = new BookmarkJsonModel();
     databaseAdapter = new BookmarksDBAdapter(context);
     externalFilesDir = new File(context.getExternalFilesDir(null), "backups");
+    appContext = context.getApplicationContext();
   }
 
-  public Observable<Boolean> exportBookmarksObservable() {
-    return Observable.fromCallable(new Callable<Boolean>() {
+  public Observable<Uri> exportBookmarksObservable() {
+    return Observable.fromCallable(new Callable<Uri>() {
       @Override
-      public Boolean call() throws Exception {
+      public Uri call() throws Exception {
         return exportBookmarks();
       }
     }).subscribeOn(Schedulers.io());
   }
 
-  private boolean exportBookmarks() {
+  private Uri exportBookmarks() {
     BookmarkData data = new BookmarkData(databaseAdapter.getTags(),
         databaseAdapter.getBookmarks(true, BookmarksDBAdapter.SORT_DATE_ADDED));
 
+    Uri result = null;
     try {
       if (externalFilesDir.exists() || externalFilesDir.mkdir()) {
-        BufferedSink sink = Okio.buffer(Okio.sink(new File(externalFilesDir, "backup.json")));
+        File file = new File(externalFilesDir, "backup.json");
+        BufferedSink sink = Okio.buffer(Okio.sink(file));
         jsonModel.toJson(sink, data);
         sink.close();
+
+        result = FileProvider.getUriForFile(
+            appContext, appContext.getString(R.string.file_authority), file);
       }
-    } catch (FileNotFoundException ffe) {
-      return false;
     } catch (IOException ioe) {
-      return false;
+      // probably log at some point
     }
-    return true;
+    return result;
   }
 }
