12c9224f1af9d06cdec3dc673e32ffea89f03d78
ahmedre <ahmedre@gmail.com>
2015-12-09 23:35:56 -0800

    Use Timber for logging.
    This has two benefits - the first is that we don't get logging to adb by
    default in production builds (which is bad practice). The second is that
    we can install a specific production log listener that records the last
    few lines, and, in the case of someone reporting a problem, we can
    easily attach logs to know what's happening.

--- app/src/debug/java/com/quran/labs/androidquran/DebugApplication.java
+++ app/src/debug/java/com/quran/labs/androidquran/DebugApplication.java
@@ -5,12 +5,15 @@ import com.squareup.leakcanary.LeakCanary;
 
 import android.os.Build;
 
+import timber.log.Timber;
+
 public class DebugApplication extends QuranApplication {
 
   @Override
   public void onCreate() {
     super.onCreate();
 
+    Timber.plant(new Timber.DebugTree());
     Stetho.initializeWithDefaults(this);
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
       LeakCanary.install(this);
