quran/quran_android

commit e389760e6a23f23057906a3ead6f54e7355734cf

Remove isChecked from Tag.
Tag is a model object and should not contain isChecked, which is
specifically tied to the ui for adding or removing tags from a bookmark.

app/src/main/java/com/quran/labs/androidquran/ui/fragment/TagBookmarkDialog.java
@@ -1,9 +1,9 @@
 package com.quran.labs.androidquran.ui.fragment;
 
 import com.quran.labs.androidquran.R;
+import com.quran.labs.androidquran.dao.Tag;
 import com.quran.labs.androidquran.data.SuraAyah;
 import com.quran.labs.androidquran.database.BookmarksDBAdapter;
-import com.quran.labs.androidquran.dao.Tag;
 import com.quran.labs.androidquran.ui.helpers.BookmarkHandler;
 
 import android.app.Activity;
@@ -30,6 +30,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class TagBookmarkDialog extends DialogFragment {
@@ -43,6 +44,7 @@ public class TagBookmarkDialog extends DialogFragment {
   private Integer mAyah;
   private int mPage = -1;
   private List<Tag> mTags;
+  private HashSet<Long> mCheckedTags = new HashSet<>();
   private TagsAdapter mAdapter;
 
   private AsyncTask mCurrentTask;
@@ -54,6 +56,7 @@ public class TagBookmarkDialog extends DialogFragment {
   private static final String EXTRA_SURA = "sura";
   private static final String EXTRA_AYAH = "ayah";
   private static final String TAG_LIST = "taglist";
+  private static final String CHECKED_TAG_LIST = "checked_tag_list";
 
   public static TagBookmarkDialog newInstance(long bookmarkId) {
     final Bundle args = new Bundle();
@@ -111,8 +114,8 @@ public class TagBookmarkDialog extends DialogFragment {
   @Override
   public void onSaveInstanceState(Bundle outState) {
     outState.putBoolean(MADE_CHANGES, mMadeChanges);
-    outState.putParcelableArrayList(TAG_LIST,
-        (ArrayList<? extends Parcelable>) mTags);
+    outState.putParcelableArrayList(TAG_LIST, (ArrayList<? extends Parcelable>) mTags);
+    outState.putSerializable(CHECKED_TAG_LIST, mCheckedTags);
     super.onSaveInstanceState(outState);
   }
 
@@ -143,10 +146,23 @@ public class TagBookmarkDialog extends DialogFragment {
     if (savedInstanceState != null) {
       mMadeChanges = savedInstanceState.getBoolean(MADE_CHANGES);
       mTags = savedInstanceState.getParcelableArrayList(TAG_LIST);
+      //noinspection unchecked
+      HashSet<Long> set = (HashSet<Long>) savedInstanceState.getSerializable(CHECKED_TAG_LIST);
+      if (set != null) {
+        mCheckedTags = set;
+      }
     }
 
     if (mTags == null) {
       new RefreshTagsTask().execute();
+    }
+  }
+
+  private void toggleTag(long id) {
+    if (mCheckedTags.contains(id)) {
+      mCheckedTags.remove(id);
+    } else {
+      mCheckedTags.add(id);
     }
   }
 
@@ -162,7 +178,7 @@ public class TagBookmarkDialog extends DialogFragment {
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Tag tag = (Tag) mAdapter.getItem(position);
         if (tag.id >= 0) {
-          tag.toggle();
+          toggleTag(tag.id);
           setMadeChanges();
         } else if (tag.id == -1) {
           Context context = getActivity();
@@ -172,12 +188,10 @@ public class TagBookmarkDialog extends DialogFragment {
           }
         }
 
-        if (view.getTag() != null) {
-          Object viewTag = view.getTag();
-          if (viewTag instanceof ViewHolder) {
-            ViewHolder holder = (ViewHolder) viewTag;
-            holder.checkBox.setChecked(tag.isChecked());
-          }
+        Object viewTag = view.getTag();
+        if (viewTag instanceof ViewHolder) {
+          ViewHolder holder = (ViewHolder) viewTag;
+          holder.checkBox.setChecked(mCheckedTags.contains(tag.id));
         }
       }
     });
@@ -264,8 +278,7 @@ public class TagBookmarkDialog extends DialogFragment {
     }
 
     @Override
-    public View getView(int position, View convertView,
-        ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
       ViewHolder holder;
       if (convertView == null) {
         convertView = mInflater.inflate(R.layout.tag_row, parent, false);
@@ -287,10 +300,10 @@ public class TagBookmarkDialog extends DialogFragment {
       } else {
         holder.addImage.setVisibility(View.GONE);
         holder.checkBox.setVisibility(View.VISIBLE);
-        holder.checkBox.setChecked(tag.isChecked());
+        holder.checkBox.setChecked(mCheckedTags.contains(tag.id));
         holder.checkBox.setOnClickListener(new OnClickListener() {
           public void onClick(View v) {
-            tag.toggle();
+            toggleTag(tag.id);
             setMadeChanges();
           }
         });
@@ -338,7 +351,7 @@ public class TagBookmarkDialog extends DialogFragment {
       if (result != null) {
         for (Tag tag : result) {
           if (mBookmarkTags.contains(tag.id)) {
-            tag.setChecked(true);
+            mCheckedTags.add(tag.id);
           }
         }
         mMadeChanges = false;
@@ -351,7 +364,6 @@ public class TagBookmarkDialog extends DialogFragment {
   }
 
   class ViewHolder {
-
     CheckBox checkBox;
     TextView tagName;
     ImageView addImage;
@@ -377,7 +389,7 @@ public class TagBookmarkDialog extends DialogFragment {
     @Override
     protected void onPostExecute(Tag result) {
       if (result != null && mTags != null && mAdapter != null) {
-        result.setChecked(true);
+        mCheckedTags.add(result.id);
         mTags.add(mTags.size() - 1, result);
         mAdapter.notifyDataSetChanged();
         setMadeChanges();
@@ -397,7 +409,7 @@ public class TagBookmarkDialog extends DialogFragment {
     protected Void doInBackground(Void... params) {
       BookmarksDBAdapter adapter = null;
       Activity activity = getActivity();
-      if (activity != null && activity instanceof BookmarkHandler) {
+      if (activity instanceof BookmarkHandler) {
         adapter = ((BookmarkHandler) activity).getBookmarksAdapter();
       }
 
@@ -409,9 +421,9 @@ public class TagBookmarkDialog extends DialogFragment {
         if (mBookmarkId < 0) {
           mBookmarkId = adapter.addBookmarkIfNotExists(mSura, mAyah, mPage);
         }
-        adapter.tagBookmark(mBookmarkId, mTags);
+        adapter.tagBookmark(mBookmarkId, mCheckedTags);
       } else {
-        adapter.tagBookmarks(mBookmarkIds, mTags);
+        adapter.tagBookmarks(mBookmarkIds, mCheckedTags);
       }
       return null;
     }
@@ -421,7 +433,7 @@ public class TagBookmarkDialog extends DialogFragment {
       mCurrentTask = null;
       mMadeChanges = false;
       final Activity activity = getActivity();
-      if (activity != null && activity instanceof OnBookmarkTagsUpdateListener) {
+      if (activity instanceof OnBookmarkTagsUpdateListener) {
         ((OnBookmarkTagsUpdateListener) activity).onBookmarkTagsUpdated();
       }
 

app/src/main/java/com/quran/labs/androidquran/dao/Tag.java
@@ -2,13 +2,11 @@ package com.quran.labs.androidquran.dao;
 
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.widget.Checkable;
 
-public class Tag implements Checkable, Parcelable {
+public class Tag implements Parcelable {
 
   public long id;
   public String name;
-  private boolean isChecked = false;
 
   public Tag(long id, String name) {
     this.id = id;
@@ -25,21 +23,6 @@ public class Tag implements Checkable, Parcelable {
   }
 
   @Override
-  public boolean isChecked() {
-    return isChecked;
-  }
-
-  @Override
-  public void setChecked(boolean checked) {
-    isChecked = checked;
-  }
-
-  @Override
-  public void toggle() {
-    isChecked = !isChecked;
-  }
-
-  @Override
   public int describeContents() {
     return 0;
   }
@@ -48,13 +31,11 @@ public class Tag implements Checkable, Parcelable {
   public void writeToParcel(Parcel dest, int flags) {
     dest.writeLong(id);
     dest.writeString(name);
-    dest.writeByte((byte) (isChecked ? 1 : 0));
   }
 
   public void readFromParcel(Parcel parcel) {
     id = parcel.readLong();
     name = parcel.readString();
-    isChecked = parcel.readByte() == 1;
   }
 
   public static final Creator<Tag> CREATOR =

app/src/main/java/com/quran/labs/androidquran/database/BookmarksDBAdapter.java
@@ -15,6 +15,7 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class BookmarksDBAdapter {
 
@@ -215,54 +216,45 @@ public class BookmarksDBAdapter {
     return removed;
   }
 
-  public void tagBookmarks(long[] bookmarkIds, List<Tag> tags) {
+  public void tagBookmarks(long[] bookmarkIds, Set<Long> tagIds) {
     mDb.beginTransaction();
     try {
-      for (int i = 0, tagsSize = tags.size(); i < tagsSize; i++) {
-        Tag t = tags.get(i);
-        if (t.id < 0 || !t.isChecked()) {
-          continue;
-        }
+      for (Long tagId : tagIds) {
         for (long bookmarkId : bookmarkIds) {
           ContentValues values = new ContentValues();
           values.put(BookmarkTagTable.BOOKMARK_ID, bookmarkId);
-          values.put(BookmarkTagTable.TAG_ID, t.id);
+          values.put(BookmarkTagTable.TAG_ID, tagId);
           mDb.replace(BookmarkTagTable.TABLE_NAME, null, values);
         }
       }
+      mDb.setTransactionSuccessful();
     } catch (Exception e) {
       Log.d(TAG, "exception in tagBookmark", e);
+    } finally {
+      mDb.endTransaction();
     }
-
-    mDb.setTransactionSuccessful();
-    mDb.endTransaction();
   }
 
-  public void tagBookmark(long bookmarkId, List<Tag> tags) {
+  public void tagBookmark(long bookmarkId, Set<Long> tagIds) {
     mDb.beginTransaction();
     try {
-      for (int i = 0, tagsSize = tags.size(); i < tagsSize; i++) {
-        Tag t = tags.get(i);
-        if (t.id < 0) {
-          continue;
-        }
-        if (t.isChecked()) {
-          ContentValues values = new ContentValues();
-          values.put(BookmarkTagTable.BOOKMARK_ID, bookmarkId);
-          values.put(BookmarkTagTable.TAG_ID, t.id);
-          mDb.replace(BookmarkTagTable.TABLE_NAME, null, values);
-        } else {
-          mDb.delete(BookmarkTagTable.TABLE_NAME,
-              BookmarkTagTable.BOOKMARK_ID + "=" + bookmarkId +
-                  " AND " + BookmarkTagTable.TAG_ID + "=" + t.id, null);
-        }
+      // delete all tags for this bookmark, then re-add the relevant ones - since we're in a
+      // transaction, we should be okay.
+      mDb.delete(BookmarkTagTable.TABLE_NAME,
+          BookmarkTagTable.BOOKMARK_ID + "=" + bookmarkId, null);
+
+      for (Long tagId : tagIds) {
+        ContentValues values = new ContentValues();
+        values.put(BookmarkTagTable.BOOKMARK_ID, bookmarkId);
+        values.put(BookmarkTagTable.TAG_ID, tagId);
+        mDb.replace(BookmarkTagTable.TABLE_NAME, null, values);
       }
+      mDb.setTransactionSuccessful();
     } catch (Exception e) {
       Log.d(TAG, "exception in tagBookmark", e);
+    } finally {
+      mDb.endTransaction();
     }
-
-    mDb.setTransactionSuccessful();
-    mDb.endTransaction();
   }
 
   public void untagBookmark(long bookmarkId, long tagId) {

commit 822067c714884807987118da94494f4d9fd82310

Fix tags being incorrectly checked.
This fixes a bug introduced in e389760e6a23f23057906a3ead6f54e7355734cf,
where isChecked was removed from Tag, consequently forcing
TagBookmarkDialog to have a list of tags and a list of checked tags. The
bug was that on refresh, the list of checked tags was never being
cleared.

In practical terms, this meant that if one unbookmarks a tagged ayah and
goes to see the ayah tags, they would see the previous tag(s) checked, even
though unbookmarking the ayah removes all tags.

This patch fixes this issue by clearing the list of checked tags upon
refresh (instead of only adding newly checked tags to the list).

app/src/main/java/com/quran/labs/androidquran/ui/fragment/TagBookmarkDialog.java
@@ -403,11 +403,11 @@ public class TagBookmarkDialog extends DialogFragment {
                 tags.add(new Tag(-1, mNewTagString));
 
                 List<Long> bookmarkTags = refreshInfo.bookmarkTagIds;
-                if (!bookmarkTags.isEmpty()) {
-                  for (Tag tag : tags) {
-                    if (bookmarkTags.contains(tag.id)) {
-                      mCheckedTags.add(tag.id);
-                    }
+                mCheckedTags.clear();
+                for (int i = 0, tagsSize = tags.size(); i < tagsSize; i++) {
+                  Tag tag = tags.get(i);
+                  if (bookmarkTags.contains(tag.id)) {
+                    mCheckedTags.add(tag.id);
                   }
                 }
                 mMadeChanges = false;