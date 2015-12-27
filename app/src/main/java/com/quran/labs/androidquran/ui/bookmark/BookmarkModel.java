2713de9588c38ae7e1356a1b2c20add91e2b3ce7
ahmedre <ahmedre@gmail.com>
2015-12-26 23:38:32 -0800

    Minor code refactoring and moving.
    BookmarkModel had a few methods that were specific to BookmarkPresenter,
    so move those methods there. Moreover, BookmarkUtil had code that is
    only used by BookmarksFragment, so move that code there.

--- app/src/main/java/com/quran/labs/androidquran/ui/bookmark/BookmarkModel.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/bookmark/BookmarkModel.java
@@ -3,21 +3,15 @@ package com.quran.labs.androidquran.ui.bookmark;
 import com.quran.labs.androidquran.dao.Bookmark;
 import com.quran.labs.androidquran.dao.BookmarkData;
 import com.quran.labs.androidquran.dao.Tag;
-import com.quran.labs.androidquran.data.Constants;
 import com.quran.labs.androidquran.database.BookmarksDBAdapter;
 import com.quran.labs.androidquran.ui.helpers.QuranRow;
-import com.quran.labs.androidquran.ui.helpers.QuranRowFactory;
-import com.quran.labs.androidquran.util.QuranSettings;
 
 import android.content.Context;
 import android.support.annotation.VisibleForTesting;
 import android.support.v4.util.Pair;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
@@ -25,15 +19,9 @@ import rx.Observable;
 import rx.functions.Func1;
 import rx.functions.Func2;
 import rx.schedulers.Schedulers;
-import timber.log.Timber;
 
 public class BookmarkModel {
-  private static final long BOOKMARKS_WITHOUT_TAGS_ID = -1;
-
   private static BookmarkModel sInstance;
-
-  private final Context mAppContext;
-  private final QuranSettings mQuranSettings;
   private final BookmarksDBAdapter mBookmarksDBAdapter;
 
   public static synchronized BookmarkModel getInstance(Context context) {
@@ -44,20 +32,15 @@ public class BookmarkModel {
   }
 
   private BookmarkModel(Context context) {
-    mAppContext = context.getApplicationContext();
-    mQuranSettings = QuranSettings.getInstance(context);
     mBookmarksDBAdapter = new BookmarksDBAdapter(context);
   }
 
   @VisibleForTesting
-  BookmarkModel(Context appContext, QuranSettings settings, BookmarksDBAdapter adapter) {
-    mAppContext = appContext;
-    mQuranSettings = settings;
+  BookmarkModel(BookmarksDBAdapter adapter) {
     mBookmarksDBAdapter = adapter;
   }
 
-  public Observable<BookmarkResult> getBookmarksListObservable(
-      final int sortOrder, final boolean groupByTags) {
+  public Observable<BookmarkData> getBookmarkDataObservable(final int sortOrder) {
     return Observable
         .zip(getTagsObservable(), getBookmarksObservable(sortOrder),
             new Func2<List<Tag>, List<Bookmark>, BookmarkData>() {
@@ -66,15 +49,7 @@ public class BookmarkModel {
                 return new BookmarkData(tags, bookmarks);
               }
             })
-        .map(new Func1<BookmarkData, BookmarkResult>() {
-          @Override
-          public BookmarkResult call(BookmarkData bookmarkData) {
-            List<QuranRow> rows = getBookmarkRows(bookmarkData, groupByTags);
-            Map<Long, Tag> tagMap = generateTagMap(bookmarkData.getTags());
-            return new BookmarkResult(rows, tagMap);
-          }
-        })
-        .subscribeOn(Schedulers.io());
+            .subscribeOn(Schedulers.io());
   }
 
   public Observable<Void> removeItemsObservable(
@@ -234,130 +209,5 @@ public class BookmarkModel {
             }
           }
         }).subscribeOn(Schedulers.io());
-  }
-
-  private List<QuranRow> getRowsSortedByTags(List<Tag> tags, List<Bookmark> bookmarks) {
-    List<QuranRow> rows = new ArrayList<>();
-    // sort by tags, alphabetical
-    Map<Long, List<Bookmark>> tagsMapping = generateTagsMapping(tags, bookmarks);
-    for (int i = 0, tagsSize = tags.size(); i < tagsSize; i++) {
-      Tag tag = tags.get(i);
-      rows.add(QuranRowFactory.fromTag(tag));
-      List<Bookmark> tagBookmarks = tagsMapping.get(tag.id);
-      for (int j = 0, tagBookmarksSize = tagBookmarks.size(); j < tagBookmarksSize; j++) {
-        rows.add(QuranRowFactory.fromBookmark(mAppContext, tagBookmarks.get(j), tag.id));
-      }
-    }
-
-    // add untagged bookmarks
-    List<Bookmark> untagged = tagsMapping.get(BOOKMARKS_WITHOUT_TAGS_ID);
-    if (untagged.size() > 0) {
-      rows.add(QuranRowFactory.fromNotTaggedHeader(mAppContext));
-      for (int i = 0, untaggedSize = untagged.size(); i < untaggedSize; i++) {
-        rows.add(QuranRowFactory.fromBookmark(mAppContext, untagged.get(i)));
-      }
-    }
-    return rows;
-  }
-
-  private List<QuranRow> getSortedRows(List<Bookmark> bookmarks) {
-    List<QuranRow> rows = new ArrayList<>(bookmarks.size());
-    List<Bookmark> ayahBookmarks = new ArrayList<>();
-
-    // add the page bookmarks directly, save ayah bookmarks for later
-    for (int i = 0, bookmarksSize = bookmarks.size(); i < bookmarksSize; i++) {
-      Bookmark bookmark = bookmarks.get(i);
-      if (bookmark.isPageBookmark()) {
-        rows.add(QuranRowFactory.fromBookmark(mAppContext, bookmark));
-      } else {
-        ayahBookmarks.add(bookmark);
-      }
-    }
-
-    // add page bookmarks header if needed
-    if (rows.size() > 0) {
-      rows.add(0, QuranRowFactory.fromPageBookmarksHeader(mAppContext));
-    }
-
-    // add ayah bookmarks if any
-    if (ayahBookmarks.size() > 0) {
-      rows.add(QuranRowFactory.fromAyahBookmarksHeader(mAppContext));
-      for (int i = 0, ayahBookmarksSize = ayahBookmarks.size(); i < ayahBookmarksSize; i++) {
-        rows.add(QuranRowFactory.fromBookmark(mAppContext, ayahBookmarks.get(i)));
-      }
-    }
-
-    return rows;
-  }
-
-  private List<QuranRow> getBookmarkRows(BookmarkData data, boolean groupByTags) {
-    List<QuranRow> rows;
-
-    List<Tag> tags = data.getTags();
-    List<Bookmark> bookmarks = data.getBookmarks();
-
-    if (groupByTags) {
-      rows = getRowsSortedByTags(tags, bookmarks);
-    } else {
-      rows = getSortedRows(bookmarks);
-    }
-
-    int lastPage = mQuranSettings.getLastPage();
-    boolean showLastPage = lastPage != Constants.NO_PAGE_SAVED;
-    if (showLastPage && (lastPage > Constants.PAGES_LAST || lastPage < Constants.PAGES_FIRST)) {
-      showLastPage = false;
-      Timber.w("Got invalid last saved page as %d", lastPage);
-    }
-
-    if (showLastPage) {
-      rows.add(0, QuranRowFactory.fromCurrentPageHeader(mAppContext));
-      rows.add(1, QuranRowFactory.fromCurrentPage(mAppContext, lastPage));
-    }
-
-    return rows;
-  }
-
-  private BookmarkData fetchBookmarkData(int sortOrder) {
-    List<Bookmark> bookmarks = mBookmarksDBAdapter.getBookmarks(sortOrder);
-    List<Tag> tags = mBookmarksDBAdapter.getTags();
-    return new BookmarkData(tags, bookmarks);
-  }
-
-  private Map<Long, List<Bookmark>> generateTagsMapping(
-      List<Tag> tags, List<Bookmark> bookmarks) {
-    Set<Long> seenBookmarks = new HashSet<>();
-    Map<Long, List<Bookmark>> tagMappings = new HashMap<>();
-    for (int i = 0, tagSize = tags.size(); i < tagSize; i++) {
-      long id = tags.get(i).id;
-      List<Bookmark> matchingBookmarks = new ArrayList<>();
-      for (int j = 0, bookmarkSize = bookmarks.size(); j < bookmarkSize; j++) {
-        Bookmark bookmark = bookmarks.get(j);
-        if (bookmark.tags.contains(id)) {
-          matchingBookmarks.add(bookmark);
-          seenBookmarks.add(bookmark.id);
-        }
-      }
-      tagMappings.put(id, matchingBookmarks);
-    }
-
-    List<Bookmark> untaggedBookmarks = new ArrayList<>();
-    for (int i = 0, bookmarksSize = bookmarks.size(); i < bookmarksSize; i++) {
-      Bookmark bookmark = bookmarks.get(i);
-      if (!seenBookmarks.contains(bookmark.id)) {
-        untaggedBookmarks.add(bookmark);
-      }
-    }
-    tagMappings.put(BOOKMARKS_WITHOUT_TAGS_ID, untaggedBookmarks);
-
-    return tagMappings;
-  }
-
-  private Map<Long, Tag> generateTagMap(List<Tag> tags) {
-    Map<Long, Tag> tagMap = new HashMap<>(tags.size());
-    for (int i = 0, size = tags.size(); i < size; i++) {
-      Tag tag = tags.get(i);
-      tagMap.put(tag.id, tag);
-    }
-    return tagMap;
   }
 }
--- app/src/main/java/com/quran/labs/androidquran/ui/bookmark/BookmarkPresenter.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/bookmark/BookmarkPresenter.java
@@ -1,14 +1,24 @@
 package com.quran.labs.androidquran.ui.bookmark;
 
+import com.quran.labs.androidquran.dao.Bookmark;
+import com.quran.labs.androidquran.dao.BookmarkData;
+import com.quran.labs.androidquran.dao.Tag;
+import com.quran.labs.androidquran.data.Constants;
 import com.quran.labs.androidquran.presenter.Presenter;
 import com.quran.labs.androidquran.ui.fragment.BookmarksFragment;
 import com.quran.labs.androidquran.ui.helpers.QuranRow;
+import com.quran.labs.androidquran.ui.helpers.QuranRowFactory;
 import com.quran.labs.androidquran.util.QuranSettings;
 
 import android.content.Context;
+import android.support.annotation.VisibleForTesting;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
@@ -21,9 +31,11 @@ import timber.log.Timber;
 
 public class BookmarkPresenter implements Presenter<BookmarksFragment> {
   public static final int DELAY_DELETION_DURATION_IN_MS = 4 * 1000; // 4 seconds
+  private static final long BOOKMARKS_WITHOUT_TAGS_ID = -1;
 
   private static BookmarkPresenter sInstance;
 
+  private final Context mAppContext;
   private final BookmarkModel mBookmarkModel;
   private final QuranSettings mQuranSettings;
 
@@ -43,8 +55,18 @@ public class BookmarkPresenter implements Presenter<BookmarksFragment> {
   }
 
   private BookmarkPresenter(Context context) {
+    mAppContext = context.getApplicationContext();
     mQuranSettings = QuranSettings.getInstance(context);
     mBookmarkModel = BookmarkModel.getInstance(context);
+    mSortOrder = mQuranSettings.getBookmarksSortOrder();
+    mGroupByTags = mQuranSettings.getBookmarksGroupedByTags();
+  }
+
+  @VisibleForTesting
+  BookmarkPresenter(Context context, QuranSettings settings, BookmarkModel bookmarkModel) {
+    mAppContext = context.getApplicationContext();
+    mQuranSettings = settings;
+    mBookmarkModel = bookmarkModel;
     mSortOrder = mQuranSettings.getBookmarksSortOrder();
     mGroupByTags = mQuranSettings.getBookmarksGroupedByTags();
   }
@@ -157,7 +179,7 @@ public class BookmarkPresenter implements Presenter<BookmarksFragment> {
         .flatMap(new Func1<Void, Observable<BookmarkResult>>() {
           @Override
           public Observable<BookmarkResult> call(Void aVoid) {
-            return mBookmarkModel.getBookmarksListObservable(mSortOrder, mGroupByTags);
+            return getBookmarksListObservable(mSortOrder, mGroupByTags);
           }
         });
   }
@@ -170,8 +192,23 @@ public class BookmarkPresenter implements Presenter<BookmarksFragment> {
     }
   }
 
+  @VisibleForTesting
+  Observable<BookmarkResult> getBookmarksListObservable(
+      int sortOrder, final boolean groupByTags) {
+    return mBookmarkModel.getBookmarkDataObservable(sortOrder)
+        .map(new Func1<BookmarkData, BookmarkResult>() {
+          @Override
+          public BookmarkResult call(BookmarkData bookmarkData) {
+            List<QuranRow> rows = getBookmarkRows(bookmarkData, groupByTags);
+            Map<Long, Tag> tagMap = generateTagMap(bookmarkData.getTags());
+            return new BookmarkResult(rows, tagMap);
+          }
+        })
+        .subscribeOn(Schedulers.io());
+  }
+
   private void getBookmarks(final int sortOrder, final boolean groupByTags) {
-    mBookmarkModel.getBookmarksListObservable(sortOrder, groupByTags)
+    getBookmarksListObservable(sortOrder, groupByTags)
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(new Action1<BookmarkResult>() {
           @Override
@@ -183,6 +220,125 @@ public class BookmarkPresenter implements Presenter<BookmarksFragment> {
             }
           }
         });
+  }
+
+  private List<QuranRow> getBookmarkRows(BookmarkData data, boolean groupByTags) {
+    List<QuranRow> rows;
+
+    List<Tag> tags = data.getTags();
+    List<Bookmark> bookmarks = data.getBookmarks();
+
+    if (groupByTags) {
+      rows = getRowsSortedByTags(tags, bookmarks);
+    } else {
+      rows = getSortedRows(bookmarks);
+    }
+
+    int lastPage = mQuranSettings.getLastPage();
+    boolean showLastPage = lastPage != Constants.NO_PAGE_SAVED;
+    if (showLastPage && (lastPage > Constants.PAGES_LAST || lastPage < Constants.PAGES_FIRST)) {
+      showLastPage = false;
+      Timber.w("Got invalid last saved page as %d", lastPage);
+    }
+
+    if (showLastPage) {
+      rows.add(0, QuranRowFactory.fromCurrentPageHeader(mAppContext));
+      rows.add(1, QuranRowFactory.fromCurrentPage(mAppContext, lastPage));
+    }
+
+    return rows;
+  }
+
+  private List<QuranRow> getRowsSortedByTags(List<Tag> tags, List<Bookmark> bookmarks) {
+    List<QuranRow> rows = new ArrayList<>();
+    // sort by tags, alphabetical
+    Map<Long, List<Bookmark>> tagsMapping = generateTagsMapping(tags, bookmarks);
+    for (int i = 0, tagsSize = tags.size(); i < tagsSize; i++) {
+      Tag tag = tags.get(i);
+      rows.add(QuranRowFactory.fromTag(tag));
+      List<Bookmark> tagBookmarks = tagsMapping.get(tag.id);
+      for (int j = 0, tagBookmarksSize = tagBookmarks.size(); j < tagBookmarksSize; j++) {
+        rows.add(QuranRowFactory.fromBookmark(mAppContext, tagBookmarks.get(j), tag.id));
+      }
+    }
+
+    // add untagged bookmarks
+    List<Bookmark> untagged = tagsMapping.get(BOOKMARKS_WITHOUT_TAGS_ID);
+    if (untagged.size() > 0) {
+      rows.add(QuranRowFactory.fromNotTaggedHeader(mAppContext));
+      for (int i = 0, untaggedSize = untagged.size(); i < untaggedSize; i++) {
+        rows.add(QuranRowFactory.fromBookmark(mAppContext, untagged.get(i)));
+      }
+    }
+    return rows;
+  }
+
+  private List<QuranRow> getSortedRows(List<Bookmark> bookmarks) {
+    List<QuranRow> rows = new ArrayList<>(bookmarks.size());
+    List<Bookmark> ayahBookmarks = new ArrayList<>();
+
+    // add the page bookmarks directly, save ayah bookmarks for later
+    for (int i = 0, bookmarksSize = bookmarks.size(); i < bookmarksSize; i++) {
+      Bookmark bookmark = bookmarks.get(i);
+      if (bookmark.isPageBookmark()) {
+        rows.add(QuranRowFactory.fromBookmark(mAppContext, bookmark));
+      } else {
+        ayahBookmarks.add(bookmark);
+      }
+    }
+
+    // add page bookmarks header if needed
+    if (rows.size() > 0) {
+      rows.add(0, QuranRowFactory.fromPageBookmarksHeader(mAppContext));
+    }
+
+    // add ayah bookmarks if any
+    if (ayahBookmarks.size() > 0) {
+      rows.add(QuranRowFactory.fromAyahBookmarksHeader(mAppContext));
+      for (int i = 0, ayahBookmarksSize = ayahBookmarks.size(); i < ayahBookmarksSize; i++) {
+        rows.add(QuranRowFactory.fromBookmark(mAppContext, ayahBookmarks.get(i)));
+      }
+    }
+
+    return rows;
+  }
+
+  private Map<Long, List<Bookmark>> generateTagsMapping(
+      List<Tag> tags, List<Bookmark> bookmarks) {
+    Set<Long> seenBookmarks = new HashSet<>();
+    Map<Long, List<Bookmark>> tagMappings = new HashMap<>();
+    for (int i = 0, tagSize = tags.size(); i < tagSize; i++) {
+      long id = tags.get(i).id;
+      List<Bookmark> matchingBookmarks = new ArrayList<>();
+      for (int j = 0, bookmarkSize = bookmarks.size(); j < bookmarkSize; j++) {
+        Bookmark bookmark = bookmarks.get(j);
+        if (bookmark.tags.contains(id)) {
+          matchingBookmarks.add(bookmark);
+          seenBookmarks.add(bookmark.id);
+        }
+      }
+      tagMappings.put(id, matchingBookmarks);
+    }
+
+    List<Bookmark> untaggedBookmarks = new ArrayList<>();
+    for (int i = 0, bookmarksSize = bookmarks.size(); i < bookmarksSize; i++) {
+      Bookmark bookmark = bookmarks.get(i);
+      if (!seenBookmarks.contains(bookmark.id)) {
+        untaggedBookmarks.add(bookmark);
+      }
+    }
+    tagMappings.put(BOOKMARKS_WITHOUT_TAGS_ID, untaggedBookmarks);
+
+    return tagMappings;
+  }
+
+  private Map<Long, Tag> generateTagMap(List<Tag> tags) {
+    Map<Long, Tag> tagMap = new HashMap<>(tags.size());
+    for (int i = 0, size = tags.size(); i < size; i++) {
+      Tag tag = tags.get(i);
+      tagMap.put(tag.id, tag);
+    }
+    return tagMap;
   }
 
 
