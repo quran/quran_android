6b016e1d846f0123352e06e14a4d457c4ab0b565
ahmedre <ahmedre@gmail.com>
2016-02-13 21:36:05 -0800

    Fix indentation on translation classes

--- app/src/main/java/com/quran/labs/androidquran/util/TranslationUtils.java
+++ app/src/main/java/com/quran/labs/androidquran/util/TranslationUtils.java
@@ -11,44 +11,45 @@ import java.util.List;
 
 public class TranslationUtils {
 
-   public static String getDefaultTranslation(Context context,
-                                       List<TranslationItem> items) {
-     final TranslationItem item = getDefaultTranslationItem(context, items);
-     return item == null ? null : item.filename;
-   }
+  public static String getDefaultTranslation(Context context,
+      List<TranslationItem> items) {
+    final TranslationItem item = getDefaultTranslationItem(context, items);
+    return item == null ? null : item.filename;
+  }
 
   public static TranslationItem getDefaultTranslationItem(Context context,
-      List<TranslationItem> items){
-      if (items == null || items.size() == 0){ return null; }
-      QuranSettings settings = QuranSettings.getInstance(context.getApplicationContext());
-      final String db = settings.getActiveTranslation();
+      List<TranslationItem> items) {
+    if (items == null || items.size() == 0) {
+      return null;
+    }
+    QuranSettings settings = QuranSettings.getInstance(context.getApplicationContext());
+    final String db = settings.getActiveTranslation();
 
-      TranslationItem result = null;
-      boolean changed = false;
-      if (db == null){
-         changed = true;
-         result = items.get(0);
-      }
-      else {
-         boolean found = false;
-         for (TranslationItem item : items){
-            if (item.filename.equals(db)){
-               found = true;
-               result = item;
-               break;
-            }
-         }
-
-         if (!found){
-            changed = true;
-            result = items.get(0);
-         }
+    TranslationItem result = null;
+    boolean changed = false;
+    if (db == null) {
+      changed = true;
+      result = items.get(0);
+    } else {
+      boolean found = false;
+      for (TranslationItem item : items) {
+        if (item.filename.equals(db)) {
+          found = true;
+          result = item;
+          break;
+        }
       }
 
-      if (changed && result != null){
-        settings.setActiveTranslation(result.filename);
+      if (!found) {
+        changed = true;
+        result = items.get(0);
       }
+    }
 
-      return result;
-   }
+    if (changed && result != null) {
+      settings.setActiveTranslation(result.filename);
+    }
+
+    return result;
+  }
 }
