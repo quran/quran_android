49c5be45e4e20b69cda4646bba148cf1da0e3bd7
ahmedre <ahmedre@gmail.com>
2016-09-05 16:26:35 -0700

    Fix indentation on QuranPagerAdapter

--- app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranPageAdapter.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranPageAdapter.java
@@ -17,82 +17,81 @@ import static com.quran.labs.androidquran.data.Constants.PAGES_LAST_DUAL;
 
 public class QuranPageAdapter extends FragmentStatePagerAdapter {
 
-   private boolean mIsShowingTranslation = false;
-   private boolean mIsDualPages = false;
+  private boolean mIsShowingTranslation = false;
+  private boolean mIsDualPages = false;
 
-   public QuranPageAdapter(FragmentManager fm, boolean dualPages,
-                           boolean isShowingTranslation){
-      super(fm);
-      mIsDualPages = dualPages;
-      mIsShowingTranslation = isShowingTranslation;
-   }
+  public QuranPageAdapter(FragmentManager fm, boolean dualPages,
+                          boolean isShowingTranslation) {
+    super(fm);
+    mIsDualPages = dualPages;
+    mIsShowingTranslation = isShowingTranslation;
+  }
 
-   public void setTranslationMode(){
-      if (!mIsShowingTranslation){
-         mIsShowingTranslation = true;
-         notifyDataSetChanged();
-      }
-   }
+  public void setTranslationMode() {
+    if (!mIsShowingTranslation) {
+      mIsShowingTranslation = true;
+      notifyDataSetChanged();
+    }
+  }
 
-   public void setQuranMode(){
-      if (mIsShowingTranslation){
-         mIsShowingTranslation = false;
-         notifyDataSetChanged();
-      }
-   }
+  public void setQuranMode() {
+    if (mIsShowingTranslation) {
+      mIsShowingTranslation = false;
+      notifyDataSetChanged();
+    }
+  }
 
-   @Override
-   public int getItemPosition(Object object){
-      /* when the ViewPager gets a notifyDataSetChanged (or invalidated),
-       * it goes through its set of saved views and runs this method on
-       * each one to figure out whether or not it should remove the view
-       * or not.  the default implementation returns POSITION_UNCHANGED,
-       * which means that "this page is as is."
-       *
-       * as noted in http://stackoverflow.com/questions/7263291 in one
-       * of the answers, if you're just updating your view (changing a
-       * field's value, etc), this is highly inefficient (because you
-       * recreate the view for nothing).
-       *
-       * in our case, however, this is the right thing to do since we
-       * change the fragment completely when we notifyDataSetChanged.
-       */
-      return POSITION_NONE;
-   }
+  @Override
+  public int getItemPosition(Object object) {
+    /* when the ViewPager gets a notifyDataSetChanged (or invalidated),
+     * it goes through its set of saved views and runs this method on
+     * each one to figure out whether or not it should remove the view
+     * or not.  the default implementation returns POSITION_UNCHANGED,
+     * which means that "this page is as is."
+     *
+     * as noted in http://stackoverflow.com/questions/7263291 in one
+     * of the answers, if you're just updating your view (changing a
+     * field's value, etc), this is highly inefficient (because you
+     * recreate the view for nothing).
+     *
+     * in our case, however, this is the right thing to do since we
+     * change the fragment completely when we notifyDataSetChanged.
+     */
+    return POSITION_NONE;
+  }
 
-	@Override
-	public int getCount() {
+  @Override
+  public int getCount() {
     return mIsDualPages ? PAGES_LAST_DUAL : PAGES_LAST;
   }
 
-	@Override
-	public Fragment getItem(int position){
+  @Override
+  public Fragment getItem(int position) {
     int page = QuranInfo.getPageFromPos(position, mIsDualPages);
-	   Timber.d("getting page: %d", page);
-      if (mIsDualPages){
-         return TabletFragment.newInstance(page,
-                mIsShowingTranslation? TabletFragment.Mode.TRANSLATION :
-                        TabletFragment.Mode.ARABIC);
-      } else if (mIsShowingTranslation){
-         return TranslationFragment.newInstance(page);
-      } else {
-        return QuranPageFragment.newInstance(page);
-      }
-	}
-	
-	@Override
-	public void destroyItem(ViewGroup container, int position, Object object){
-      Fragment f = (Fragment)object;
-      if (f instanceof QuranPageFragment){
-         ((QuranPageFragment)f).cleanup();
-      }
-      else if (f instanceof TabletFragment){
-         ((TabletFragment)f).cleanup();
-      }
-	   super.destroyItem(container, position, object);
-	}
+    Timber.d("getting page: %d", page);
+    if (mIsDualPages) {
+      return TabletFragment.newInstance(page,
+          mIsShowingTranslation ? TabletFragment.Mode.TRANSLATION :
+              TabletFragment.Mode.ARABIC);
+    } else if (mIsShowingTranslation) {
+      return TranslationFragment.newInstance(page);
+    } else {
+      return QuranPageFragment.newInstance(page);
+    }
+  }
 
-  public AyahTracker getFragmentIfExistsForPage(int page){
+  @Override
+  public void destroyItem(ViewGroup container, int position, Object object) {
+    Fragment f = (Fragment) object;
+    if (f instanceof QuranPageFragment) {
+      ((QuranPageFragment) f).cleanup();
+    } else if (f instanceof TabletFragment) {
+      ((TabletFragment) f).cleanup();
+    }
+    super.destroyItem(container, position, object);
+  }
+
+  public AyahTracker getFragmentIfExistsForPage(int page) {
     if (page < Constants.PAGES_FIRST || PAGES_LAST < page) {
       return null;
     }
