4196dc42ca06b8cf37751cd214427f12332e10a5
ahmedre <ahmedre@gmail.com>
2016-09-05 15:21:28 -0700

    Update FragmentStatePagerAdapter
    This also removes two of the catch exception blocks since upstream fixes
    likely make them unnecessary.

--- app/src/main/java/com/quran/labs/androidquran/ui/helpers/FragmentStatePagerAdapter.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/helpers/FragmentStatePagerAdapter.java
@@ -18,8 +18,6 @@ package com.quran.labs.androidquran.ui.helpers;
  * Modified for Quran
  * Modifications List (for ease of merging with upstream later on):
  * - added getFragmentIfExists() to return a fragment without recreating it
- * - catch IllegalStateException in destroyItem
- * - catch IllegalStateException in finishUpdate
  */
 
 import android.annotation.SuppressLint;
@@ -30,7 +28,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -39,232 +36,215 @@ import java.util.ArrayList;
 import timber.log.Timber;
 
 /**
- * Implementation of {@link PagerAdapter} that
- * uses a {@link Fragment} to manage each page. This class also handles
- * saving and restoring of fragment's state.
+ * Implementation of {@link PagerAdapter} that uses a {@link Fragment} to manage each page. This
+ * class also handles saving and restoring of fragment's state.
  *
- * <p>This version of the pager is more useful when there are a large number
- * of pages, working more like a list view.  When pages are not visible to
- * the user, their entire fragment may be destroyed, only keeping the saved
- * state of that fragment.  This allows the pager to hold on to much less
- * memory associated with each visited page as compared to
- * {@link FragmentPagerAdapter} at the cost of
- * potentially more overhead when switching between pages.
+ * <p>This version of the pager is more useful when there are a large number of pages, working more
+ * like a list view.  When pages are not visible to the user, their entire fragment may be
+ * destroyed, only keeping the saved state of that fragment.  This allows the pager to hold on to
+ * much less memory associated with each visited page as compared to {@link FragmentPagerAdapter} at
+ * the cost of potentially more overhead when switching between pages.
  *
- * <p>When using FragmentPagerAdapter the host ViewPager must have a
- * valid ID set.</p>
+ * <p>When using FragmentPagerAdapter the host ViewPager must have a valid ID set.</p>
  *
- * <p>Subclasses only need to implement {@link #getItem(int)}
- * and {@link #getCount()} to have a working adapter.
+ * <p>Subclasses only need to implement {@link #getItem(int)} and {@link #getCount()} to have a
+ * working adapter.
  *
- * <p>Here is an example implementation of a pager containing fragments of
- * lists:
+ * <p>Here is an example implementation of a pager containing fragments of lists:
  *
  * {@sample development/samples/Support13Demos/src/com/example/android/supportv13/app/FragmentStatePagerSupport.java
- *      complete}
+ * complete}
  *
  * <p>The <code>R.layout.fragment_pager</code> resource of the top-level fragment is:
  *
- * {@sample development/samples/Support13Demos/res/layout/fragment_pager.xml
- *      complete}
+ * {@sample development/samples/Support13Demos/res/layout/fragment_pager.xml complete}
  *
- * <p>The <code>R.layout.fragment_pager_list</code> resource containing each
- * individual fragment's layout is:
+ * <p>The <code>R.layout.fragment_pager_list</code> resource containing each individual fragment's
+ * layout is:
  *
- * {@sample development/samples/Support13Demos/res/layout/fragment_pager_list.xml
- *      complete}
+ * {@sample development/samples/Support13Demos/res/layout/fragment_pager_list.xml complete}
  */
 public abstract class FragmentStatePagerAdapter extends PagerAdapter {
-   private static final boolean DEBUG = false;
+  private static final boolean DEBUG = false;
 
-   private final FragmentManager mFragmentManager;
-   private FragmentTransaction mCurTransaction = null;
+  private final FragmentManager mFragmentManager;
+  private FragmentTransaction mCurTransaction = null;
 
-   private ArrayList<Fragment.SavedState> mSavedState = new ArrayList<Fragment.SavedState>();
-   private ArrayList<Fragment> mFragments = new ArrayList<Fragment>();
-   private Fragment mCurrentPrimaryItem = null;
+  private ArrayList<Fragment.SavedState> mSavedState = new ArrayList<>();
+  private ArrayList<Fragment> mFragments = new ArrayList<>();
+  private Fragment mCurrentPrimaryItem = null;
 
-   public FragmentStatePagerAdapter(FragmentManager fm) {
-      mFragmentManager = fm;
-   }
+  public FragmentStatePagerAdapter(FragmentManager fm) {
+    mFragmentManager = fm;
+  }
 
-   /**
-    * Return the Fragment associated with a specified position.
-    */
-   public abstract Fragment getItem(int position);
+  /**
+   * Return the Fragment associated with a specified position.
+   */
+  public abstract Fragment getItem(int position);
 
-   @Override
-   public void startUpdate(ViewGroup container) {
-   }
+  @Override
+  public void startUpdate(ViewGroup container) {
+    if (container.getId() == View.NO_ID) {
+      throw new IllegalStateException("ViewPager with adapter " + this
+          + " requires a view id");
+    }
+  }
 
-   /**
-    * gets the fragment at a particular position if it exists
-    * this was added specifically for Quran
-    * @param position the position of the fragment
-    * @return the fragment or null
-    */
-   public Fragment getFragmentIfExists(int position){
-      if (mFragments.size() > position) {
-         Fragment f = mFragments.get(position);
-         if (f != null) {
-            return f;
-         }
+  /**
+   * gets the fragment at a particular position if it exists this was added specifically for Quran
+   *
+   * @param position the position of the fragment
+   * @return the fragment or null
+   */
+  public Fragment getFragmentIfExists(int position) {
+    if (mFragments.size() > position) {
+      Fragment f = mFragments.get(position);
+      if (f != null) {
+        return f;
       }
-      return null;
-   }
+    }
+    return null;
+  }
 
-   @SuppressLint("CommitTransaction")
-   @Override
-   public Object instantiateItem(ViewGroup container, int position) {
-      // If we already have this item instantiated, there is nothing
-      // to do.  This can happen when we are restoring the entire pager
-      // from its saved state, where the fragment manager has already
-      // taken care of restoring the fragments we previously had instantiated.
-      if (mFragments.size() > position) {
-         Fragment f = mFragments.get(position);
-         if (f != null) {
-            return f;
-         }
+  @SuppressLint("CommitTransaction")
+  @Override
+  public Object instantiateItem(ViewGroup container, int position) {
+    // If we already have this item instantiated, there is nothing
+    // to do.  This can happen when we are restoring the entire pager
+    // from its saved state, where the fragment manager has already
+    // taken care of restoring the fragments we previously had instantiated.
+    if (mFragments.size() > position) {
+      Fragment f = mFragments.get(position);
+      if (f != null) {
+        return f;
       }
+    }
 
-      if (mCurTransaction == null) {
-         mCurTransaction = mFragmentManager.beginTransaction();
+    if (mCurTransaction == null) {
+      mCurTransaction = mFragmentManager.beginTransaction();
+    }
+
+    Fragment fragment = getItem(position);
+    if (DEBUG) Timber.v("Adding item #%d: f=%s", position, fragment);
+    if (mSavedState.size() > position) {
+      Fragment.SavedState fss = mSavedState.get(position);
+      if (fss != null) {
+        fragment.setInitialSavedState(fss);
       }
+    }
+    while (mFragments.size() <= position) {
+      mFragments.add(null);
+    }
+    fragment.setMenuVisibility(false);
+    fragment.setUserVisibleHint(false);
+    mFragments.set(position, fragment);
+    mCurTransaction.add(container.getId(), fragment);
 
-      Fragment fragment = getItem(position);
-      if (DEBUG) Timber.v("Adding item #" + position + ": f=" + fragment);
-      if (mSavedState.size() > position) {
-         Fragment.SavedState fss = mSavedState.get(position);
-         if (fss != null) {
-            fragment.setInitialSavedState(fss);
-         }
+    return fragment;
+  }
+
+  @SuppressLint("CommitTransaction")
+  @Override
+  public void destroyItem(ViewGroup container, int position, Object object) {
+    Fragment fragment = (Fragment) object;
+
+    if (mCurTransaction == null) {
+      mCurTransaction = mFragmentManager.beginTransaction();
+    }
+    if (DEBUG) Timber.v("Removing item #%d: f=%s v=%s",
+        position, object, ((Fragment) object).getView());
+    while (mSavedState.size() <= position) {
+      mSavedState.add(null);
+    }
+
+    mSavedState.set(position, fragment.isAdded()
+        ? mFragmentManager.saveFragmentInstanceState(fragment) : null);
+    mFragments.set(position, null);
+
+    mCurTransaction.remove(fragment);
+  }
+
+  @Override
+  public void setPrimaryItem(ViewGroup container, int position, Object object) {
+    Fragment fragment = (Fragment) object;
+    if (fragment != mCurrentPrimaryItem) {
+      if (mCurrentPrimaryItem != null) {
+        mCurrentPrimaryItem.setMenuVisibility(false);
+        mCurrentPrimaryItem.setUserVisibleHint(false);
       }
-      while (mFragments.size() <= position) {
-         mFragments.add(null);
+      if (fragment != null) {
+        fragment.setMenuVisibility(true);
+        fragment.setUserVisibleHint(true);
       }
-      fragment.setMenuVisibility(false);
-      fragment.setUserVisibleHint(false);
-      mFragments.set(position, fragment);
-      mCurTransaction.add(container.getId(), fragment);
+      mCurrentPrimaryItem = fragment;
+    }
+  }
 
-      return fragment;
-   }
+  @Override
+  public void finishUpdate(ViewGroup container) {
+    if (mCurTransaction != null) {
+      mCurTransaction.commitNowAllowingStateLoss();
+      mCurTransaction = null;
+    }
+  }
 
-   @SuppressLint("CommitTransaction")
-   @Override
-   public void destroyItem(ViewGroup container, int position, Object object) {
-      Fragment fragment = (Fragment)object;
+  @Override
+  public boolean isViewFromObject(View view, Object object) {
+    return ((Fragment) object).getView() == view;
+  }
 
-      if (mCurTransaction == null) {
-         mCurTransaction = mFragmentManager.beginTransaction();
+  @Override
+  public Parcelable saveState() {
+    Bundle state = null;
+    if (mSavedState.size() > 0) {
+      state = new Bundle();
+      Fragment.SavedState[] fss = new Fragment.SavedState[mSavedState.size()];
+      mSavedState.toArray(fss);
+      state.putParcelableArray("states", fss);
+    }
+    for (int i = 0; i < mFragments.size(); i++) {
+      Fragment f = mFragments.get(i);
+      if (f != null && f.isAdded()) {
+        if (state == null) {
+          state = new Bundle();
+        }
+        String key = "f" + i;
+        mFragmentManager.putFragment(state, key, f);
       }
-      if (DEBUG) Timber.v("Removing item #" + position + ": f=" + object
-              + " v=" + ((Fragment)object).getView());
-      while (mSavedState.size() <= position) {
-         mSavedState.add(null);
+    }
+    return state;
+  }
+
+  @Override
+  public void restoreState(Parcelable state, ClassLoader loader) {
+    if (state != null) {
+      Bundle bundle = (Bundle) state;
+      bundle.setClassLoader(loader);
+      Parcelable[] fss = bundle.getParcelableArray("states");
+      mSavedState.clear();
+      mFragments.clear();
+      if (fss != null) {
+        for (Parcelable fs : fss) {
+          mSavedState.add((Fragment.SavedState) fs);
+        }
       }
-
-      try {
-         mSavedState.set(position,
-                 mFragmentManager.saveFragmentInstanceState(fragment));
-      }
-      catch (IllegalStateException ise){
-         // when the fragment isn't in the fragment manager, just don't
-         // write saved state for this particular fragment
-         mSavedState.set(position, null);
-      }
-
-      mFragments.set(position, null);
-
-      mCurTransaction.remove(fragment);
-   }
-
-   @Override
-   public void setPrimaryItem(ViewGroup container, int position, Object object) {
-      Fragment fragment = (Fragment)object;
-      if (fragment != mCurrentPrimaryItem) {
-         if (mCurrentPrimaryItem != null) {
-            mCurrentPrimaryItem.setMenuVisibility(false);
-            mCurrentPrimaryItem.setUserVisibleHint(false);
-         }
-         if (fragment != null) {
-            fragment.setMenuVisibility(true);
-            fragment.setUserVisibleHint(true);
-         }
-         mCurrentPrimaryItem = fragment;
-      }
-   }
-
-   @Override
-   public void finishUpdate(ViewGroup container) {
-      if (mCurTransaction != null) {
-         try {
-            mCurTransaction.commitAllowingStateLoss();
-            mCurTransaction = null;
-            mFragmentManager.executePendingTransactions();
-         }
-         catch (IllegalStateException ise){
-            // happens if the activity is gone
-         }
-      }
-   }
-
-   @Override
-   public boolean isViewFromObject(View view, Object object) {
-      return ((Fragment)object).getView() == view;
-   }
-
-   @Override
-   public Parcelable saveState() {
-      Bundle state = null;
-      if (mSavedState.size() > 0) {
-         state = new Bundle();
-         Fragment.SavedState[] fss = new Fragment.SavedState[mSavedState.size()];
-         mSavedState.toArray(fss);
-         state.putParcelableArray("states", fss);
-      }
-      for (int i=0; i<mFragments.size(); i++) {
-         Fragment f = mFragments.get(i);
-         if (f != null && f.isAdded()) {
-            if (state == null) {
-               state = new Bundle();
+      Iterable<String> keys = bundle.keySet();
+      for (String key : keys) {
+        if (key.startsWith("f")) {
+          int index = Integer.parseInt(key.substring(1));
+          Fragment f = mFragmentManager.getFragment(bundle, key);
+          if (f != null) {
+            while (mFragments.size() <= index) {
+              mFragments.add(null);
             }
-            String key = "f" + i;
-            mFragmentManager.putFragment(state, key, f);
-         }
+            f.setMenuVisibility(false);
+            mFragments.set(index, f);
+          } else {
+            Timber.w("Bad fragment at key %s", key);
+          }
+        }
       }
-      return state;
-   }
-
-   @Override
-   public void restoreState(Parcelable state, ClassLoader loader) {
-      if (state != null) {
-         Bundle bundle = (Bundle)state;
-         bundle.setClassLoader(loader);
-         Parcelable[] fss = bundle.getParcelableArray("states");
-         mSavedState.clear();
-         mFragments.clear();
-         if (fss != null) {
-            for (int i=0; i<fss.length; i++) {
-               mSavedState.add((Fragment.SavedState)fss[i]);
-            }
-         }
-         Iterable<String> keys = bundle.keySet();
-         for (String key: keys) {
-            if (key.startsWith("f")) {
-               int index = Integer.parseInt(key.substring(1));
-               Fragment f = mFragmentManager.getFragment(bundle, key);
-               if (f != null) {
-                  while (mFragments.size() <= index) {
-                     mFragments.add(null);
-                  }
-                  f.setMenuVisibility(false);
-                  mFragments.set(index, f);
-               } else {
-                  Timber.w("Bad fragment at key " + key);
-               }
-            }
-         }
-      }
-   }
+    }
+  }
 }
