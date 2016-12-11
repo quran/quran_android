d726ad331db21c8d66d172d69f4cf365d1bccc8c
Shuhrat Dehkanov <k@efir.uz>
2016-12-11 21:35:24 +0900

    Save translator's localized name in db

--- app/src/main/java/com/quran/labs/androidquran/database/TranslationsDBHelper.java
+++ app/src/main/java/com/quran/labs/androidquran/database/TranslationsDBHelper.java
@@ -4,40 +4,53 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class TranslationsDBHelper extends SQLiteOpenHelper {
+import timber.log.Timber;
 
-   private static final String DB_NAME = "translations.db";
-   private static final int DB_VERSION = 1;
+class TranslationsDBHelper extends SQLiteOpenHelper {
 
-   public static class TranslationsTable {
-      public static final String TABLE_NAME = "translations";
-      public static final String ID = "id";
-      public static final String NAME = "name";
-      public static final String TRANSLATOR = "translator";
-      public static final String FILENAME = "filename";
-      public static final String URL = "url";
-      public static final String VERSION = "version";
-   }
+  private static final String DB_NAME = "translations.db";
+  private static final int DB_VERSION = 2;
+  private static final String CREATE_TRANSLATIONS_TABLE =
+      "CREATE TABLE " + TranslationsTable.TABLE_NAME + "(" +
+          TranslationsTable.ID + " integer primary key, " +
+          TranslationsTable.NAME + " varchar not null, " +
+          TranslationsTable.TRANSLATOR + " varchar, " +
+          TranslationsTable.TRANSLATOR_FOREIGN + " varchar, " +
+          TranslationsTable.FILENAME + " varchar not null, " +
+          TranslationsTable.URL + " varchar, " +
+          TranslationsTable.VERSION + " integer not null default 0);";
 
-   private static final String CREATE_TRANSLATIONS_TABLE =
-           "CREATE TABLE " + TranslationsTable.TABLE_NAME + "(" +
-                   TranslationsTable.ID + " integer primary key, " +
-                   TranslationsTable.NAME + " varchar not null, " +
-                   TranslationsTable.TRANSLATOR + " varchar, " +
-                   TranslationsTable.FILENAME + " varchar not null, " +
-                   TranslationsTable.URL + " varchar, " +
-                   TranslationsTable.VERSION + " integer not null default 0);";
+  TranslationsDBHelper(Context context) {
+    super(context, DB_NAME, null, DB_VERSION);
+  }
 
-   public TranslationsDBHelper(Context context) {
-      super(context, DB_NAME, null, DB_VERSION);
-   }
+  @Override
+  public void onCreate(SQLiteDatabase db) {
+    db.execSQL(CREATE_TRANSLATIONS_TABLE);
+  }
 
-   @Override
-   public void onCreate(SQLiteDatabase db) {
+  @Override
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    if (newVersion <= oldVersion) {
+      Timber.w("Can't downgrade from version %d to version %d", oldVersion, newVersion);
+      return;
+    }
+
+    if (oldVersion < 2) {
+      // a new column is added and columns are re-arranged
+      db.execSQL("DROP TABLE IF EXISTS " + TranslationsTable.TABLE_NAME);
       db.execSQL(CREATE_TRANSLATIONS_TABLE);
-   }
+    }
+  }
 
-   @Override
-   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-   }
+  static class TranslationsTable {
+    static final String TABLE_NAME = "translations";
+    static final String ID = "id";
+    static final String NAME = "name";
+    static final String TRANSLATOR = "translator";
+    static final String TRANSLATOR_FOREIGN = "translator_foreign";
+    static final String FILENAME = "filename";
+    static final String URL = "url";
+    static final String VERSION = "version";
+  }
 }
