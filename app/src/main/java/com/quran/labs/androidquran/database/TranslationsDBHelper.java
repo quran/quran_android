deb3436e699dabbc08e545352114fa8f63e4e8e4
Shuhrat Dehkanov <k@efir.uz>
2016-12-12 11:04:41 +0900

    Keep existing data
    Translator's name will be updated next time user downloads the translation

--- app/src/main/java/com/quran/labs/androidquran/database/TranslationsDBHelper.java
+++ app/src/main/java/com/quran/labs/androidquran/database/TranslationsDBHelper.java
@@ -4,21 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import timber.log.Timber;
-
 class TranslationsDBHelper extends SQLiteOpenHelper {
 
   private static final String DB_NAME = "translations.db";
   private static final int DB_VERSION = 2;
-  private static final String CREATE_TRANSLATIONS_TABLE =
-      "CREATE TABLE " + TranslationsTable.TABLE_NAME + "(" +
-          TranslationsTable.ID + " integer primary key, " +
-          TranslationsTable.NAME + " varchar not null, " +
-          TranslationsTable.TRANSLATOR + " varchar, " +
-          TranslationsTable.TRANSLATOR_FOREIGN + " varchar, " +
-          TranslationsTable.FILENAME + " varchar not null, " +
-          TranslationsTable.URL + " varchar, " +
-          TranslationsTable.VERSION + " integer not null default 0);";
 
   TranslationsDBHelper(Context context) {
     super(context, DB_NAME, null, DB_VERSION);
@@ -26,20 +15,34 @@ class TranslationsDBHelper extends SQLiteOpenHelper {
 
   @Override
   public void onCreate(SQLiteDatabase db) {
-    db.execSQL(CREATE_TRANSLATIONS_TABLE);
+    db.execSQL(TranslationsTable.getCreateTableSql(TranslationsTable.TABLE_NAME));
   }
 
   @Override
   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    if (newVersion <= oldVersion) {
-      Timber.w("Can't downgrade from version %d to version %d", oldVersion, newVersion);
-      return;
-    }
-
     if (oldVersion < 2) {
       // a new column is added and columns are re-arranged
-      db.execSQL("DROP TABLE IF EXISTS " + TranslationsTable.TABLE_NAME);
-      db.execSQL(CREATE_TRANSLATIONS_TABLE);
+      final String BACKUP_TABLE = TranslationsTable.TABLE_NAME + "_backup";
+      db.execSQL(TranslationsTable.getCreateTableSql(BACKUP_TABLE));
+
+      db.execSQL("INSERT INTO " + BACKUP_TABLE + " (" +
+          TranslationsTable.ID + ", " +
+          TranslationsTable.NAME + ", " +
+          TranslationsTable.TRANSLATOR + ", " +
+          TranslationsTable.FILENAME + ", " +
+          TranslationsTable.URL + ", " +
+          TranslationsTable.VERSION + ")" +
+          "SELECT " + TranslationsTable.ID + ", " +
+          TranslationsTable.NAME + ", " +
+          TranslationsTable.TRANSLATOR + ", " +
+          TranslationsTable.FILENAME + ", " +
+          TranslationsTable.URL + ", " +
+          TranslationsTable.VERSION +
+          " FROM " + TranslationsTable.TABLE_NAME + ";");
+
+      db.execSQL("DROP TABLE " + TranslationsTable.TABLE_NAME);
+      db.execSQL("ALTER TABLE " + BACKUP_TABLE +
+          " RENAME TO " + TranslationsTable.TABLE_NAME);
     }
   }
 
@@ -52,5 +55,16 @@ class TranslationsDBHelper extends SQLiteOpenHelper {
     static final String FILENAME = "filename";
     static final String URL = "url";
     static final String VERSION = "version";
+
+    static String getCreateTableSql(String tableName) {
+      return "CREATE TABLE " + tableName + "(" +
+          ID + " integer primary key, " +
+          NAME + " varchar not null, " +
+          TRANSLATOR + " varchar, " +
+          TRANSLATOR_FOREIGN + " varchar, " +
+          FILENAME + " varchar not null, " +
+          URL + " varchar, " +
+          VERSION + " integer not null default 0);";
+    }
   }
 }
