b35eb501a1dc089cb6079d6d9c26a1effd9c4690
Ahmed El-Helw <ahmedre@gmail.com>
2016-12-13 01:27:15 -0800

    Support Stetho network interceptor for debug
    This patch adds a separate component for Debug, which overrides the
    NetworkModule with the debug module (that enables the network
    interceptor for Stetho).

---
+++ app/src/debug/java/com/quran/labs/androidquran/module/application/DebugNetworkModule.java
@@ -0,0 +1,28 @@
+package com.quran.labs.androidquran.module.application;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+
+@Module
+public class DebugNetworkModule {
+
+  private static final int DEFAULT_READ_TIMEOUT_SECONDS = 20;
+  private static final int DEFAULT_CONNECT_TIMEOUT_SECONDS = 15;
+
+  @Provides
+  @Singleton
+  OkHttpClient provideOkHttpClient() {
+    return new OkHttpClient.Builder()
+        .readTimeout(DEFAULT_READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+        .connectTimeout(DEFAULT_CONNECT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+        .addNetworkInterceptor(new StethoInterceptor())
+        .build();
+  }
+}
