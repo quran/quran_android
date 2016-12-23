2bb80a36937db1c8dd4b22725a4201fdb1568cd6
Ahmed El-Helw <ahmedre@gmail.com>
2016-12-23 00:39:49 -0800

    Support page number for TranslationPresenter
    Update AbstractTranslationPresenter to allow for any callback type, so
    that inline translations can just have a no-parameter callback, whereas
    the one for page translations can also receive a page parameter in the
    callback (which will be useful for tablet soon insha'Allah).

--- app/src/main/java/com/quran/labs/androidquran/presenter/translation/InlineTranslationPresenter.java
+++ app/src/main/java/com/quran/labs/androidquran/presenter/translation/InlineTranslationPresenter.java
@@ -1,16 +1,33 @@
 package com.quran.labs.androidquran.presenter.translation;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import com.quran.labs.androidquran.common.QuranAyah;
 import com.quran.labs.androidquran.data.VerseRange;
 
-public class InlineTranslationPresenter extends AbstractTranslationPresenter {
+import java.util.List;
+
+public class InlineTranslationPresenter extends
+    AbstractTranslationPresenter<InlineTranslationPresenter.TranslationScreen> {
 
   public InlineTranslationPresenter(Context context) {
     super(context);
   }
 
+  @Override
+  void onData(@Nullable TranslationScreen translationScreen, @NonNull List<QuranAyah> verses) {
+    if (translationScreen != null) {
+      translationScreen.setVerses(verses);
+    }
+  }
+
   public void refresh(VerseRange verseRange, String activeTranslation) {
     getVerses(false, activeTranslation, verseRange);
+  }
+
+  public interface TranslationScreen {
+    void setVerses(@NonNull List<QuranAyah> verses);
   }
 }
