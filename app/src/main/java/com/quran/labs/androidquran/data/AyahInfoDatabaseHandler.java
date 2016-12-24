ec1f91e3aba9bcb7e18f854a3c6ddb911c225a23
Ahmed El-Helw <ahmedre@gmail.com>
2016-12-23 17:36:54 -0800

    Add a provider for providing and caching ayahinfo
    Instead of the previous way, where PagerActivity would attempt to get a
    handle to the database and cache it forever (to be shared by the pages),
    this patch moves the logic to an AyahInfoDatabaseProvider instead.

--- app/src/main/java/com/quran/labs/androidquran/data/AyahInfoDatabaseHandler.java
+++ app/src/main/java/com/quran/labs/androidquran/data/AyahInfoDatabaseHandler.java
@@ -1,83 +1,75 @@
 package com.quran.labs.androidquran.data;
 
-import com.quran.labs.androidquran.database.DatabaseUtils;
-import com.quran.labs.androidquran.util.QuranFileUtils;
-
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.RectF;
 
+import com.quran.labs.androidquran.database.DatabaseUtils;
+import com.quran.labs.androidquran.util.QuranFileUtils;
+
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 public class AyahInfoDatabaseHandler {
 
-	private SQLiteDatabase database = null;
-	public static String COL_PAGE = "page_number";
-	public static String COL_LINE = "line_number";
-	public static String COL_SURA = "sura_number";
-	public static String COL_AYAH = "ayah_number";
-	public static String COL_POSITION = "position";
-	public static String MIN_X = "min_x";
-	public static String MIN_Y = "min_y";
-	public static String MAX_X = "max_x";
-	public static String MAX_Y = "max_y";
-	public static String GLYPHS_TABLE = "glyphs";
+  private static final String COL_PAGE = "page_number";
+  private static final String COL_LINE = "line_number";
+  private static final String COL_SURA = "sura_number";
+  private static final String COL_AYAH = "ayah_number";
+  private static final String COL_POSITION = "position";
+  private static final String MIN_X = "min_x";
+  private static final String MIN_Y = "min_y";
+  private static final String MAX_X = "max_x";
+  private static final String MAX_Y = "max_y";
+  private static final String GLYPHS_TABLE = "glyphs";
 
-  private static Map<String, AyahInfoDatabaseHandler> sHandlerMap = new HashMap<>();
+  private final SQLiteDatabase database;
 
-  public static synchronized AyahInfoDatabaseHandler getDatabaseHandler(
-      Context context, String databaseName) {
-    AyahInfoDatabaseHandler handler = sHandlerMap.get(databaseName);
-    if (handler == null) {
-      handler = new AyahInfoDatabaseHandler(context.getApplicationContext(), databaseName);
-      sHandlerMap.put(databaseName, handler);
+  AyahInfoDatabaseHandler(Context context, String databaseName) throws SQLException {
+    String base = QuranFileUtils.getQuranAyahDatabaseDirectory(context);
+    if (base == null) {
+      database = null;
+    } else {
+      String path = base + File.separator + databaseName;
+      database = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.NO_LOCALIZED_COLLATORS);
     }
-    return handler;
   }
-	
-	private AyahInfoDatabaseHandler(Context context, String databaseName) throws SQLException {
-		String base = QuranFileUtils.getQuranAyahDatabaseDirectory(context);
-		if (base == null) return;
-		String path = base + File.separator + databaseName;
-		database = SQLiteDatabase.openDatabase(path, null,
-				SQLiteDatabase.NO_LOCALIZED_COLLATORS);
-	}
-	
-	public boolean validDatabase(){
-		return database != null && database.isOpen();
-	}
 
-   public Cursor getVersesBoundsForPage(int page){
-      if (!validDatabase()) return null;
-      return database.query(GLYPHS_TABLE,
-              new String[]{ COL_PAGE, COL_LINE, COL_SURA, COL_AYAH,
-                      COL_POSITION, MIN_X, MIN_Y, MAX_X, MAX_Y },
-              COL_PAGE + "=" + page,
-              null, null, null,
-              COL_SURA + "," + COL_AYAH + "," + COL_POSITION);
-   }
-	
-	public RectF getPageBounds(int page) {
-		if (!validDatabase()){ return null; }
+  boolean validDatabase() {
+    return database != null && database.isOpen();
+  }
 
-      Cursor c = null;
-      try {
-         String[] colNames = new String[] {
-               "MIN(" + MIN_X + ")", "MIN(" + MIN_Y + ")",
-               "MAX(" + MAX_X + ")", "MAX(" + MAX_Y + ")"};
-         c = database.query(GLYPHS_TABLE,
-                 colNames, COL_PAGE + "=" + page, null, null, null, null);
-         if (!c.moveToFirst()){ return null; }
-         return new RectF(c.getInt(0), c.getInt(1), c.getInt(2), c.getInt(3));
+  public Cursor getVersesBoundsForPage(int page) {
+    if (!validDatabase()) return null;
+    return database.query(GLYPHS_TABLE,
+        new String[]{ COL_PAGE, COL_LINE, COL_SURA, COL_AYAH,
+            COL_POSITION, MIN_X, MIN_Y, MAX_X, MAX_Y },
+        COL_PAGE + "=" + page,
+        null, null, null,
+        COL_SURA + "," + COL_AYAH + "," + COL_POSITION);
+  }
+
+  public RectF getPageBounds(int page) {
+    if (!validDatabase()) {
+      return null;
+    }
+
+    Cursor c = null;
+    try {
+      String[] colNames = new String[]{
+          "MIN(" + MIN_X + ")", "MIN(" + MIN_Y + ")",
+          "MAX(" + MAX_X + ")", "MAX(" + MAX_Y + ")" };
+      c = database.query(GLYPHS_TABLE,
+          colNames, COL_PAGE + "=" + page, null, null, null, null);
+      if (!c.moveToFirst()) {
+        return null;
       }
-      catch (Exception e){
-         return null;
-      } finally {
-        DatabaseUtils.closeCursor(c);
-      }
-   }
+      return new RectF(c.getInt(0), c.getInt(1), c.getInt(2), c.getInt(3));
+    } catch (Exception e) {
+      return null;
+    } finally {
+      DatabaseUtils.closeCursor(c);
+    }
+  }
 }
