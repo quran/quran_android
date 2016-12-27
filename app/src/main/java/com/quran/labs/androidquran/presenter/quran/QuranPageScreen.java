a4d1098d1e0dfe799ab632568a488f6183d90c69
Ahmed El-Helw <ahmedre@gmail.com>
2016-12-26 17:08:31 -0800

    Move page downloading code into QuranPagePresenter

--- app/src/main/java/com/quran/labs/androidquran/presenter/quran/QuranPageScreen.java
+++ app/src/main/java/com/quran/labs/androidquran/presenter/quran/QuranPageScreen.java
@@ -1,6 +1,9 @@
 package com.quran.labs.androidquran.presenter.quran;
 
 import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 
 import com.quran.labs.androidquran.common.AyahBounds;
 import com.quran.labs.androidquran.dao.Bookmark;
@@ -12,5 +15,7 @@ public interface QuranPageScreen {
   void setBookmarksOnPage(List<Bookmark> bookmarks);
   void setPageCoordinates(int page, RectF pageCoordinates);
   void setAyahCoordinatesError();
+  void setPageImage(int page, @NonNull BitmapDrawable pageDrawable);
+  void setPageDownloadError(@StringRes int errorMessage);
   void setAyahCoordinatesData(int page, Map<String, List<AyahBounds>> coordinates);
 }
