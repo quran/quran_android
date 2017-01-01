6a559c150670ad1e6b082661252ba8e0261b8bf8
Ahmed El-Helw <ahmedre@gmail.com>
2016-12-31 21:04:11 -0800

    Improve TranslationPresenter
    Fix some bugs and cache the translation list instead of getting it on
    every single refresh call.

--- app/src/main/java/com/quran/labs/androidquran/presenter/translation/TranslationPresenter.java
+++ app/src/main/java/com/quran/labs/androidquran/presenter/translation/TranslationPresenter.java
@@ -12,20 +12,26 @@ import com.quran.labs.androidquran.di.QuranPageScope;
 import com.quran.labs.androidquran.model.translation.TranslationModel;
 import com.quran.labs.androidquran.util.QuranSettings;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
+import io.reactivex.Completable;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 @QuranPageScope
 public class TranslationPresenter extends
     AbstractTranslationPresenter<TranslationPresenter.TranslationScreen> {
   private final Integer[] pages;
   private final QuranSettings quranSettings;
+  private final Map<String, LocalTranslation> translationMap;
+
   private TranslationsDBAdapter translationsAdapter;
 
   @Inject
@@ -33,74 +39,90 @@ public class TranslationPresenter extends
                        TranslationModel translationModel,
                        QuranSettings quranSettings,
                        Integer... pages) {
-    super(appContext, translationModel);
+    super(translationModel);
     this.pages = pages;
     this.quranSettings = quranSettings;
+    this.translationMap = new HashMap<>();
     this.translationsAdapter = new TranslationsDBAdapter(appContext);
   }
 
   public void refresh() {
+    if (disposable != null) {
+      disposable.dispose();
+    }
+
     final String activeTranslation = quranSettings.getActiveTranslation();
-    disposable = Observable.zip(
-        Observable.fromCallable(() -> translationsAdapter.getTranslations())
-            .repeat(pages.length)
-            .onErrorReturn(throwable -> new ArrayList<>()),
-        Observable.fromArray(pages)
-            .flatMap(page -> getVerses(quranSettings.wantArabicInTranslationView(),
-                activeTranslation, BaseQuranInfo.getVerseRangeForPage(page))
-                .toObservable()),
-        (translations, ayahs) -> new ResultHolder(ayahs, translations))
-        .map(resultHolder -> {
-          String translator = null;
-          for (int i = 0, size = resultHolder.deviceTranslations.size(); i < size; i++) {
-            LocalTranslation localTranslation = resultHolder.deviceTranslations.get(i);
-            if (localTranslation.filename.equals(activeTranslation)) {
-              translator = localTranslation.translator;
-              break;
-            }
-          }
-          List<QuranAyah> verses = resultHolder.verseTranslations;
-          if (translator != null) {
-            for (int i = 0, size = verses.size(); i < size; i++) {
-              verses.get(i).setTranslator(translator);
-            }
-          }
-          return verses;
-        })
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribeWith(new DisposableObserver<List<QuranAyah>>() {
-          @Override
-          public void onNext(List<QuranAyah> result) {
-            if (translationScreen != null && result.size() > 0) {
-              final int page;
-              if (pages.length == 1) {
-                page = pages[0];
-              } else {
-                QuranAyah ayah = result.get(0);
-                page = QuranInfo.getPageFromSuraAyah(ayah.getSura(), ayah.getAyah());
+    disposable =
+        getTranslationMapCompletable()
+            .andThen(Observable.fromArray(pages)
+                .flatMap(page -> getVerses(quranSettings.wantArabicInTranslationView(),
+                    activeTranslation, BaseQuranInfo.getVerseRangeForPage(page))
+                    .toObservable()))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(new DisposableObserver<List<QuranAyah>>() {
+              @Override
+              public void onNext(List<QuranAyah> result) {
+                if (translationScreen != null && result.size() > 0) {
+                  result = populateTranslator(result, activeTranslation);
+                  translationScreen.setVerses(getPage(result), result);
+                }
               }
-              translationScreen.setVerses(page, result);
-            }
-          }
 
-          @Override
-          public void onError(Throwable e) {
-          }
+              @Override
+              public void onError(Throwable e) {
+              }
 
-          @Override
-          public void onComplete() {
-          }
-        });
+              @Override
+              public void onComplete() {
+              }
+            });
   }
 
-  private static class ResultHolder {
-    final List<QuranAyah> verseTranslations;
-    final List<LocalTranslation> deviceTranslations;
+  private int getPage(List<QuranAyah> result) {
+    final int page;
+    if (pages.length == 1) {
+      page = pages[0];
+    } else {
+      QuranAyah ayah = result.get(0);
+      page = QuranInfo.getPageFromSuraAyah(ayah.getSura(), ayah.getAyah());
+    }
+    return page;
+  }
 
-    private ResultHolder(List<QuranAyah> verseTranslations,
-                         List<LocalTranslation> deviceTranslations) {
-      this.verseTranslations = verseTranslations;
-      this.deviceTranslations = deviceTranslations;
+  private List<QuranAyah> populateTranslator(List<QuranAyah> result, String activeTranslation) {
+    LocalTranslation localTranslation = translationMap.get(activeTranslation);
+    String translator = localTranslation == null ?
+        null : localTranslation.translator;
+    if (translator != null && result.size() > 0) {
+      for (int i = 0, size = result.size(); i < size; i++) {
+        QuranAyah ayah = result.get(i);
+        ayah.setTranslator(translator);
+      }
+    }
+    return result;
+  }
+
+  @NonNull
+  private Completable getTranslationMapCompletable() {
+    if (this.translationMap.size() == 0) {
+      return Single.fromCallable(() -> translationsAdapter.getTranslations())
+          .map(translations -> {
+            Map<String, LocalTranslation> map = new HashMap<>();
+            for (int i = 0, size = translations.size(); i < size; i++) {
+              LocalTranslation translation = translations.get(i);
+              map.put(translation.filename, translation);
+            }
+            return map;
+          })
+          .subscribeOn(Schedulers.io())
+          .observeOn(AndroidSchedulers.mainThread())
+          .doOnSuccess(map -> {
+            this.translationMap.clear();
+            this.translationMap.putAll(map);
+          })
+          .toCompletable();
+    } else {
+      return Completable.complete();
     }
   }
 
