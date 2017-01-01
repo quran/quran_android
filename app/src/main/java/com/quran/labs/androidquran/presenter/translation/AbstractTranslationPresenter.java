63dd139dcf6facc4e7903824da2fa7679638a4ee
Ahmed El-Helw <ahmedre@gmail.com>
2016-12-31 21:04:11 -0800

    Improve TranslationPresenter
    Fix some bugs and cache the translation list instead of getting it on
    every single refresh call.

--- app/src/main/java/com/quran/labs/androidquran/presenter/translation/AbstractTranslationPresenter.java
+++ app/src/main/java/com/quran/labs/androidquran/presenter/translation/AbstractTranslationPresenter.java
@@ -1,13 +1,11 @@
 package com.quran.labs.androidquran.presenter.translation;
 
-import android.content.Context;
 import android.support.annotation.Nullable;
 
 import com.quran.labs.androidquran.common.QuranAyah;
 import com.quran.labs.androidquran.data.VerseRange;
 import com.quran.labs.androidquran.model.translation.TranslationModel;
 import com.quran.labs.androidquran.presenter.Presenter;
-import com.quran.labs.androidquran.util.QuranSettings;
 
 import java.util.List;
 
@@ -16,24 +14,18 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 abstract class AbstractTranslationPresenter<T> implements Presenter<T> {
-  private final Context appContext;
   private final TranslationModel translationModel;
 
   @Nullable T translationScreen;
   Disposable disposable;
 
-  AbstractTranslationPresenter(Context appContext, TranslationModel translationModel) {
-    this.appContext = appContext;
+  AbstractTranslationPresenter(TranslationModel translationModel) {
     this.translationModel = translationModel;
   }
 
   Single<List<QuranAyah>> getVerses(boolean getArabic,
                                     String activeTranslation,
                                     VerseRange verseRange) {
-    if (disposable != null) {
-      disposable.dispose();
-    }
-
     if (getArabic) {
       return Single.zip(
           translationModel.getArabicFromDatabase(verseRange),
@@ -49,8 +41,7 @@ abstract class AbstractTranslationPresenter<T> implements Presenter<T> {
           })
           .subscribeOn(Schedulers.io());
     } else {
-      return translationModel.getTranslationFromDatabase(
-          verseRange, QuranSettings.getInstance(appContext).getActiveTranslation())
+      return translationModel.getTranslationFromDatabase(verseRange, activeTranslation)
           .subscribeOn(Schedulers.io());
     }
   }
