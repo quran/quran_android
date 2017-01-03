2f84a1efa27e8901b5d972647da30bf14ccae722
Hosain <contact@hosain.net>
2017-01-03 23:41:32 +0000

    #750 - Added basic test QuranActivityTest:
    - Click on Surah in list view an ensure it opens Surah by checking triggered intents
    - Removed unused code

--- app/src/androidTest/java/com/quran/labs/androidquran/BaseActivityTest.java
+++ app/src/androidTest/java/com/quran/labs/androidquran/BaseActivityTest.java
@@ -1,40 +1,20 @@
 package com.quran.labs.androidquran;
 
 import android.app.Activity;
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-
-import java.io.File;
 
 @RunWith(AndroidJUnit4.class)
 public abstract class BaseActivityTest<T extends Activity> {
 
   @Rule
-  public ActivityTestRule<T> rule;
-
-  @Before
-  public void setup() {
-    resetData();
-  }
+  public IntentsTestRule<T> rule;
 
   public BaseActivityTest(
       Class<T> activityClass) {
-    rule = new ActivityTestRule<>(activityClass, true, false);
-  }
-
-  private void resetData() {
-    File root = InstrumentationRegistry.getTargetContext().getFilesDir().getParentFile();
-    String[] sharedPreferencesFileNames = new File(root, "shared_prefs").list();
-    for (String fileName : sharedPreferencesFileNames) {
-      InstrumentationRegistry.getTargetContext()
-          .getSharedPreferences(fileName.replace(".xml", ""), Context.MODE_PRIVATE).edit().clear()
-          .commit();
-    }
+    rule = new IntentsTestRule<>(activityClass, true, false);
   }
 }
