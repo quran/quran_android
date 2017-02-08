quran/quran_android

commit f7cb2397f447c452daf33257b71b96e641c580ba

Fix an off by one crash when there is a -1 page

app/src/main/java/com/quran/labs/androidquran/util/AudioUtils.java
@@ -23,6 +23,7 @@ import java.util.Locale;
 
 import timber.log.Timber;
 
+import static com.quran.labs.androidquran.data.Constants.PAGES_FIRST;
 import static com.quran.labs.androidquran.data.Constants.PAGES_LAST;
 
 public class AudioUtils {
@@ -139,12 +140,12 @@ public class AudioUtils {
 
     int pageLastSura = 114;
     int pageLastAyah = 6;
-    if (page > PAGES_LAST || page < 0) {
+    if (page > PAGES_LAST || page < PAGES_FIRST) {
       return null;
     }
     if (page < PAGES_LAST) {
-      int nextPageSura = QuranInfo.PAGE_SURA_START[page];
-      int nextPageAyah = QuranInfo.PAGE_AYAH_START[page];
+      int nextPageSura = QuranInfo.safelyGetSuraOnPage(page);
+      int nextPageAyah = QuranInfo.PAGE_AYAH_START[page - 1];
 
       pageLastSura = nextPageSura;
       pageLastAyah = nextPageAyah - 1;

app/src/main/java/com/quran/labs/androidquran/data/BaseQuranInfo.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
+import com.crashlytics.android.Crashlytics;
 import com.quran.labs.androidquran.R;
 import com.quran.labs.androidquran.util.QuranUtils;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import static com.quran.labs.androidquran.data.Constants.PAGES_FIRST;
 import static com.quran.labs.androidquran.data.Constants.PAGES_LAST;
 import static com.quran.labs.androidquran.data.Constants.PAGES_LAST_DUAL;
 
@@ -198,6 +200,14 @@ public class BaseQuranInfo {
       }
     }
     return bounds;
+  }
+
+  public static int safelyGetSuraOnPage(int page) {
+    if (page < PAGES_FIRST || page > PAGES_LAST) {
+      Crashlytics.logException(new IllegalArgumentException("got page: " + page));
+      page = 1;
+    }
+    return PAGE_SURA_START[page - 1];
   }
 
   public static String getSuraNameFromPage(Context context, int page) {

app/src/main/java/com/quran/labs/androidquran/database/BookmarksDBHelper.java
@@ -138,7 +138,7 @@ class BookmarksDBHelper extends SQLiteOpenHelper {
 
   private void upgradeToVer3(SQLiteDatabase db) {
     db.execSQL(LAST_PAGES_TABLE);
-    if (this.lastPage != Constants.NO_PAGE) {
+    if (this.lastPage >= Constants.PAGES_FIRST && this.lastPage <= Constants.PAGES_LAST) {
       db.execSQL("INSERT INTO last_pages(page) values(?)", new Object[] { lastPage });
     }
   }

app/src/main/java/com/quran/labs/androidquran/presenter/translation/TranslationPresenter.java
@@ -3,7 +3,6 @@ package com.quran.labs.androidquran.presenter.translation;
 import android.support.annotation.NonNull;
 
 import com.quran.labs.androidquran.common.QuranAyahInfo;
-import com.quran.labs.androidquran.data.BaseQuranInfo;
 import com.quran.labs.androidquran.data.QuranInfo;
 import com.quran.labs.androidquran.database.TranslationsDBAdapter;
 import com.quran.labs.androidquran.di.QuranPageScope;
@@ -41,7 +40,7 @@ public class TranslationPresenter extends
 
     disposable = Observable.fromArray(pages)
         .flatMap(page -> getVerses(quranSettings.wantArabicInTranslationView(),
-            getTranslations(quranSettings), BaseQuranInfo.getVerseRangeForPage(page))
+            getTranslations(quranSettings), QuranInfo.getVerseRangeForPage(page))
             .toObservable())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribeWith(new DisposableObserver<ResultHolder>() {

app/src/main/java/com/quran/labs/androidquran/ui/PagerActivity.java
@@ -1394,7 +1394,7 @@ public class PagerActivity extends QuranActionBarActivity implements
       page = ((PAGES_LAST_DUAL - position) * 2) - 1;
     }
 
-    int startSura = QuranInfo.PAGE_SURA_START[page - 1];
+    int startSura = QuranInfo.safelyGetSuraOnPage(page);
     int startAyah = QuranInfo.PAGE_AYAH_START[page - 1];
     playFromAyah(page, startSura, startAyah, false);
   }

app/src/main/java/com/quran/labs/androidquran/ui/fragment/SuraListFragment.java
@@ -74,7 +74,7 @@ public class SuraListFragment extends Fragment {
             @Override
             public void onSuccess(Integer recentPage) {
               if (recentPage != Constants.NO_PAGE) {
-                int sura = QuranInfo.PAGE_SURA_START[recentPage - 1];
+                int sura = QuranInfo.safelyGetSuraOnPage(recentPage);
                 int juz = QuranInfo.getJuzFromPage(recentPage);
                 int position = sura + juz - 1;
                 mRecyclerView.scrollToPosition(position);

app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranRowFactory.java
@@ -32,7 +32,7 @@ public class QuranRowFactory {
     return new QuranRow.Builder()
         .withText(QuranInfo.getSuraNameString(context, page))
         .withMetadata(QuranInfo.getPageSubtitle(context, page))
-        .withSura(QuranInfo.PAGE_SURA_START[page-1])
+        .withSura(QuranInfo.safelyGetSuraOnPage(page))
         .withPage(page)
         .withImageResource(R.drawable.bookmark_currentpage).build();
   }

commit ec1ad30433f34e6b29d95b6c002d505e431e8624

Fix play button not working
The bug was introduced in f7cb2397f447c452daf33257b71b96e641c580ba,
where the "next page" ayah was changed to use the previous page's ayah.
This caused us to try to play a range of ayat with an end less than
start, which caused nothing to play at all. This fixes #801.

app/src/main/java/com/quran/labs/androidquran/util/AudioUtils.java
@@ -23,7 +23,6 @@ import java.util.Locale;
 
 import timber.log.Timber;
 
-import static com.quran.labs.androidquran.data.Constants.PAGES_FIRST;
 import static com.quran.labs.androidquran.data.Constants.PAGES_LAST;
 
 public class AudioUtils {
@@ -140,12 +139,14 @@ public class AudioUtils {
 
     int pageLastSura = 114;
     int pageLastAyah = 6;
-    if (page > PAGES_LAST || page < PAGES_FIRST) {
+    // page < 0 - intentional, because nextPageAyah looks up the ayah on the next page
+    if (page > PAGES_LAST || page < 0) {
       return null;
     }
     if (page < PAGES_LAST) {
       int nextPageSura = QuranInfo.safelyGetSuraOnPage(page);
-      int nextPageAyah = QuranInfo.PAGE_AYAH_START[page - 1];
+      // not using [page-1] as an index because we literally want the next page
+      int nextPageAyah = QuranInfo.PAGE_AYAH_START[page];
 
       pageLastSura = nextPageSura;
       pageLastAyah = nextPageAyah - 1;