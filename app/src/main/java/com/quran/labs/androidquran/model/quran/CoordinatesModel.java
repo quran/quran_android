eb9d4f882e78b1b9a522d632a333644a88ff9785
Ahmed El-Helw <ahmedre@gmail.com>
2017-07-22 14:04:30 +0400

    Improve implementation of coordinate merging code

--- app/src/main/java/com/quran/labs/androidquran/model/quran/CoordinatesModel.java
+++ app/src/main/java/com/quran/labs/androidquran/model/quran/CoordinatesModel.java
@@ -3,6 +3,7 @@ package com.quran.labs.androidquran.model.quran;
 import android.graphics.RectF;
 import android.support.v4.util.Pair;
 
+import com.crashlytics.android.Crashlytics;
 import com.quran.labs.androidquran.common.AyahBounds;
 import com.quran.labs.androidquran.data.AyahInfoDatabaseHandler;
 import com.quran.labs.androidquran.data.AyahInfoDatabaseProvider;
@@ -61,116 +62,95 @@ public class CoordinatesModel {
     final Set<String> keys = original.keySet();
     for (String key : keys) {
       List<AyahBounds> normalBounds = original.get(key);
-      normalizedMap.put(key, normalizeAyahBounds(normalBounds));
+      normalizedMap.put(key, normalizeAyahBounds(key, normalBounds));
     }
     return normalizedMap;
   }
 
-  private List<AyahBounds> normalizeAyahBounds(List<AyahBounds> ayahBounds) {
-    int total = ayahBounds.size();
+  private List<AyahBounds> normalizeAyahBounds(String key, List<AyahBounds> ayahBounds) {
+    final int total = ayahBounds.size();
     if (total < 2) {
-      // only one line, nothing to normalize
       return ayahBounds;
-    }
-
-    AyahBounds middle = null;
-    for (int i = 1; i < (total - 1); i++) {
-      if (middle == null) {
-        middle = ayahBounds.get(i);
-      } else {
+    } else if (total < 3) {
+      return consolidate(ayahBounds.get(0), ayahBounds.get(1));
+    } else {
+      AyahBounds middle = ayahBounds.get(1);
+      for (int i = 2; i < total - 1; i++) {
         middle.engulf(ayahBounds.get(i));
       }
-    }
 
-    AyahBounds first = ayahBounds.get(0);
-    AyahBounds last = ayahBounds.get(total - 1);
+      List<AyahBounds> top = consolidate(ayahBounds.get(0), middle);
+      final int topSize = top.size();
+      // the first parameter is essentially middle (after its consolidation with the top line)
+      List<AyahBounds> bottom = consolidate(top.get(topSize - 1), ayahBounds.get(total - 1));
 
-    RectF firstRect = first.getBounds();
-    RectF lastRect = last.getBounds();
-    if (middle != null) {
-      // the middle bounds must, by definition, be full lines, and the first line must, by
-      // definition, reach the end of the line. normalize these to be the least x value.
-      RectF middleRect = middle.getBounds();
-      float bestEndOfLine = Math.min(firstRect.left, middleRect.left);
-
-      // also take into account the last line if it it goes to the very end (otherwise, it must
-      // be greater than bestEndOfLine, so this will have no effect)
-      bestEndOfLine = Math.min(bestEndOfLine, lastRect.left);
-      firstRect.left = bestEndOfLine;
-      middleRect.left = bestEndOfLine;
-
-      // update the left of the last row only if it's a full row
-      if (Math.abs(lastRect.left - bestEndOfLine) < PIXEL_THRESHOLD) {
-        lastRect.left = bestEndOfLine;
-      }
-
-      // let the top line touch the middle line
-      firstRect.bottom = middleRect.top;
-
-      // the last line must, by definition, start at the right hand side of the screen, so
-      // normalize that with the right side of the middle such that they are the max x value.
-      float bestStartOfLine = Math.max(lastRect.right, middleRect.right);
-
-      // also take into account the first line if it fills the entire line. if it doesn't, then
-      // the right must be less than bestStartOfLine, so this will have no effect.
-      bestStartOfLine = Math.max(bestStartOfLine, firstRect.right);
-      lastRect.right = bestStartOfLine;
-      middleRect.right = bestStartOfLine;
-
-      // update the right of the first row only if it's a full row
-      if (Math.abs(firstRect.right - bestStartOfLine) < PIXEL_THRESHOLD) {
-        firstRect.right = bestStartOfLine;
-      }
-
-      // let the bottom line touch the middle line
-      lastRect.top = middleRect.bottom;
-
-      // get the updated ayah bounds
-      first = first.withBounds(firstRect);
-      middle = middle.withBounds(middleRect);
-      last = last.withBounds(lastRect);
-    } else {
-      // only 2 lines - let's see if any of them are full lines
-      boolean firstIsFullLine = Math.abs(firstRect.right - lastRect.right) < PIXEL_THRESHOLD;
-      boolean secondIsFullLine = Math.abs(firstRect.left - lastRect.left) < PIXEL_THRESHOLD;
-      if (firstIsFullLine && secondIsFullLine) {
-        first.engulf(last);
-        return Collections.singletonList(first);
-      } else if (firstIsFullLine) {
-        lastRect.top = firstRect.bottom;
-        float bestStartOfLine = Math.max(firstRect.right, lastRect.right);
-        firstRect.right = bestStartOfLine;
-        lastRect.right = bestStartOfLine;
-
-        first = first.withBounds(firstRect);
-        last = last.withBounds(lastRect);
-      } else if (secondIsFullLine) {
-        firstRect.bottom = lastRect.top;
-        float bestEndOfLine = Math.min(firstRect.left, lastRect.left);
-        firstRect.left = bestEndOfLine;
-        lastRect.left = bestEndOfLine;
-
-        first = first.withBounds(firstRect);
-        last = last.withBounds(lastRect);
+      List<AyahBounds> result = new ArrayList<>();
+      if (topSize == 1) {
+        return bottom;
+      } else if (topSize + bottom.size() > 4) {
+        Crashlytics.log("current verse: " + key);
+        Crashlytics.logException(new IllegalStateException("Lines not properly joined for verse"));
+        result.addAll(top);
+        result.addAll(bottom);
+        return result;
       } else {
-        // neither one is a full line, let's generate a middle entry to join them if they have
-        // anything in common (i.e. any part of them intersects)
-        if (lastRect.left < firstRect.right) {
-          RectF middleBounds = new RectF(lastRect.left,
-              firstRect.bottom,
-              firstRect.right,
-              lastRect.top);
-          middle = new AyahBounds(first.getLine(), first.getPosition(), middleBounds);
+        // re-consolidate top and middle again, since middle may have changed
+        top = consolidate(top.get(0), bottom.get(0));
+        result.addAll(top);
+        if (bottom.size() > 1) {
+          result.add(bottom.get(1));
         }
+        return result;
+      }
+    }
+  }
+
+  private List<AyahBounds> consolidate(AyahBounds top, AyahBounds bottom) {
+    final RectF firstRect = top.getBounds();
+    final RectF lastRect = bottom.getBounds();
+
+    AyahBounds middle = null;
+
+    // only 2 lines - let's see if any of them are full lines
+    boolean firstIsFullLine = Math.abs(firstRect.right - lastRect.right) < PIXEL_THRESHOLD;
+    boolean secondIsFullLine = Math.abs(firstRect.left - lastRect.left) < PIXEL_THRESHOLD;
+    if (firstIsFullLine && secondIsFullLine) {
+      top.engulf(bottom);
+      return Collections.singletonList(top);
+    } else if (firstIsFullLine) {
+      lastRect.top = firstRect.bottom;
+      float bestStartOfLine = Math.max(firstRect.right, lastRect.right);
+      firstRect.right = bestStartOfLine;
+      lastRect.right = bestStartOfLine;
+
+      top = top.withBounds(firstRect);
+      bottom = bottom.withBounds(lastRect);
+    } else if (secondIsFullLine) {
+      firstRect.bottom = lastRect.top;
+      float bestEndOfLine = Math.min(firstRect.left, lastRect.left);
+      firstRect.left = bestEndOfLine;
+      lastRect.left = bestEndOfLine;
+
+      top = top.withBounds(firstRect);
+      bottom = bottom.withBounds(lastRect);
+    } else {
+      // neither one is a full line, let's generate a middle entry to join them if they have
+      // anything in common (i.e. any part of them intersects)
+      if (lastRect.left < firstRect.right) {
+        RectF middleBounds = new RectF(lastRect.left,
+            firstRect.bottom,
+            firstRect.right,
+            lastRect.top);
+        middle = new AyahBounds(top.getLine(), top.getPosition(), middleBounds);
       }
     }
 
     List<AyahBounds> result = new ArrayList<>();
-    result.add(first);
+    result.add(top);
     if (middle != null) {
       result.add(middle);
     }
-    result.add(last);
+    result.add(bottom);
     return result;
   }
 }
