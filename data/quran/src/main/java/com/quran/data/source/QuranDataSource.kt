bfa0f08990141571c5b93fd24e4c261300469fc3
Ahmed El-Helw <ahmedre@gmail.com>
2018-02-03 13:35:45 +0400

    Add quarter start by page and sura for page arrays
    This patch adds the quarter start by page and sura for page arrays into
    the data source classes. They were excluded initially since the data can
    be derived using the existing arrays, but insha'Allah this can be
    revisited in the future.

--- data/quran/src/main/java/com/quran/data/source/QuranDataSource.kt
+++ data/quran/src/main/java/com/quran/data/source/QuranDataSource.kt
@@ -3,9 +3,11 @@ package com.quran.data.source
 interface QuranDataSource {
   fun getNumberOfPages() : Int
   fun getPageForSuraArray() : IntArray
+  fun getSuraForPageArray() : IntArray
   fun getAyahForPageArray() : IntArray
   fun getPageForJuzArray() : IntArray
   fun getNumberOfAyahsForSuraArray() : IntArray
   fun getIsMakkiBySuraArray() : BooleanArray
+  fun getQuarterStartByPage() : IntArray
   fun getQuartersArray() : Array<IntArray>
 }
