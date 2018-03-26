73ddae8317287ec33ecb5a4c04f1ee3ed34e49ba
Ahmed Kamal <ahmed.kamal@badrit.com>
2018-03-26 12:05:06 +0400

    Fix next page bug with a unit test validation (#938)
    - Fix the page index bug and remove unnecessary code
    - Change the scope of MadaniDataSource for unit testing
    - Add a simple unit test for this case
    
    Fixes #935

---
+++ app/src/test/java/com/quran/labs/androidquran/util/AudioUtilsTest.java
@@ -0,0 +1,30 @@
+package com.quran.labs.androidquran.util;
+
+import com.quran.data.source.PageProvider;
+import com.quran.data.pageinfo.common.MadaniDataSource;
+
+import com.quran.labs.androidquran.data.QuranInfo;
+import com.quran.labs.androidquran.data.SuraAyah;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class AudioUtilsTest {
+
+  @Test
+  public void testGetLastAyahWithNewSurahOnNextPageForMadani() throws Exception {
+    PageProvider pageProviderMock = Mockito.mock(PageProvider.class);
+    when(pageProviderMock.getDataSource())
+        .thenReturn(new MadaniDataSource());
+
+    QuranInfo quranInfoMock = new QuranInfo(pageProviderMock);
+
+    AudioUtils audioUtils = new AudioUtils(quranInfoMock, null);
+    SuraAyah lastAyah = audioUtils.getLastAyahToPlay(new SuraAyah(109, 1), 603, 1, false);
+    assertTrue(lastAyah.ayah == 5);
+    assertTrue(lastAyah.sura == 111);
+  }
+}
