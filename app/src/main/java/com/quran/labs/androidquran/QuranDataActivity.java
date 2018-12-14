9df9ac5c592c50eac7fb3eec13726f498eaa1582
Ahmed El-Helw <ahmedre@gmail.com>
2018-12-14 16:03:03 +0400

    Rename QuranPageChecker for clarity (#1020)

--- app/src/main/java/com/quran/labs/androidquran/QuranDataActivity.java
+++ app/src/main/java/com/quran/labs/androidquran/QuranDataActivity.java
@@ -22,7 +22,7 @@ import com.quran.labs.androidquran.service.util.QuranDownloadNotifier;
 import com.quran.labs.androidquran.service.util.ServiceIntentHelper;
 import com.quran.labs.androidquran.ui.QuranActivity;
 import com.quran.labs.androidquran.util.QuranFileUtils;
-import com.quran.labs.androidquran.util.QuranPageChecker;
+import com.quran.labs.androidquran.util.QuranPartialPageChecker;
 import com.quran.labs.androidquran.util.QuranScreenInfo;
 import com.quran.labs.androidquran.util.QuranSettings;
 
@@ -59,7 +59,7 @@ public class QuranDataActivity extends Activity implements
   @Inject QuranFileUtils quranFileUtils;
   @Inject QuranScreenInfo quranScreenInfo;
   @Inject PageProvider quranPageProvider;
-  @Inject QuranPageChecker quranPageChecker;
+  @Inject QuranPartialPageChecker quranPartialPageChecker;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -375,7 +375,7 @@ public class QuranDataActivity extends Activity implements
       final String width = quranScreenInfo.getWidthParam();
       if (quranScreenInfo.isDualPageMode()) {
         final String tabletWidth = quranScreenInfo.getTabletWidthParam();
-        quranPageChecker.checkPages(latestImagesVersion, totalPages, width, tabletWidth);
+        quranPartialPageChecker.checkPages(latestImagesVersion, totalPages, width, tabletWidth);
         boolean haveLandscape = quranFileUtils.haveAllImages(appContext, tabletWidth, totalPages);
         boolean havePortrait = tabletWidth.equals(width) ? haveLandscape :
             quranFileUtils.haveAllImages(appContext, width, totalPages);
@@ -396,7 +396,7 @@ public class QuranDataActivity extends Activity implements
         }
         return haveLandscape && havePortrait;
       } else {
-        quranPageChecker.checkPages(latestImagesVersion, totalPages, width, width);
+        quranPartialPageChecker.checkPages(latestImagesVersion, totalPages, width, width);
         boolean haveAll = quranFileUtils.haveAllImages(appContext,
             quranScreenInfo.getWidthParam(), totalPages);
         Timber.d("checkPages: have all images: %s", haveAll ? "yes" : "no");
