a62999f4b65ec1b093115ffae0bb5f5506710be3
Ahmed El-Helw <ahmedre@gmail.com>
2019-04-28 17:34:18 +0400

    Remove stetho dependency (#1159)
    This is mainly being removed to avoid becoming multidex, since the next
    patch will add a small library as a dependency. Can consider adding this
    (or Flipper) back once the app is multidex.

--- app/src/debug/java/com/quran/labs/androidquran/DebugApplication.java
+++ app/src/debug/java/com/quran/labs/androidquran/DebugApplication.java
@@ -1,9 +1,5 @@
 package com.quran.labs.androidquran;
 
-import com.facebook.stetho.Stetho;
-import com.quran.labs.androidquran.component.application.ApplicationComponent;
-import com.quran.labs.androidquran.component.application.DaggerDebugApplicationComponent;
-import com.quran.labs.androidquran.module.application.ApplicationModule;
 import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
@@ -16,15 +12,7 @@ public class DebugApplication extends QuranApplication {
 
     if (!LeakCanary.isInAnalyzerProcess(this)) {
       Timber.plant(new Timber.DebugTree());
-      Stetho.initializeWithDefaults(this);
       LeakCanary.install(this);
     }
-  }
-
-  @Override
-  protected ApplicationComponent initializeInjector() {
-    return DaggerDebugApplicationComponent.builder()
-        .applicationModule(new ApplicationModule(this))
-        .build();
   }
 }
