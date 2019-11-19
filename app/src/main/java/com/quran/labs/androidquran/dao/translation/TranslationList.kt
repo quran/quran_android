29feae2ad5b934bd51261924289f99d6b40ee916
Ahmed El-Helw <ahmedre@gmail.com>
2019-11-20 00:29:08 +0400

    Upgrade Moshi and enable codegen (#1227)

--- app/src/main/java/com/quran/labs/androidquran/dao/translation/TranslationList.kt
+++ app/src/main/java/com/quran/labs/androidquran/dao/translation/TranslationList.kt
@@ -1,5 +1,7 @@
 package com.quran.labs.androidquran.dao.translation
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class TranslationList(@field:Json(name = "data") val translations: List<Translation>)
