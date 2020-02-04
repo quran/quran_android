quran/quran_android

commit a3b49167cb32bc6b720ec9944913eef70807208a

Suppress many non-actionable warnings (#1247)
This also checks for connectivity before trying to download pages, since
many silently reported crashes come from this. This should also be
checked in other places in the future.

app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranPageWorker.java
@@ -1,6 +1,8 @@
 package com.quran.labs.androidquran.ui.helpers;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
@@ -42,6 +44,12 @@ public class QuranPageWorker {
     Response response = null;
     OutOfMemoryError oom = null;
 
+    if (!isNetworkAvailable()) {
+      final Response noNetworkResponse = new Response(Response.ERROR_NO_INTERNET);
+      noNetworkResponse.setPageData(pageNumber);
+      return noNetworkResponse;
+    }
+
     try {
       response = QuranDisplayHelper.getQuranPage(
           okHttpClient, appContext, imageWidth, pageNumber, quranFileUtils);
@@ -77,6 +85,13 @@ public class QuranPageWorker {
 
     response.setPageData(pageNumber);
     return response;
+  }
+
+  private boolean isNetworkAvailable() {
+    final ConnectivityManager connectivityManager =
+        (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+    final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+    return networkInfo != null && networkInfo.isConnected();
   }
 
   public Observable<Response> loadPages(Integer... pages) {

app/src/main/java/com/quran/labs/androidquran/common/Response.java
@@ -6,8 +6,9 @@ public class Response {
   public static final int ERROR_SD_CARD_NOT_FOUND = 1;
   public static final int ERROR_FILE_NOT_FOUND = 2;
   public static final int ERROR_DOWNLOADING_ERROR = 3;
-  public static final int WARN_SD_CARD_NOT_FOUND = 4;
-  public static final int WARN_COULD_NOT_SAVE_FILE = 5;
+  public static final int ERROR_NO_INTERNET = 4;
+  public static final int WARN_SD_CARD_NOT_FOUND = 5;
+  public static final int WARN_COULD_NOT_SAVE_FILE = 6;
 
   private Bitmap bitmap;
   private int errorCode;

app/src/main/java/com/quran/labs/androidquran/presenter/quran/QuranPagePresenter.java
@@ -153,6 +153,7 @@ public class QuranPagePresenter implements Presenter<QuranPageScreen> {
                   case Response.ERROR_SD_CARD_NOT_FOUND:
                     errorRes = R.string.sdcard_error;
                     break;
+                  case Response.ERROR_NO_INTERNET:
                   case Response.ERROR_DOWNLOADING_ERROR:
                     errorRes = R.string.download_error_network;
                     break;

app/src/main/java/com/quran/labs/androidquran/presenter/translation/TranslationManagerPresenter.java
@@ -20,6 +20,9 @@ import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -115,7 +118,10 @@ public class TranslationManagerPresenter implements Presenter<TranslationManager
 
           @Override
           public void onError(Throwable e) {
-            Timber.e(e, "error updating translations list");
+            if (!(e instanceof IOException)) {
+              Timber.e(e, "error updating translations list");
+            }
+
             if (currentActivity != null) {
               currentActivity.onErrorDownloadTranslations();
             }

app/src/main/java/com/quran/labs/androidquran/service/AudioService.java
@@ -437,7 +437,7 @@ public class AudioService extends Service implements OnCompletionListener,
       processRewindRequest();
     } else if (ACTION_UPDATE_REPEAT.equals(action)) {
       final AudioRequest playInfo = intent.getParcelableExtra(EXTRA_PLAY_INFO);
-      if (playInfo != null) {
+      if (playInfo != null && audioQueue != null) {
         audioQueue = audioQueue.withUpdatedAudioRequest(playInfo);
         audioRequest = playInfo;
       }

app/src/main/java/com/quran/labs/androidquran/service/QuranDownloadService.java
@@ -29,6 +29,8 @@ import com.quran.labs.androidquran.util.ZipUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -615,7 +617,7 @@ public class QuranDownloadService extends Service implements
         Timber.e(new Exception("Unable to download file - code: " + response.code()));
       }
     } catch (IOException exception) {
-      Timber.e(exception, "Failed to download file");
+      Timber.d(exception, "Failed to download file");
     } catch (SecurityException se) {
       Timber.e(se, "Security exception while downloading file");
     } finally {

commit a9e1040eb3e4661de84c771364cf6a519a208b14

Fix images not loading when in airplane mode
This fixes a bad bug introduced in #1247. The code added a check for
internet before attempting to load the image (thinking it was checking
before attempting to download the image). This moves the internet
checking code to the right place and renames the method to avoid
confusion in the future.

app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranDisplayHelper.java
@@ -38,7 +38,14 @@ public class QuranDisplayHelper {
       // the user to mount their sdcard and try again.
       if (response.getErrorCode() != Response.ERROR_SD_CARD_NOT_FOUND) {
         Timber.d("failed to get %d with name %s from sd...", page, filename);
-        response = quranFileUtils.getImageFromWeb(okHttpClient, context, filename);
+
+        if (!QuranUtils.haveInternet(context)) {
+          final Response noNetworkResponse = new Response(Response.ERROR_NO_INTERNET);
+          noNetworkResponse.setPageData(page);
+          response = noNetworkResponse;
+        } else {
+          response = quranFileUtils.getImageFromWeb(okHttpClient, context, filename);
+        }
       }
     }
     return response;

app/src/main/java/com/quran/labs/androidquran/ui/helpers/QuranPageWorker.java
@@ -1,8 +1,6 @@
 package com.quran.labs.androidquran.ui.helpers;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
@@ -11,7 +9,6 @@ import com.quran.labs.androidquran.di.ActivityScope;
 import com.quran.labs.androidquran.util.QuranFileUtils;
 import com.quran.labs.androidquran.util.QuranScreenInfo;
 
-import com.quran.labs.androidquran.util.QuranUtils;
 import javax.inject.Inject;
 
 import io.reactivex.Observable;
@@ -41,15 +38,9 @@ public class QuranPageWorker {
     this.quranFileUtils = quranFileUtils;
   }
 
-  private Response downloadImage(int pageNumber) {
+  private Response loadImage(int pageNumber) {
     Response response = null;
     OutOfMemoryError oom = null;
-
-    if (!QuranUtils.haveInternet(appContext)) {
-      final Response noNetworkResponse = new Response(Response.ERROR_NO_INTERNET);
-      noNetworkResponse.setPageData(pageNumber);
-      return noNetworkResponse;
-    }
 
     try {
       response = QuranDisplayHelper.getQuranPage(
@@ -90,7 +81,7 @@ public class QuranPageWorker {
 
   public Observable<Response> loadPages(Integer... pages) {
     return Observable.fromArray(pages)
-        .flatMap(page -> Observable.fromCallable(() -> downloadImage(page)))
+        .flatMap(page -> Observable.fromCallable(() -> loadImage(page)))
         .subscribeOn(Schedulers.io());
   }
 }