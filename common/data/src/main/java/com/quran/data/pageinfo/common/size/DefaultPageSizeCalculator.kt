fe104347a9269ee5cd9a02501d9b136a305fc12b
ahmedre <ahmedre@gmail.com>
2020-02-09 23:48:49 +0400

    Fix bugs with the partial page checker and tablet
    Al7amdulillah, got really lucky here! The parital page checker had a
    number of bugs - namely it used the width where it should have used the
    tablet width. Consequently, it marks a massive number of pages as "to be
    deleted." Due to another bug though (looping over only the pages to
    delete instead of all pages to delete), al7amdulillah the pages didn't
    actually get deleted in this case. This fixes these bugs.

--- common/data/src/main/java/com/quran/data/pageinfo/common/size/DefaultPageSizeCalculator.kt
+++ common/data/src/main/java/com/quran/data/pageinfo/common/size/DefaultPageSizeCalculator.kt
@@ -29,10 +29,10 @@ open class DefaultPageSizeCalculator(displaySize: DisplaySize) : PageSizeCalcula
   }
 
   override fun setOverrideParameter(parameter: String) {
-    if (parameter.isNotBlank()) {
-      overrideParam = parameter
+    overrideParam = if (parameter.isNotBlank()) {
+      parameter
     } else {
-      overrideParam = null
+      null
     }
   }
 }
