aeff390d3922d438a5d1de2485fd652009468f80
Ismail S <ismail-s@users.noreply.github.com>
2020-06-10 22:12:09 +0100

    Fix case of ids

--- app/src/main/java/com/quran/labs/androidquran/SearchWidget.java
+++ app/src/main/java/com/quran/labs/androidquran/SearchWidget.java
@@ -6,7 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
-import com.quran.labs.androidquran.ui.PagerActivity;
+
 import com.quran.labs.androidquran.ui.QuranActivity;
 
 public class SearchWidget extends AppWidgetProvider {
@@ -19,16 +19,16 @@ public class SearchWidget extends AppWidgetProvider {
 
       Intent intent = new Intent(context, QuranDataActivity.class);
       PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-      widget.setOnClickPendingIntent(R.id.SearchWidgetIconButton, pendingIntent);
+      widget.setOnClickPendingIntent(R.id.search_widget_icon_button, pendingIntent);
 
       intent = new Intent(context, SearchActivity.class);
       pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-      widget.setOnClickPendingIntent(R.id.SearchWidgetBtnSearch, pendingIntent);
+      widget.setOnClickPendingIntent(R.id.search_widget_btn_search, pendingIntent);
 
       intent = new Intent(context, QuranActivity.class);
       intent.setAction(ShortcutsActivity.ACTION_JUMP_TO_LATEST);
       pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-      widget.setOnClickPendingIntent(R.id.SearchWidgetBtnGoToQuran, pendingIntent);
+      widget.setOnClickPendingIntent(R.id.search_widget_btn_go_to_quran, pendingIntent);
 
       appWidgetManager.updateAppWidget(appWidgetId, widget);
     }
