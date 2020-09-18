74ec9095589e202c570933119272c7fddc6025b8
Murtaza Raja <murtraja@gmail.com>
2020-09-19 04:26:25 +0530

    Floating highlights (#1313)
    * overlapping highlights - plan B
    * demonstrating all ways to animate
    * animating using the division algorithm
    * checking for coordinate data before making a copy
    * overlapping animations in ayahMode + animator null crash fix
    * removing debugging changes, cleanup, sura -> surah, adding comments + TODOs
    * expanding 'o' to original for rectToBeDivided
    * can't modify Evaluator result in place, have to make it immutable
    * refactoring into various classes
    * cleanup: to show or not to show animation
    * less convoluted way of accessing the highlights set and retrieving previousSurahAyah
    * adding another strategy which simplifies animation when going from more to less
    * using AyahHighlight instead of strings
    * resolving conflicts introduced by 3e3de4
    * Extracting AyahHighlight and friends out of the HighlightingImageView
    
    Co-authored-by: Murtaza Raja <murt.raja@gmail.com>

--- app/src/main/java/com/quran/labs/androidquran/ui/helpers/HighlightType.java
+++ app/src/main/java/com/quran/labs/androidquran/ui/helpers/HighlightType.java
@@ -8,20 +8,22 @@ import com.quran.labs.androidquran.R;
 
 public class HighlightType implements Comparable<HighlightType> {
 
-  public static final HighlightType SELECTION = new HighlightType(1, false, R.color.selection_highlight);
-  public static final HighlightType AUDIO =     new HighlightType(2, false, R.color.audio_highlight);
-  public static final HighlightType NOTE =      new HighlightType(3, true,  R.color.note_highlight);
-  public static final HighlightType BOOKMARK =  new HighlightType(4, true,  R.color.bookmark_highlight);
+  public static final HighlightType SELECTION = new HighlightType(1, false, R.color.selection_highlight, HighlightAnimationConfig.NONE);
+  public static final HighlightType AUDIO =     new HighlightType(2, false, R.color.audio_highlight,     HighlightAnimationConfig.AUDIO);
+  public static final HighlightType NOTE =      new HighlightType(3, true,  R.color.note_highlight,      HighlightAnimationConfig.NONE);
+  public static final HighlightType BOOKMARK =  new HighlightType(4, true,  R.color.bookmark_highlight,  HighlightAnimationConfig.NONE);
 
   private Long id;
   private boolean multipleHighlightsAllowed;
   private int colorId;
   private Integer color = null;
+  private HighlightAnimationConfig animationConfig;
 
-  private HighlightType(long id, boolean multipleHighlightsAllowed, int colorId) {
+  private HighlightType(long id, boolean multipleHighlightsAllowed, int colorId, HighlightAnimationConfig config) {
     this.id = id;
     this.multipleHighlightsAllowed = multipleHighlightsAllowed;
     this.colorId = colorId;
+    this.animationConfig = config;
   }
 
   public boolean isMultipleHighlightsAllowed() {
@@ -33,6 +35,14 @@ public class HighlightType implements Comparable<HighlightType> {
       color = ContextCompat.getColor(context, colorId);
     }
     return color;
+  }
+
+  public boolean isFloatable() {
+    return animationConfig.isFloatable();
+  }
+
+  public HighlightAnimationConfig getAnimationConfig() {
+    return animationConfig;
   }
 
   @Override
