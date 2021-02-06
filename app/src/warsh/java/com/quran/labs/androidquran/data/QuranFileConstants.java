f46ab3f5361cda3a54214f3ad284a5807d59a5df
Ahmed El-Helw <ahmedre@gmail.com>
2021-02-07 01:05:29 +0400

    Support for warsh fonts

--- app/src/warsh/java/com/quran/labs/androidquran/data/QuranFileConstants.java
+++ app/src/warsh/java/com/quran/labs/androidquran/data/QuranFileConstants.java
@@ -5,10 +5,10 @@ import com.quran.labs.androidquran.ui.util.TypefaceManager;
 
 public class QuranFileConstants {
   // server urls
-  public static final int FONT_TYPE = TypefaceManager.TYPE_UTHMANI_HAFS;
+  public static final int FONT_TYPE = TypefaceManager.TYPE_UTHMANIC_WARSH;
 
   // arabic database
-  public static final String ARABIC_DATABASE = "quran.ar.db";
+  public static final String ARABIC_DATABASE = "warsh.db";
   public static final String ARABIC_SHARE_TABLE = DatabaseHandler.ARABIC_TEXT_TABLE;
   public static final boolean ARABIC_SHARE_TEXT_HAS_BASMALLAH = true;
 }
