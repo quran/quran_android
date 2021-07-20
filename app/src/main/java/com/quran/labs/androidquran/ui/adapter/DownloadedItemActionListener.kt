ff30a88cd504a4b9f8636f7f05dddd57f7d48bae
wirya <abrarwiryawan@gmail.com>
2021-07-20 07:36:38 +0700

    migrate `DownloadedItemActionListener` to kotlin

---
+++ app/src/main/java/com/quran/labs/androidquran/ui/adapter/DownloadedItemActionListener.kt
@@ -0,0 +1,7 @@
+package com.quran.labs.androidquran.ui.adapter
+
+interface DownloadedItemActionListener {
+  fun handleDeleteItemAction()
+  fun handleRankUpItemAction()
+  fun handleRankDownItemAction()
+}
