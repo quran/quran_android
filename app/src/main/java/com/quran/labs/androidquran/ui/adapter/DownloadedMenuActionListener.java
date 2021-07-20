e282293d9e180c1507192d412e56bf1d8e5b1300
wirya <abrarwiryawan@gmail.com>
2021-07-20 07:37:09 +0700

    migrate `DownloadedMenuActionListener` to kotlin

--- app/src/main/java/com/quran/labs/androidquran/ui/adapter/DownloadedMenuActionListener.java
+++
@@ -1,8 +0,0 @@
-package com.quran.labs.androidquran.ui.adapter;
-
-import com.quran.labs.androidquran.dao.translation.TranslationItem;
-
-public interface DownloadedMenuActionListener {
-  void startMenuAction(TranslationItem item, DownloadedItemActionListener downloadedItemActionListener);
-  void finishMenuAction();
-}
