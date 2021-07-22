79a0f34d3479795e20203453177197cab570a7bb
wirya <abrarwiryawan@gmail.com>
2021-07-22 20:14:50 +0700

    migrate `PagerActivityModule` to kotlin

---
+++ app/src/main/java/com/quran/labs/androidquran/di/module/activity/PagerActivityModule.kt
@@ -0,0 +1,42 @@
+package com.quran.labs.androidquran.di.module.activity
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.quran.data.core.QuranInfo
+import com.quran.labs.androidquran.R
+import com.quran.labs.androidquran.di.ActivityScope
+import com.quran.labs.androidquran.ui.PagerActivity
+import com.quran.labs.androidquran.ui.helpers.AyahSelectedListener
+import com.quran.labs.androidquran.util.QuranScreenInfo
+import com.quran.labs.androidquran.util.QuranUtils
+import com.quran.labs.androidquran.util.TranslationUtil
+import dagger.Module
+import dagger.Provides
+
+@Module
+class PagerActivityModule(private val pagerActivity: PagerActivity) {
+
+  @Provides
+  fun provideAyahSelectedListener(): AyahSelectedListener {
+    return pagerActivity
+  }
+
+  @Provides
+  @ActivityScope
+  fun provideImageWidth(screenInfo: QuranScreenInfo): String {
+    return if (QuranUtils.isDualPages(pagerActivity, screenInfo)) {
+      screenInfo.tabletWidthParam
+    } else {
+      screenInfo.widthParam
+    }
+  }
+
+  @Provides
+  @ActivityScope
+  fun provideTranslationUtil(context: Context, quranInfo: QuranInfo): TranslationUtil {
+    return TranslationUtil(
+      ContextCompat.getColor(context, R.color.translation_translator_color),
+      quranInfo
+    )
+  }
+}
