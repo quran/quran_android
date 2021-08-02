8fed0b7226540b805d99fa3c3f4101038c19a0f2
wiryadev <abrarwiryawan@gmail.com>
2021-08-02 10:01:29 +0700

    remove SDK check for setting typeface overlayParams

--- app/src/main/java/com/quran/labs/androidquran/view/HighlightingImageView.java
+++ app/src/main/java/com/quran/labs/androidquran/view/HighlightingImageView.java
@@ -375,7 +375,7 @@ public class HighlightingImageView extends AppCompatImageView {
     overlayParams.rub3Text = rub3Text;
     overlayParams.paint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DEV_KERN_TEXT_FLAG);
     overlayParams.paint.setTextSize(fontSize);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && juzText.contains("ج")) {
+    if (juzText.contains("ج")) {
       // change typeface for Arabic
       overlayParams.paint.setTypeface(TypefaceManager.getHeaderFooterTypeface(context));
     }
