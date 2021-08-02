2947a9c11087c7f2848d8a698b0edcfc72d19dc1
wiryadev <abrarwiryawan@gmail.com>
2021-08-02 13:59:16 +0700

    make variable const

--- pages/madani/src/main/java/com/quran/data/page/provider/MadaniConstants.kt
+++ pages/madani/src/main/java/com/quran/data/page/provider/MadaniConstants.kt
@@ -1,8 +1,5 @@
 package com.quran.data.page.provider
 
-import android.os.Build.VERSION
-
 object MadaniConstants {
-  @JvmField
-  val ARABIC_DATABASE = "quran.ar.uthmani.v2.db"
+  const val ARABIC_DATABASE = "quran.ar.uthmani.v2.db"
 }
