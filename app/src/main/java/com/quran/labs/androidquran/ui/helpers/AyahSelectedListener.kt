a1038c160e919d520fad1ebacc747fcd5b968f97
wiryadev <abrarwiryawan@gmail.com>
2021-08-08 17:54:49 +0700

    migrate `AyahSelectedListener` to Kotlin

---
+++ app/src/main/java/com/quran/labs/androidquran/ui/helpers/AyahSelectedListener.kt
@@ -0,0 +1,27 @@
+package com.quran.labs.androidquran.ui.helpers
+
+import com.quran.data.model.SuraAyah
+
+interface AyahSelectedListener {
+
+  enum class EventType { SINGLE_TAP, LONG_PRESS, DOUBLE_TAP }
+
+  /** Return true to receive the ayah info along with the
+   * click event, false to receive just the event type  */
+  fun isListeningForAyahSelection(eventType: EventType): Boolean
+
+  /** Click event with ayah info and highlighter passed  */
+  fun onAyahSelected(
+    eventType: EventType?,
+    suraAyah: SuraAyah?, tracker: AyahTracker?
+  ): Boolean
+
+  /** General click event without ayah info  */
+  fun onClick(eventType: EventType): Boolean
+
+  /** end ayah mode  */
+  fun endAyahMode()
+
+  /** set menu bar position  */
+  fun requestMenuPositionUpdate(tracker: AyahTracker?)
+}
