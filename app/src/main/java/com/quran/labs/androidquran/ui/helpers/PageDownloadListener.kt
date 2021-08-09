396f711329d7bf97b77f9cadaf64449feac12f9b
wiryadev <abrarwiryawan@gmail.com>
2021-08-09 07:24:42 +0700

    remove `PageDownloadListener`

--- app/src/main/java/com/quran/labs/androidquran/ui/helpers/PageDownloadListener.kt
+++
@@ -1,8 +0,0 @@
-package com.quran.labs.androidquran.ui.helpers
-
-import android.graphics.drawable.BitmapDrawable
-import com.quran.labs.androidquran.common.Response
-
-interface PageDownloadListener {
-  fun onLoadImageResponse(drawable: BitmapDrawable?, response: Response?)
-}
