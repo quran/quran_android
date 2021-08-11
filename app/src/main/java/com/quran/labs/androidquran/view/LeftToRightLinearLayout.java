8f36644e4b56cc831db21e44c1c0e05791ce0994
wiryadev <abrarwiryawan@gmail.com>
2021-08-11 14:01:21 +0700

    remove unnecessary SDK checking and `TargetApi` annotation

--- app/src/main/java/com/quran/labs/androidquran/view/LeftToRightLinearLayout.java
+++ app/src/main/java/com/quran/labs/androidquran/view/LeftToRightLinearLayout.java
@@ -1,11 +1,10 @@
 package com.quran.labs.androidquran.view;
 
 import android.content.Context;
-import android.os.Build;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
 
 public class LeftToRightLinearLayout extends LinearLayout {
   public LeftToRightLinearLayout(Context context) {
@@ -18,17 +17,12 @@ public class LeftToRightLinearLayout extends LinearLayout {
 
   public LeftToRightLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      setLayoutDirection(LAYOUT_DIRECTION_LTR);
-    }
+    setLayoutDirection(LAYOUT_DIRECTION_LTR);
   }
 
-  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
   public LeftToRightLinearLayout(Context context, AttributeSet attrs,
                                  int defStyleAttr, int defStyleRes) {
     super(context, attrs, defStyleAttr, defStyleRes);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      setLayoutDirection(LAYOUT_DIRECTION_LTR);
-    }
+    setLayoutDirection(LAYOUT_DIRECTION_LTR);
   }
 }
