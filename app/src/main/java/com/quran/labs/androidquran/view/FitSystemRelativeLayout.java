23faf5c832cc6f7c89c3dbb487956a9e65d1f183
Abrar Wiryawan <abrarwiryawan@gmail.com>
2021-08-12 17:48:49 +0700

    Minor change in `AudioFocusHelper` and `BookmarkPresenterTest` (#1672)
    * change variable name
    * minor readability improvement for `BookmarkPresenterTest`

--- app/src/main/java/com/quran/labs/androidquran/view/FitSystemRelativeLayout.java
+++ app/src/main/java/com/quran/labs/androidquran/view/FitSystemRelativeLayout.java
@@ -2,13 +2,12 @@ package com.quran.labs.androidquran.view;
 
 import android.content.Context;
 import android.graphics.Rect;
-import android.os.Build;
-import androidx.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.quran.labs.androidquran.R;
+import androidx.annotation.NonNull;
 
 public class FitSystemRelativeLayout extends RelativeLayout {
   private static final boolean IS_PRE_KITKAT = false;
@@ -49,14 +48,12 @@ public class FitSystemRelativeLayout extends RelativeLayout {
     /*
       this is needed to fix a bug where the Toolbar is half cut off before Kitkat (especially when
       playing audio).
-
       the reason for this is that we always animate the Toolbar's parent to either 0 or to
       the negative value of its height, and on pre-Kitkat, the parent's height is incorrect (not
       reflecting the updated top margin on the toolbar itself), unless we explicitly
       requestLayout. the reason for this is that on Kitkat and above, the insets don't change when
       the toolbar is shown or hidden whereas pre-Kitkat, the inset's top value is 0 when the
       toolbar is gone and some value when the toolbar is visible.
-
       the audio bar solves this same problem by animating to its height plus its bottomMargin,
       (partially because the audio bar does not have a parent wrapper that is being animated, and,
       as a result, by definition, will never have its height reflect any updated margins). it is
