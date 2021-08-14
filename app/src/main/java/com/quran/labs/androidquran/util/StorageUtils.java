d95ab09b9459f6660f90201e4869e5470fc62dc1
wiryadev <abrarwiryawan@gmail.com>
2021-08-14 12:07:28 +0700

    remove unused method for Kitkat and below

--- app/src/main/java/com/quran/labs/androidquran/util/StorageUtils.java
+++ app/src/main/java/com/quran/labs/androidquran/util/StorageUtils.java
@@ -4,18 +4,14 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
+
 import androidx.core.content.ContextCompat;
 
 import com.quran.labs.androidquran.R;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-
-import timber.log.Timber;
 
 
 /**
@@ -84,158 +80,6 @@ public class StorageUtils {
       }
     }
     return result;
-  }
-
-  /**
-   * Attempt to return a list of storage locations pre-Kitkat.
-   * @param context the context
-   * @return the list of storage locations
-   */
-  private static List<Storage> getLegacyStorageLocations(Context context) {
-    List<String> mounts = readMountsFile();
-
-    // As per http://source.android.com/devices/tech/storage/config.html
-    // device-specific vold.fstab file is removed after Android 4.2.2
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
-      Set<String> volds = readVoldsFile();
-
-      List<String> toRemove = new ArrayList<>();
-      for (String mount : mounts) {
-        if (!volds.contains(mount)) {
-          toRemove.add(mount);
-        }
-      }
-
-      for (String s : toRemove) {
-        mounts.remove(s);
-      }
-    } else {
-      Timber.d("Android version: %d, skip reading vold.fstab file", Build.VERSION.SDK_INT);
-    }
-
-    Timber.d("mounts list is: %s", mounts);
-    return buildMountsList(context, mounts);
-  }
-
-  /**
-   * Converts a list of mount strings to a list of Storage items
-   * @param context the context
-   * @param mounts a list of mount points as strings
-   * @return a list of Storage items that can be rendered by the ui
-   */
-  private static List<Storage> buildMountsList(Context context, List<String> mounts) {
-    List<Storage> list = new ArrayList<>(mounts.size());
-
-    int externalSdcardsCount;
-    if (mounts.size() > 0) {
-      // internal files dir
-      list.add(
-          new Storage(context.getString(R.string.prefs_sdcard_internal),
-              context.getFilesDir().getAbsolutePath()));
-
-      externalSdcardsCount = 1;
-      list.add(new Storage(context.getString(R.string.prefs_sdcard_external,
-          externalSdcardsCount), mounts.get(0)));
-
-      // All other mounts rather than the first mount point are considered as External SD Card
-      if (mounts.size() > 1) {
-        externalSdcardsCount++;
-        for (int i = 1/*skip the first item*/; i < mounts.size(); i++) {
-          list.add(new Storage(context.getString(R.string.prefs_sdcard_external,
-              externalSdcardsCount++), mounts.get(i)));
-        }
-      }
-    }
-
-    Timber.d("final storage list is: %s", list);
-    return list;
-  }
-
-  /**
-   * Read /proc/mounts. This is a set of hacks for versions below Kitkat.
-   * @return list of mounts based on the mounts file.
-   */
-  private static List<String> readMountsFile() {
-    String sdcardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
-    List<String> mounts = new ArrayList<>();
-    mounts.add(sdcardPath);
-
-    Timber.d("reading mounts file begin");
-    try {
-      File mountFile = new File("/proc/mounts");
-      if (mountFile.exists()) {
-        Timber.d("mounts file exists");
-        Scanner scanner = new Scanner(mountFile, "US-ASCII");
-        while (scanner.hasNext()) {
-          String line = scanner.nextLine();
-          Timber.d("line: %s", line);
-          if (line.startsWith("/dev/block/vold/")) {
-            String[] lineElements = line.split(" ", -1);
-            String element = lineElements[1];
-            Timber.d("mount element is: %s", element);
-            if (!sdcardPath.equals(element)) {
-              mounts.add(element);
-            }
-          } else {
-            Timber.d("skipping mount line: %s", line);
-          }
-        }
-      } else {
-        Timber.d("mounts file doesn't exist");
-      }
-
-      Timber.d("reading mounts file end.. list is: %s", mounts);
-    } catch (Exception e) {
-      Timber.e(e, "Error reading mounts file");
-    }
-    return mounts;
-  }
-
-  /**
-   * Reads volume manager daemon file for auto-mounted storage.
-   * Read more about it <a href="http://vold.sourceforge.net/">here</a>.
-   *
-   * Set usage, to safely avoid duplicates, is intentional.
-   * @return Set of mount points from `vold.fstab` configuration file
-   */
-  private static Set<String> readVoldsFile() {
-    Set<String> volds = new HashSet<>();
-    volds.add(Environment.getExternalStorageDirectory().getAbsolutePath());
-
-    Timber.d("reading volds file");
-    try {
-      File voldFile = new File("/system/etc/vold.fstab");
-      if (voldFile.exists()) {
-        Timber.d("reading volds file begin");
-        Scanner scanner = new Scanner(voldFile, "US-ASCII");
-        while (scanner.hasNext()) {
-          String line = scanner.nextLine();
-          Timber.d("line: %s", line);
-          if (line.startsWith("dev_mount")) {
-            String[] lineElements = line.split(" ", -1);
-            String element = lineElements[2];
-            Timber.d("volds element is: %s", element);
-
-            if (element.contains(":")) {
-              element = element.substring(0, element.indexOf(":"));
-              Timber.d("volds element is: %s", element);
-            }
-
-            Timber.d("adding volds element to list: %s", element);
-            volds.add(element);
-          } else {
-            Timber.d("skipping volds line: %s", line);
-          }
-        }
-      } else {
-        Timber.d("volds file doesn't exit");
-      }
-      Timber.d("reading volds file end.. list is: %s", volds);
-    } catch (Exception e) {
-      Timber.e(e, "Error reading volds file");
-    }
-
-    return volds;
   }
 
   public static class Storage {
