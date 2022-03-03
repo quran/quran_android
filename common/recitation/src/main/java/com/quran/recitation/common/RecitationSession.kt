c7de88bf2082962d374e18dfa92a129dd7757ca8
aabdurrahman <49524354+aabdurrahman@users.noreply.github.com>
2022-03-03 10:07:08 -0500

    Add recitation module

---
+++ common/recitation/src/main/java/com/quran/recitation/common/RecitationSession.kt
@@ -0,0 +1,12 @@
+package com.quran.recitation.common
+
+import com.quran.data.model.SuraAyah
+
+interface RecitationSession {
+  fun id(): String
+
+  fun startAyah(): SuraAyah
+  fun currentAyah(): SuraAyah
+
+  fun recitation(): Recitation
+}
