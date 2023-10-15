7dab4936d5911db80b2b00a1953e2f0d0bea306a
Ahmed El-Helw <ahmedre@gmail.com>
2023-10-15 17:30:53 +0400

    Use ksp for Moshi and avoid kapt for Dagger
    We only need kapt for Dagger for the app module (and, as of today, we
    can't yet use ksp Dagger here due to the interop with Anvil). This at
    least limits kapt usage only to the app module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.crashlytics) apply false
   alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.ksp) apply false
   alias(libs.plugins.errorprone) apply false
   alias(libs.plugins.google.services) apply false
   alias(libs.plugins.sqldelight) apply false
