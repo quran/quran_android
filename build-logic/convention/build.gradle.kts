7892dc0280a1d4cd2a44b4b400f3af1798e474c4
Ahmed El-Helw <ahmedre@gmail.com>
2024-01-12 23:37:45 +0400

    Update Kotlin to 1.9.22

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -6,7 +6,7 @@ group = "com.quran.labs.androidquran.buildlogic"
 
 dependencies {
   compileOnly("com.android.tools.build:gradle:8.2.1")
-  compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+  compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
 
