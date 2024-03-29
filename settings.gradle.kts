7fe57bb843af6c488f61aa683947f586f17cb2ef
Jibran Iqbal <jibran.iqbal@protonmail.com>
2024-03-29 06:31:22 +0100

    Added module to setting.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -45,6 +45,7 @@ include(":pages:data:madani")
 include(":pages:data:warsh")
 include(":pages:common:madani")
 include(":pages:common:warsh")
+include(":autoquran")
 
 if (File(rootDir, "extras/settings-extra.gradle").exists()) {
   apply(File(rootDir, "extras/settings-extra.gradle"))
