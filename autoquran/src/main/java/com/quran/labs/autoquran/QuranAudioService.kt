4e872122b28a473d2c34da4aa4b502e59a3af57e
Jibran Iqbal <jibran.iqbal@protonmail.com>
2024-10-02 01:23:15 +0200

    Cleaned up, & Added multiple Qari with gapeless

--- autoquran/src/main/java/com/quran/labs/autoquran/QuranAudioService.kt
+++ autoquran/src/main/java/com/quran/labs/autoquran/QuranAudioService.kt
@@ -1,61 +1,40 @@
 package com.quran.labs.autoquran
 
-import android.media.AudioAttributes
-import android.media.AudioManager
 import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat.MediaItem
-import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
-import android.util.Log
-import androidx.core.net.toUri
 import androidx.media.MediaBrowserServiceCompat
-import androidx.media.utils.MediaConstants
-import com.quran.data.core.QuranInfo
-import com.quran.labs.autoquran.common.getSuraName
-import com.quran.labs.autoquran.common.makeThreeDigit
+import com.quran.labs.autoquran.common.MediaSessionCallback
+import com.quran.labs.autoquran.common.SurahBuilder
+import com.quran.labs.autoquran.common.filterWithQari
 import com.quran.labs.autoquran.di.DaggerServiceComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class QuranAudioService : MediaBrowserServiceCompat(), MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
+class QuranAudioService : MediaBrowserServiceCompat() {
   @Inject
-  lateinit var quranInfo: QuranInfo
+  lateinit var surahBuilder: SurahBuilder
   private lateinit var mSession: MediaSessionCompat
-  private var job: Job? = null
-  var mediaPlayer: MediaPlayer? = null
-  var loadedMediaItem: MediaItem? = null
-  val mediaItems = mutableListOf(
-    createBrowsableMediaItem("quran", "Quran")
+  private var mediaPlayer: MediaPlayer? = null
+  private var mediaItems = mutableListOf<MediaItem>()
+  private val mediaSessionCallback = MediaSessionCallback(
+    context = this,
+    setMediaPlayer = { mediaPlayer = it },
+    setMetaData = this::setMetaData,
+    setMediaPlaybackState = this::setMediaPlaybackState,
   )
 
   override fun onCreate() {
     super.onCreate()
-    mSession = MediaSessionCompat(baseContext, "QuranAudioService")
-    setSessionToken(mSession.sessionToken)
-    mSession.setCallback(MediaSessionCallback())
     val component = DaggerServiceComponent.create()
     component.inject(this)
-    quranInfo.suraPageStart.forEachIndexed { index, _ ->
-      val name = getSuraName(this, index + 1, true, false)
-      mediaItems.add(
-        createMediaItem(
-          name,
-          name,
-          "Quran",
-          "https://download.quranicaudio.com/quran/muhammad_siddeeq_al-minshaawee/${
-            makeThreeDigit(index + 1)
-          }.mp3".toUri()
-        )
-      )
-    }
+    mediaItems = surahBuilder.create(this)
+    mSession = MediaSessionCompat(baseContext, "QuranAudioService")
+    setSessionToken(mSession.sessionToken)
+    mediaSessionCallback.setMediaItems(mediaItems)
+    mSession.setCallback(mediaSessionCallback)
   }
 
   override fun onDestroy() {
@@ -75,148 +54,25 @@ class QuranAudioService : MediaBrowserServiceCompat(), MediaPlayer.OnPreparedLis
     parentMediaId: String,
     result: Result<List<MediaItem>>
   ) {
-    result.sendResult(mediaItems)
-  }
-
-  private fun createBrowsableMediaItem(
-    mediaId: String,
-    folderName: String
-  ): MediaItem {
-    val mediaDescriptionBuilder = MediaDescriptionCompat.Builder()
-    mediaDescriptionBuilder.setMediaId(mediaId)
-    mediaDescriptionBuilder.setTitle(folderName)
-    val extras = Bundle()
-    extras.putInt(
-      MediaConstants.DESCRIPTION_EXTRAS_KEY_CONTENT_STYLE_SINGLE_ITEM,
-      MediaConstants.DESCRIPTION_EXTRAS_VALUE_CONTENT_STYLE_CATEGORY_LIST_ITEM
-    )
-    extras.putInt(
-      MediaConstants.DESCRIPTION_EXTRAS_KEY_CONTENT_STYLE_BROWSABLE,
-      MediaConstants.DESCRIPTION_EXTRAS_VALUE_CONTENT_STYLE_LIST_ITEM
-    )
-    extras.putInt(
-      MediaConstants.DESCRIPTION_EXTRAS_KEY_CONTENT_STYLE_PLAYABLE,
-      MediaConstants.DESCRIPTION_EXTRAS_VALUE_CONTENT_STYLE_GRID_ITEM
-    )
-    extras.putDouble(
-      MediaConstants.DESCRIPTION_EXTRAS_KEY_COMPLETION_PERCENTAGE,
-      0.0
-    )
-    mediaDescriptionBuilder.setExtras(extras)
-    return MediaItem(
-      mediaDescriptionBuilder.build(), MediaItem.FLAG_BROWSABLE
-    )
-  }
-
-  private fun createMediaItem(mediaId: String, title: String, artist: String, uri: Uri): MediaItem {
-    val playbackStateExtras = Bundle()
-    playbackStateExtras.putString(
-      MediaConstants.PLAYBACK_STATE_EXTRAS_KEY_MEDIA_ID, mediaId
-    )
-    val description = MediaDescriptionCompat.Builder()
-      .setMediaId(mediaId)
-      .setTitle(title)
-      .setSubtitle(artist)
-      .setMediaUri(uri)
-      .setExtras(playbackStateExtras)
-      .build()
-
-    return MediaItem(description, MediaItem.FLAG_PLAYABLE)
-  }
-
-  override fun onPrepared(mp: MediaPlayer) {
-    mp.start()
-    setMetaData(loadedMediaItem!!)
-    setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING)
-    startUpdatingProgress(mediaPlayer!!)
-  }
-
-  override fun onCompletion(mp: MediaPlayer?) {
-    mp?.pause()
-    setMediaPlaybackState(PlaybackStateCompat.STATE_PAUSED)
-    stopUpdatingProgress()
-  }
-
-  private inner class MediaSessionCallback : MediaSessionCompat.Callback() {
-    override fun onPlay() {
-      if (mediaPlayer?.isPlaying == false) {
-        mediaPlayer?.start()
-        setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING, mediaPlayer?.currentPosition?.toLong())
-        startUpdatingProgress(mediaPlayer!!)
-      }
+    when (parentMediaId) {
+      "root" -> result.sendResult(mediaItems)
+      else -> result.sendResult(mediaItems.filterWithQari(parentMediaId))
     }
+  }
 
-    override fun onSkipToQueueItem(queueId: Long) {}
-    override fun onSeekTo(position: Long) {
-      mediaPlayer?.seekTo(position.toInt())
-    }
 
-    override fun onPlayFromMediaId(mediaId: String, extras: Bundle) {
-      val mediaUrl: MediaItem? = mediaItems.find { mediaId == it.description.mediaId }
-      loadedMediaItem = mediaUrl
-      prepareMediaItem(mediaUrl)
-    }
-
-    override fun onPause() {
-      if (mediaPlayer?.isPlaying == true) {
-        mediaPlayer?.pause()
-        setMediaPlaybackState(
-          PlaybackStateCompat.STATE_PAUSED,
-          mediaPlayer?.currentPosition?.toLong()
+  private fun setMediaPlaybackState(state: Int, currentPosition: Long? = 0) {
+    mSession.setPlaybackState(
+      PlaybackStateCompat.Builder()
+        .setState(state, currentPosition ?: 0, 1.0f)
+        .setActions(
+          PlaybackStateCompat.ACTION_PLAY or
+              PlaybackStateCompat.ACTION_PAUSE or
+              PlaybackStateCompat.ACTION_STOP or
+              PlaybackStateCompat.ACTION_SEEK_TO or
+              PlaybackStateCompat.ACTION_SKIP_TO_NEXT or
+              PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
         )
-        stopUpdatingProgress()
-      }
-    }
-
-    override fun onStop() {
-      mediaPlayer?.stop()
-      setMediaPlaybackState(PlaybackStateCompat.STATE_STOPPED)
-      stopUpdatingProgress()
-      Log.d("QuranAudioService", "onStop")
-    }
-
-    override fun onSkipToNext() {
-      val currentMediaIndex = mediaItems.indexOf(loadedMediaItem)
-      if (currentMediaIndex >= 0) {
-        mediaPlayer?.stop()
-        val nextMediaItem = mediaItems[currentMediaIndex + 1]
-        prepareMediaItem(nextMediaItem)
-        loadedMediaItem = nextMediaItem
-      }
-    }
-
-    override fun onSkipToPrevious() {
-      val currentMediaIndex = mediaItems.indexOf(loadedMediaItem)
-      if (currentMediaIndex > 1) {
-        mediaPlayer?.stop()
-        val nextMediaItem = mediaItems[currentMediaIndex - 1]
-        prepareMediaItem(nextMediaItem)
-        loadedMediaItem = nextMediaItem
-      }
-    }
-    override fun onCustomAction(action: String, extras: Bundle) {}
-    override fun onPlayFromSearch(query: String, extras: Bundle) {}
-  }
-
-  private fun prepareMediaItem(mediaUrl: MediaItem?) {
-    if (mediaUrl != null) {
-      mediaPlayer = MediaPlayer().apply {
-        setAudioAttributes()
-        setMetaData(mediaUrl)
-        setOnPreparedListener(this@QuranAudioService)
-        setOnCompletionListener(this@QuranAudioService)
-        setDataSource(baseContext, mediaUrl.description.mediaUri ?: Uri.EMPTY)
-        prepareAsync()
-      }
-    }
-  }
-
-  private fun MediaPlayer.setAudioAttributes() {
-    setAudioAttributes(
-      AudioAttributes.Builder()
-        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-        .setUsage(AudioAttributes.USAGE_MEDIA)
-        .setLegacyStreamType(AudioManager.STREAM_MUSIC)
         .build()
     )
   }
@@ -239,36 +95,5 @@ class QuranAudioService : MediaBrowserServiceCompat(), MediaPlayer.OnPreparedLis
         )
         .build()
     )
-  }
-
-  private fun setMediaPlaybackState(state: Int, currentPosition: Long? = 0) {
-    mSession.setPlaybackState(
-      PlaybackStateCompat.Builder()
-        .setState(state, currentPosition ?: 0, 1.0f)
-        .setActions(
-          PlaybackStateCompat.ACTION_PLAY or
-              PlaybackStateCompat.ACTION_PAUSE or
-              PlaybackStateCompat.ACTION_STOP or
-              PlaybackStateCompat.ACTION_SEEK_TO or
-              PlaybackStateCompat.ACTION_SKIP_TO_NEXT or
-              PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
-        )
-        .build()
-    )
-  }
-
-  private fun startUpdatingProgress(mediaPlayer: MediaPlayer) {
-    job = CoroutineScope(Dispatchers.Main).launch {
-      while (mediaPlayer.isPlaying) {
-        val currentPosition = mediaPlayer.currentPosition
-        setMediaPlaybackState(PlaybackStateCompat.STATE_PLAYING, currentPosition.toLong())
-        delay(1000) // Update progress every second, adjust as needed
-      }
-    }
-    job?.start()
-  }
-
-  private fun stopUpdatingProgress() {
-    job?.cancel()
   }
 }
