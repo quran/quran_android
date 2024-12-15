5552dcf5dac44c4b92641f807c179202033b2593
Ahmed El-Helw <ahmedre@gmail.com>
2024-12-15 23:56:28 +0400

    Bump version to 3.4.5-p3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
   namespace = "com.quran.labs.androidquran"
 
   defaultConfig {
-    versionCode = 3452
+    versionCode = 3453
     versionName = "3.4.5"
     testInstrumentationRunner = "com.quran.labs.androidquran.core.QuranTestRunner"
   }
