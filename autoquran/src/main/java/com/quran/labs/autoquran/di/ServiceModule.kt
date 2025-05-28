810924d81b24c95e3695571ca129c53e606121d8
Ahmed El-Helw <ahmedre@gmail.com>
2025-05-29 01:20:42 +0400

    Fix build for Android Automotive

--- autoquran/src/main/java/com/quran/labs/autoquran/di/ServiceModule.kt
+++ autoquran/src/main/java/com/quran/labs/autoquran/di/ServiceModule.kt
@@ -1,9 +1,12 @@
 package com.quran.labs.autoquran.di
 
 import android.content.Context
+import com.quran.data.model.audio.Qari
 import com.quran.data.page.provider.QuranDataModule
 import com.quran.data.source.PageProvider
 import com.quran.data.source.QuranDataSource
+import com.quran.labs.androidquran.common.audio.model.QariItem
+import com.quran.labs.androidquran.common.audio.util.AudioExtensionDecider
 import com.quran.labs.feature.autoquran.service.QuranBrowsableAudioPlaybackService
 import com.quran.mobile.di.qualifier.ApplicationContext
 import dagger.Component
@@ -27,6 +30,16 @@ class ServiceModule(private val appContext: Context) {
   @Provides
   fun provideQuranDataSource(pageProvider: PageProvider): QuranDataSource {
     return pageProvider.getDataSource()
+  }
+
+  @Provides
+  fun provideAudioExtensionDecider(): AudioExtensionDecider {
+    return object : AudioExtensionDecider {
+      override fun audioExtensionForQari(qari: Qari): String = "mp3"
+      override fun audioExtensionForQari(qariItem: QariItem): String = "mp3"
+      override fun allowedAudioExtensions(qari: Qari): List<String> = listOf("mp3")
+      override fun allowedAudioExtensions(qariItem: QariItem): List<String> = listOf("mp3")
+    }
   }
 }
 
