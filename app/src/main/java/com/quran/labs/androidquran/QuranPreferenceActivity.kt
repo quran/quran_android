1ac5e43ed2db07e06ec8f100b5ffaf0411847ad0
Ahmed El-Helw <ahmedre@gmail.com>
2025-06-04 01:51:09 +0400

    Support light mode in preferences

--- app/src/main/java/com/quran/labs/androidquran/QuranPreferenceActivity.kt
+++ app/src/main/java/com/quran/labs/androidquran/QuranPreferenceActivity.kt
@@ -16,13 +16,7 @@ import com.quran.labs.androidquran.ui.fragment.QuranSettingsFragment
 class QuranPreferenceActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    // override these to always be dark since the app doesn't really
-    // have a light theme until now. without this, the clock color in
-    // the status bar will be dark on a dark background.
-    enableEdgeToEdge(
-      statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
-      navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
-    )
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
     setContentView(R.layout.preferences)
 
