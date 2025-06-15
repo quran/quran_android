3ed8555e1039eb04733215bbf12a7ed846ed6b87
Ahmed El-Helw <ahmedre@gmail.com>
2025-06-15 21:25:37 +0400

    Use ExoPlayer instead of MediaPlayer
    ExoPlayer is much more resilient and customizable and is the recommended
    route today. For Quran specifically, it has improved seeking, which
    stops vbr audio seeks from being extremely inaccurate, and stops us from
    having to force encode everything as cbr.
    
    It also handles things for us (like audio focus and ducking) resulting
    in less code to maintain. Since it is in use for Android Auto also,
    there is no additional apk size to pay on top of today's apk size.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -171,6 +171,8 @@ dependencies {
 
   implementation(libs.androidx.appcompat)
   implementation(libs.androidx.media)
+  implementation(libs.androidx.media3.exoplayer)
+  implementation(libs.androidx.media3.session)
   implementation(libs.androidx.localbroadcastmanager)
   implementation(libs.androidx.preference.ktx)
   implementation(libs.androidx.recyclerview)
