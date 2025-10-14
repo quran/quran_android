a45e65b1598485e2375272aaeb39745600e14234
Ahmed Hamad <60454980+ahmed-okkio@users.noreply.github.com>
2025-10-14 23:29:05 +0100

    Make container background transparent

--- feature/qarilist/src/main/kotlin/com/quran/mobile/feature/qarilist/QariListWrapper.kt
+++ feature/qarilist/src/main/kotlin/com/quran/mobile/feature/qarilist/QariListWrapper.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -101,6 +102,7 @@ class QariListWrapper(
         sheetPeekHeight = 0.dp,
         sheetDragHandle = null,
         sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        containerColor = Color.Transparent,
         sheetContainerColor = MaterialTheme.colorScheme.surface,
         sheetContentColor = MaterialTheme.colorScheme.contentColorFor(MaterialTheme.colorScheme.background),
         sheetContent = {
