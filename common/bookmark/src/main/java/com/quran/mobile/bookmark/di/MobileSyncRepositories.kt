d1acfb6d4378f2d2cd025a9a8a917675948656ec
Ahmed El-Helw <ahmedre@gmail.com>
2026-05-10 01:01:02 +0400

    Add a data change notifier to make sync easier
    In the near future, in sha' Allah, when sync support is added, people
    with sync enabled should end up telling sync that an update happened.
    This patch makes that easier.

---
+++ common/bookmark/src/main/java/com/quran/mobile/bookmark/di/MobileSyncRepositories.kt
@@ -0,0 +1,29 @@
+package com.quran.mobile.bookmark.di
+
+import com.quran.data.di.AppScope
+import com.quran.shared.persistence.repository.bookmark.repository.BookmarksRepository
+import com.quran.shared.persistence.repository.bookmark.repository.BookmarksRepositoryImpl
+import com.quran.shared.persistence.repository.collection.repository.CollectionsRepository
+import com.quran.shared.persistence.repository.collection.repository.CollectionsRepositoryImpl
+import com.quran.shared.persistence.repository.collectionbookmark.repository.CollectionBookmarksRepository
+import com.quran.shared.persistence.repository.collectionbookmark.repository.CollectionBookmarksRepositoryImpl
+import com.quran.shared.persistence.repository.readingbookmark.repository.ReadingBookmarksRepository
+import com.quran.shared.persistence.repository.readingbookmark.repository.ReadingBookmarksRepositoryImpl
+import com.quran.shared.persistence.repository.readingsession.repository.ReadingSessionsRepository
+import com.quran.shared.persistence.repository.readingsession.repository.ReadingSessionsRepositoryImpl
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.SingleIn
+
+@SingleIn(AppScope::class)
+class MobileSyncRepositories @Inject constructor(
+  mobileSyncDatabase: MobileSyncDatabase
+) {
+  val bookmarksRepository: BookmarksRepository = BookmarksRepositoryImpl(mobileSyncDatabase.database)
+  val collectionsRepository: CollectionsRepository = CollectionsRepositoryImpl(mobileSyncDatabase.database)
+  val collectionBookmarksRepository: CollectionBookmarksRepository =
+    CollectionBookmarksRepositoryImpl(mobileSyncDatabase.database)
+  val readingBookmarksRepository: ReadingBookmarksRepository =
+    ReadingBookmarksRepositoryImpl(mobileSyncDatabase.database)
+  val readingSessionsRepository: ReadingSessionsRepository =
+    ReadingSessionsRepositoryImpl(mobileSyncDatabase.database)
+}
