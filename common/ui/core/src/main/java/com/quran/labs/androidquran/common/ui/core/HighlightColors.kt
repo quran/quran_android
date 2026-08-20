300a414969043e499c3757cf16e1d294af8c7448
Ahmed El-Helw <ahmedre@gmail.com>
2026-08-21 01:47:50 +0400

    Draw highlights on normal image pages

--- common/ui/core/src/main/java/com/quran/labs/androidquran/common/ui/core/HighlightColors.kt
+++ common/ui/core/src/main/java/com/quran/labs/androidquran/common/ui/core/HighlightColors.kt
@@ -1,5 +1,6 @@
 package com.quran.labs.androidquran.common.ui.core
 
+import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
@@ -10,6 +11,7 @@ import com.quran.mobile.common.ui.core.R
 data class HighlightColorSpec(
   val highlightColor: HighlightColor,
   val color: Color,
+  @ColorRes val colorResourceId: Int,
   @StringRes val nameResourceId: Int
 )
 
@@ -17,30 +19,35 @@ object HighlightColors {
   private val yellow = HighlightColorSpec(
     highlightColor = HighlightColor.YELLOW,
     color = Color(0xFFFDEC63),
+    colorResourceId = R.color.highlight_page_yellow,
     nameResourceId = R.string.highlight_color_yellow
   )
 
   private val green = HighlightColorSpec(
     highlightColor = HighlightColor.GREEN,
     color = Color(0xFFC1EC71),
+    colorResourceId = R.color.highlight_page_green,
     nameResourceId = R.string.highlight_color_green
   )
 
   private val blue = HighlightColorSpec(
     highlightColor = HighlightColor.BLUE,
     color = Color(0xFFADD7FE),
+    colorResourceId = R.color.highlight_page_blue,
     nameResourceId = R.string.highlight_color_blue
   )
 
   private val red = HighlightColorSpec(
     highlightColor = HighlightColor.RED,
     color = Color(0xFFFEB0CA),
+    colorResourceId = R.color.highlight_page_red,
     nameResourceId = R.string.highlight_color_red
   )
 
   private val purple = HighlightColorSpec(
     highlightColor = HighlightColor.PURPLE,
     color = Color(0xFFD8B1FE),
+    colorResourceId = R.color.highlight_page_purple,
     nameResourceId = R.string.highlight_color_purple
   )
 
